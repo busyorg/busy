5b957221c05fb96b653e48995868b791bb65049c
adcpm <fabien@adcpm.com>
2016-09-01 23:32:46 +0700

    - Update

--- src/containers/triggers.js
+++ src/containers/triggers.js
@@ -5,14 +5,14 @@ var React = require('react'),
 module.exports = React.createClass({
 	render: function(){
 		return (
-			<div className="triggers">
-
-				{this.props.edit && <Link to="/profile/edit" className="trigger"><i className="icon icon-md material-icons">format_paint</i></Link>}
-				{this.props.likes && <a href="#replies" className="trigger"><i className="icon icon-md material-icons">thumb_up</i></a>}
-				{this.props.replies && <a href="#replies" className="trigger"><i className="icon icon-md material-icons">reply</i></a>}
-				{this.props.messages && <a href="#messages" className="trigger"><i className="icon icon-md material-icons">chat_bubble_outline</i></a>}
-				{this.props.add && <a href="#write" className="trigger"><i className="icon icon-md material-icons">add</i></a>}
-				<Reply />
+			<div className="actions">
+				<div className="triggers">
+					{this.props.edit && <Link to="/profile/edit" className="trigger"><i className="icon icon-md material-icons">format_paint</i></Link>}
+					{this.props.likes && <a href="#replies" className="trigger"><i className="icon icon-md material-icons">thumb_up</i></a>}
+					{this.props.replies && <a href="#replies" className="trigger"><i className="icon icon-md material-icons">reply</i></a>}
+					{this.props.messages && <a href="#messages" className="trigger"><i className="icon icon-md material-icons">chat_bubble_outline</i></a>}
+					{this.props.add && <a href="#write" className="trigger"><i className="icon icon-md material-icons">add</i></a>}
+				</div>
 			</div>
 		);
 	}
