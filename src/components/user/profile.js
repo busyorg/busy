80a5a0208d2ab93bd1ed8012a157e2e55019190f
adcpm <fabien@adcpm.com>
2016-09-04 15:03:12 +0700

    - Update

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -46,6 +46,8 @@ var Profile = React.createClass({
 	render: function(){
 		var username = this.props.params.name;
 		var account = this.state.account;
+		try { var jsonMetadata = JSON.parse(account.json_metadata); }
+		catch(e) { var jsonMetadata = {}; }
 		var edit = (this.props.auth.isAuthenticated && username == this.props.auth.user.name);
 		return (
 			<div className="main-panel">
@@ -53,17 +55,13 @@ var Profile = React.createClass({
 				<Header account={username} />
 				<section className="align-center bg-green profile-header"
           style={{backgroundImage: 'url(https://img.busy6.com/@' + username + '/cover)', backgroundSize: 'cover'}}>
-					{_.has(account, 'name') && <div className="activity container row">
-							<div className="col col-lg-6">Created {moment(account.created).fromNow()}</div>
-							<div className="col col-lg-6">Last Active {moment(account.last_active).fromNow()}</div>
-						</div>}
 					<div className="pal">
 						<div className="mvl">
 							<div className="avatar avatar-xl">
 								{_.has(account, 'name') && <div className="reputation">{formatter.reputation(account.reputation)}</div>}
 								<img src={'https://img.busy6.com/@' + username} />
 							</div>
-							<h1>@{username}</h1>
+							<h1>{_.has(jsonMetadata, 'name')? jsonMetadata.name : '@' + username}</h1>
 						</div>
 					</div>
 				</section>
@@ -76,7 +74,14 @@ var Profile = React.createClass({
 							<li><Link to={'/@' + username + '/followers'}><i className="icon icon-md material-icons">people</i> {numeral(parseInt(this.state.followersCount)).format('0,0')}<span className="hidden-xs"> Followers</span></Link></li>
 							<li><Link to={'/@' + username + '/followed'}><i className="icon icon-md material-icons">people</i> {numeral(parseInt(this.state.followingCount)).format('0,0')}<span className="hidden-xs"> Followed</span></Link></li>
 						</ul>
-						<div className="container"></div>
+						<center className="mal">
+							{_.has(jsonMetadata, 'about') && <h3>{jsonMetadata.about}</h3>}
+							{_.has(jsonMetadata, 'website') && <p><i className="icon icon-md material-icons">link</i> <a href={jsonMetadata.website} target="_blank">{jsonMetadata.website}</a></p>}
+							{_.has(jsonMetadata, 'location') && <p><i className="icon icon-md material-icons">pin_drop</i> {jsonMetadata.location}</p>}
+							{_.has(account, 'name') && <p>
+								<span>Joined {moment(account.created).fromNow()}</span> <span>, last activity {moment(account.last_active).fromNow()}</span>
+							</p>}
+						</center>
 					</div>}
 					<div><div style={{height: '20px', overflow: 'hidden'}}></div></div>
 					<Feed path={'@' + username + '/posts'} sortBy="created" limit="1" replies="false" />
--- src/steem/api.js
+++ src/steem/api.js
@@ -1,3 +1,3 @@
-var api = require('./../../node_modules/steem/lib/api');
+var api = require('steem').api;
 api.setWebSocket('wss://node.steem.ws');
 module.exports = api;
