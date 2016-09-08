4223af42d3aefebb58ec700ca16ea9dd53e913a9
Nilesh Suthar <nileshsuthar@live.in>
2016-09-08 12:10:12 +0530

    Add Infinite Scroller for feed

--- src/containers/feed/feed.js
+++ src/containers/feed/feed.js
@@ -5,28 +5,88 @@ var React = require('react'),
   AddPost = require('./../add-post'),
   Post = require('./../post/post');
 
-module.exports = React.createClass({
-  componentWillMount: function() {
-    this.setState({content: {}});
-    api.getState(this.props.path, function(err, result) {
-      this.setState({content: result['content']});
-    }.bind(this));
-  },
-  render: function(){
-    var content = this.state.content;
-    content = (this.props.sortBy)? _.sortBy(content, this.props.sortBy).reverse() : content;
-    content = (this.props.limit)? content.slice(0, this.props.limit) : content;
+module.exports = class componentName extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { content: [], isLoading: true };
+    this._customListener = [];
+  }
+  componentWillMount() {
+    this.getDiscussions();
+  }
+
+  getDiscussions = (tag, limit, start_author, start_permlink) => {
+    limit = limit || 10;
+    let type = {
+      'trending': 'getDiscussionsByTrending',
+      'hot': 'getDiscussionsByHot',
+      'cashout': 'getDiscussionsByCashout',
+      'created': 'getDiscussionsByCreated',
+      'active': 'getDiscussionsByActive'
+    }
+    let currentType = type[this.props.path] || type['trending'];
+
+    api[currentType]({ tag, limit, start_author, start_permlink }, (err, result) => {
+      err && console.error('error while ' + currentType, JSON.stringify(err));
+      let content = _.concat(this.state.content, result);
+      this.setState({ content, isLoading: false });
+    });
+  }
+
+  componentDidMount() {
+    this.addScrollListener(this.refs.feedContainer);
+  }
+
+  addScrollListener(domNode) {
+    this.addCustomListener('scroll', domNode, this.scrollListener);
+    this.addCustomListener('resize', domNode, this.scrollListener);
+  }
+
+  addCustomListener(listenerType, target, listener) {
+    if (target) {
+      this._customListener.push({ listenerType, target, listener });
+      target.addEventListener(listenerType, listener);
+    }
+  }
+
+  componentWillUnmount() {
+    _.each(this._customListener, ({listenerType, target, listener}) => {
+      target.removeEventListener(listenerType, listener);
+    })
+  }
+
+  loadMore = () => {
+    let content = this.state.content;
+    if (content && content.length) {
+      let lastContent = content[content.length - 1];
+      this.getDiscussions("", 10, lastContent.author, lastContent.permlink);
+    }
+  }
+
+  scrollListener = (event) => {
+    var el = event.target;
+    let {scrollTop, scrollHeight, offsetHeight} = el;
+    let scrollRemain = scrollHeight - scrollTop - offsetHeight;
+    let scrollThreshold = window.innerHeight * 4;
+    if (scrollHeight && (scrollRemain < scrollThreshold) && !this.state.isLoading) {
+      this.setState({ isLoading: true }, () => {
+        this.loadMore();
+      });
+    }
+  }
+
+  render() {
+    let {content, isLoading} = this.state;
     return (
       <div className="grid">
-        <div className="grid-content">
+        <div className="grid-content" ref="feedContainer">
           <AddPost />
-          {_.size(content) > 0?
-            Object.keys(content).map(function(entry, key) {
-              return <Post key={key} entry={content[entry]} replies={this.props.replies} />;
-            }.bind(this)) : <Loading />
-          }
+          {content.map(function (entry, key) {
+            return <Post key={key} entry={entry} replies={this.props.replies} />;
+          }.bind(this)) }
+          {isLoading && <Loading />}
         </div>
       </div>
     );
   }
-});
+}
