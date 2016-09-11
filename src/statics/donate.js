f412697693b52f80d99cd2beab70e5cf6cce83e6
adcpm <fabien@bonustrack.co>
2016-09-12 02:48:03 +0700

    Update static pages

--- src/statics/donate.js
+++ src/statics/donate.js
@@ -14,6 +14,7 @@ module.exports = React.createClass({
 						<p><Link to="/@busy">@busy</Link></p>
 						<br/><br/>
 						<h1>Donors</h1>
+            <p><Link to="/@smooth">@smooth</Link></p>
 						<p><Link to="/@cass">@cass</Link></p>
 						<p><Link to="/@bhuz">@bhuz</Link> <Link to="/findandreward/@bhuz/find-and-reward-first-rewards-distribution">Find&Reward: First Rewards Distribution</Link></p>
 					</div>
--- src/statics/team.js
+++ src/statics/team.js
@@ -13,13 +13,12 @@ module.exports = React.createClass({
 						<h1>Team</h1>
 						<p><Link to="/@fabien">@fabien</Link> CTO</p>
 						<p><Link to="/@ekitcho">@ekitcho</Link> CHRO</p>
-						<p><Link to="/@kaptainkrayola">@kaptainkrayola</Link> Developer</p>
-						<p><Link to="/@heimindanger">@heimindanger</Link> Developer</p>
 						<p><Link to="/@clains">@clains</Link> CEO</p>
 						<p><Link to="/@spectral">@spectral</Link> CEO</p>
 						<p><Link to="/@nil1511">@nil1511</Link> Developer</p>
 						<p><Link to="/@picokernel">@picokernel</Link> Developer</p>
 						<p><Link to="/@svk">@svk</Link> Developer</p>
+            <p><Link to="/@p0o">@p0o</Link> Developer</p>
 						<p>You? <Link to="/@fabien">Contact us</Link></p>
 					</div>
 				</div>
