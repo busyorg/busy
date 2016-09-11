16ec213ddb38c8cb98903731c72354771d6e5967
p0o <pooria_az@ymail.com>
2016-09-12 01:11:33 +0800

    refactor post.js and NewPost.js into one component

--- src/newPost/NewPost.js
+++ src/newPost/NewPost.js
@@ -1,14 +1,29 @@
-var React = require("react"),
-	Header = require("./../app/header"),
-	Post = require('./../form/post');
+import React from 'react';
+import Header from './../app/header';
 
-module.exports = React.createClass({
-	render: function(){
-		return (
-			<div className="main-panel">
-				<Header menu="messages" />
-				<div className="container"><Post /></div>
-			</div>
-		);
-	}
-});
+export default () => {
+  return (
+    <div className="main-panel">
+      <Header menu="messages" />
+      <div className="container">
+        <div>
+          <fieldset className="form-group">
+            <label>Title</label>
+            <input autoFocus type="text" className="form-control form-control-lg" ref="title" />
+          </fieldset>
+          <fieldset className="form-group">
+            <label>Body</label>
+            <textarea className="form-control form-control-lg" ref="body"></textarea>
+          </fieldset>
+          <fieldset className="form-group">
+            <label>Category</label>
+            <input type="text" className="form-control form-control-lg" ref="category" />
+          </fieldset>
+        </div>
+        <div>
+          <a href="#">Cancel</a> <button type="submit" className="btn btn-primary btn-lg">Post</button>
+        </div>
+      </div>
+    </div>
+  );
+};
