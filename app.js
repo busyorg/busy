cab92f804e943c0ae95b7f7ee06bc84d3dc786b6
p0o <pooria_az@ymail.com>
2016-09-15 02:27:12 +0800

    remove node sass middleware

--- app.js
+++ app.js
@@ -40,12 +40,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
-}));
 
 app.use(session({ secret: 'YOUR_SECRET_HERE', resave: false,  saveUninitialized: false }));
 
