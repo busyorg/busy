b447131fd1854c75f790b55f1ccf260afef44441
yamadapc <tacla.yamada@gmail.com>
2016-09-16 16:48:03 -0300

    Remove trailing semicolon

--- src/post/newPost/NewPost.js
+++ src/post/newPost/NewPost.js
@@ -1,29 +1,52 @@
 import React from 'react';
 import Header from './../../app/header';
 
-export default () => {
+export default function NewPost() {
   return (
-    <div className="main-panel">
+    <div
+      className="main-panel"
+    >
       <Header menu="messages" />
+
       <div className="container">
         <div>
           <fieldset className="form-group">
             <label>Title</label>
-            <input autoFocus type="text" className="form-control form-control-lg" ref="title" />
+            <input
+              autoFocus
+              type="text"
+              className="form-control form-control-lg"
+              ref="title" />
           </fieldset>
+
           <fieldset className="form-group">
             <label>Body</label>
-            <textarea className="form-control form-control-lg" ref="body"></textarea>
+            <textarea
+              className="form-control form-control-lg"
+              ref="body"
+            />
           </fieldset>
+
           <fieldset className="form-group">
             <label>Category</label>
-            <input type="text" className="form-control form-control-lg" ref="category" />
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              ref="category"
+            />
           </fieldset>
         </div>
+
         <div>
-          <a href="#">Cancel</a> <button type="submit" className="btn btn-primary btn-lg">Post</button>
+          <a href="#">
+            Cancel
+          </a>
+
+          <button type="submit" className="btn btn-primary btn-lg">
+            Post
+          </button>
         </div>
       </div>
     </div>
   );
-};
+}
