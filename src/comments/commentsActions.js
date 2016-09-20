deccb31728902be68a59b3519191a4ade7a7e4e5
p0o <pooria_az@ymail.com>
2016-09-21 00:28:15 +0800

    add the thunk to load the data

--- src/comments/commentsActions.js
+++ src/comments/commentsActions.js
@@ -5,3 +5,18 @@ export const getCommentsWithoutAPICall = createAction(actionTypes.GET_COMMENTS);
 export const getCommentsSuccess = createAction(actionTypes.GET_COMMENTS_SUCCESS);
 export const getCommentsFail = createAction(actionTypes.GET_COMMENTS_FAIL);
 
+export const getComments = (postId) => {
+  return (dispatch, getState) => {
+    dispatch(getCommentsWithoutAPICall({ postId }));
+    // TODO(p0o): get author and permLink from state
+    // TODO(p0o): call the api to get the commentsData
+    const commentsData = [];
+
+    dispatch(
+      getCommentsSuccess({
+        postId,
+        commentsData
+      })
+    );
+  };
+};
