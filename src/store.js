37165223248bf669a49c6d5ec5c5e7f1022d1f4b
yamadapc <tacla.yamada@gmail.com>
2016-09-21 12:20:56 -0300

    Patch store so it's requireable on node
    So it doesn't depend on the `window` global.

--- src/store.js
+++ src/store.js
@@ -14,6 +14,6 @@ const reducers = combineReducers({
 
 export default createStore(
   reducers,
-  window.devToolsExtension && window.devToolsExtension(),
+  typeof window !== 'undefined' && window.devToolsExtension && window.devToolsExtension(),
   applyMiddleware(thunk)
 );
