b9a6141de1f570deb18a491d60722a8b9b90d1c1
p0o <pooria_az@ymail.com>
2016-09-24 01:09:00 +0800

    refactor apiHelper to execute function inside it

--- src/helpers/apiHelpers.js
+++ src/helpers/apiHelpers.js
@@ -2,22 +2,24 @@
  * Get the path from URL and the API object of steem and return the correct API call based on path
  * @param path - as in URL like 'trending'
  * @param API - the { api } from steem npm package
+ * @param query {Object} - the same query sending to Steem API
+ * @param callback - The same callback giving to Steem API
  * @returns {function}
  */
-export const getAPIFromPath = (path, API = {}) => {
+export const getDiscussionsFromAPI = function (path, API = {}, query, callback) {
   switch (path) {
     case 'feed':
-      return API.getDiscussionsByFeed;
+      return API.getDiscussionsByFeed(query, callback);
     case 'hot':
-      return API.getDiscussionsByHot;
+      return API.getDiscussionsByHot(query, callback);
     case 'cashout':
-      return API.getDiscussionsByCashout;
+      return API.getDiscussionsByCashout(query, callback);
     case 'crated':
-      return API.getDiscussionsByCreated;
+      return API.getDiscussionsByCreated(query, callback);
     case 'active':
-      return API.getDiscussionsByActive;
+      return API.getDiscussionsByActive(query, callback);
     case 'trending':
     default:
-      return API.getDiscussionsByTrending;
+      return API.getDiscussionsByTrending(query, callback);
   }
 };
