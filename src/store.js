0eeb08d14ebe5b4e38e5a42fdba924b2ff32dee8
p0o <pooria_az@ymail.com>
2016-09-24 01:10:07 +0800

    add posts and feed reducers to state

--- src/store.js
+++ src/store.js
@@ -4,12 +4,16 @@ import thunk from 'redux-thunk';
 import authReducers from './auth/authReducers';
 import appReducers, { headerReducer } from './app/appReducers';
 import commentsReducer from './comments/commentsReducer.js';
+import feedReducers from './feed/feedReducers';
+import postsReducers from './post/postsReducers';
 
 const reducers = combineReducers({
   app: appReducers,
   auth: authReducers,
   header: headerReducer,
-  comments: commentsReducer
+  comments: commentsReducer,
+  posts: postsReducers,
+  feed: feedReducers,
 });
 
 export default createStore(
