becbe040abb4687020abdc01072c924015a444ab
adcpm <fabien@bonustrack.co>
2016-09-24 03:56:08 +0700

    Remove heartbeat to websocket

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,59 +1,40 @@
 var React = require('react'),
-    ReactRedux = require('react-redux'),
-    api = require('./steemAPI'),
-    actions = require('./actions'),
-    Sidebar = require('./app/sidebar');
+  ReactRedux = require('react-redux'),
+  api = require('./steemAPI'),
+  actions = require('./actions'),
+  Sidebar = require('./app/sidebar');
 
 import * as authActions from './auth/authActions';
 
 var Wrapper = React.createClass({
-  componentWillMount: function(){
+  componentWillMount() {
     this.props.getConfig();
     this.props.login();
   },
-
-  componentDidMount: function(){
-    this.steemInterval = setInterval(function() {
-      api.getDynamicGlobalProperties(function(err, result) {});
-    }, 10000);
-  },
-
-  componentWillUnmount() {
-    if(window.clearInterval) {
-      clearInterval(this.steemInterval);
-    }
-  },
-
-  render: function() {
-      var className = (!this.props.app.sidebarIsVisible)? 'app-wrapper full-width' : 'app-wrapper';
-      return (
-        <div className={className}>
-          {this.props.app.sidebarIsVisible && <Sidebar />}
-          {this.props.children}
-        </div>
-      );
+  render() {
+    var className = (!this.props.app.sidebarIsVisible) ? 'app-wrapper full-width' : 'app-wrapper';
+    return (
+      <div className={className}>
+        {this.props.app.sidebarIsVisible && <Sidebar />}
+        {this.props.children}
+      </div>
+    );
   }
 });
 
-var mapStateToProps = function({ app }){
-  return {
-    app
-  };
+const mapStateToProps = function ({ app }) {
+  return { app };
 };
 
-var mapDispatchToProps = function(dispatch){
+const mapDispatchToProps = function (dispatch) {
   return {
     login: () => {
-      dispatch(
-        authActions.login()
-      );
+      dispatch(authActions.login());
     },
     getConfig: () => {
-      dispatch(
-        actions.getConfig()
-      );
+      dispatch(actions.getConfig());
     }
-  }
+  };
 };
 
 module.exports = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(Wrapper);
