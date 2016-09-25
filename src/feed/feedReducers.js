4e9b69ebc2ab2ea188b5af1c5a247def99d1c42c
p0o <pooria_az@ymail.com>
2016-09-25 14:01:13 +0800

    add loading to the state

--- src/feed/feedReducers.js
+++ src/feed/feedReducers.js
@@ -9,7 +9,20 @@ const initialState = {
   trending: {},
 };
 
-const feedTagItem = (state = [], action) => {
+const feedLoading = (state = false, action) => {
+  switch (action.type) {
+    case feedTypes.GET_FEED_CONTENT_SUCCESS:
+    case feedTypes.GET_MORE_FEED_CONTENT_SUCCESS:
+      return false;
+    case feedTypes.GET_FEED_CONTENT:
+    case feedTypes.GET_MORE_FEED_CONTENT:
+      return true;
+    default:
+      return state;
+  }
+};
+
+const feedIdsList = (state = [], action) => {
   switch (action.type) {
     case feedTypes.GET_FEED_CONTENT_SUCCESS:
       const postsIds = action.payload.postsData.map(post => post.id);
@@ -17,9 +30,10 @@ const feedTagItem = (state = [], action) => {
         ...postsIds,
       ];
     case feedTypes.GET_MORE_FEED_CONTENT_SUCCESS:
-      const morePostsIds = action.payload.postsData.map(post => post.id);
+      // first element of the array is the same element loaded in the previous chunk
+      const morePostsIds = action.payload.postsData.map(post => post.id).slice(1);
       // add data only if it doesn't exist
-      if (state[state.length - 1].id !== morePostsIds[morePostsIds.length - 1].id) {
+      if (state[state.length - 1] !== morePostsIds[morePostsIds.length - 1]) {
         return [
           ...state,
           ...morePostsIds,
@@ -30,13 +44,30 @@ const feedTagItem = (state = [], action) => {
   }
 };
 
-const feedPathItem = (state = {}, action) => {
+const feedCategoryItem = (state = {}, action) => {
   switch (action.type) {
+    case feedTypes.GET_FEED_CONTENT:
+    case feedTypes.GET_MORE_FEED_CONTENT:
+    case feedTypes.GET_FEED_CONTENT_SUCCESS:
+    case feedTypes.GET_MORE_FEED_CONTENT_SUCCESS:
+      return {
+        loading: feedLoading(undefined, action),
+        list: feedIdsList(state.list, action)
+      };
+    default:
+      return state;
+  }
+};
+
+const feedSortByItem = (state = {}, action) => {
+  switch (action.type) {
+    case feedTypes.GET_FEED_CONTENT:
+    case feedTypes.GET_MORE_FEED_CONTENT:
     case feedTypes.GET_FEED_CONTENT_SUCCESS:
     case feedTypes.GET_MORE_FEED_CONTENT_SUCCESS:
       return {
         ...state,
-        [action.payload.category]: feedTagItem(state[action.payload.category], action)
+        [action.payload.category]: feedCategoryItem(state[action.payload.category], action)
       };
     default:
       return state;
@@ -45,16 +76,17 @@ const feedPathItem = (state = {}, action) => {
 
 const feed = (state = initialState, action) => {
   switch (action.type) {
+    case feedTypes.GET_FEED_CONTENT:
+    case feedTypes.GET_MORE_FEED_CONTENT:
     case feedTypes.GET_FEED_CONTENT_SUCCESS:
     case feedTypes.GET_MORE_FEED_CONTENT_SUCCESS:
       return {
         ...state,
-        [action.payload.sortBy]: feedPathItem(state[action.payload.sortBy], action)
+        [action.payload.sortBy]: feedSortByItem(state[action.payload.sortBy], action)
       };
     default:
       return state;
   }
 };
-
 
 export default feed;
