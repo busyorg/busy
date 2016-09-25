bbe3f20d2ad464e377f688ff760b1f0da285c4a0
p0o <pooria_az@ymail.com>
2016-09-25 13:54:29 +0800

    write thunks to handle data fetching for feed

--- src/feed/feedActions.js
+++ src/feed/feedActions.js
@@ -1,14 +1,85 @@
 import { createAction } from 'redux-actions';
 import * as actionTypes from './feedActionTypes';
-import { api } from './../steemAPI';
+
+import { getDiscussionsFromAPI } from './../helpers/apiHelpers';
+import { getFeedFromState, getFeedContentFromState, getFeedLoadingFromState } from './../helpers/stateHelpers';
 
 export const getFeedContentWithoutAPI = createAction(actionTypes.GET_FEED_CONTENT);
 export const getFeedContentSuccess = createAction(actionTypes.GET_FEED_CONTENT_SUCCESS);
 
-export const getFeedContent = (path) => {
-  return (dispatch, getState) => {
-    dispatch(getFeedContentWithoutAPI());
+export const getMoreFeedContentWithoutAPI = createAction(actionTypes.GET_MORE_FEED_CONTENT);
+export const getMoreFeedContentSuccess = createAction(actionTypes.GET_MORE_FEED_CONTENT_SUCCESS);
 
+export const getFeedContent = ({ sortBy, category, limit }) => {
+  return (dispatch, getState) => {
+    if(getFeedFromState(sortBy, category, getState().feed).length) {
+      return;
+    }
+
+    dispatch(getFeedContentWithoutAPI({
+      sortBy: sortBy || 'trending',
+      category: category || 'all',
+    }));
+
+    getDiscussionsFromAPI(sortBy, window.steemAPI, {
+      tag: category,
+      limit,
+    },
+    (err, postsData) => {
+      if (err) {
+        console.error(`error while loading ${sortyBy}/${category}`, JSON.stringify(err));
+        return;
+      }
+
+      dispatch(
+        getFeedContentSuccess({
+          sortBy: sortBy || 'trending',
+          category: category || 'all',
+          postsData,
+        })
+      );
+    });
   };
 };
 
+export const getMoreFeedContent = ({ sortBy, category, limit }) => {
+  return (dispatch, getState) => {
+    const feedContent = getFeedContentFromState(
+        sortBy, category, getState().feed, getState().posts);
+    const isLoading = getFeedLoadingFromState(sortBy, category, getState().feed);
+
+    if (!feedContent.length || isLoading) {
+      // exit early
+      return;
+    }
+
+    dispatch(getMoreFeedContentWithoutAPI({
+      sortBy: sortBy || 'trending',
+      category: category || 'all',
+    }));
+
+    const startAuthor = feedContent[feedContent.length - 1].author;
+    const startPermlink = feedContent[feedContent.length - 1].permlink;
+
+    getDiscussionsFromAPI(sortBy, window.steemAPI, {
+      tag: category,
+      limit: limit + 1,
+      start_author: startAuthor,
+      start_permlink: startPermlink,
+    },
+    (err, postsData) => {
+      if (err) {
+        console.error(`error while loading ${sortyBy}/${category}`, JSON.stringify(err));
+        return;
+      }
+
+      dispatch(
+        getMoreFeedContentSuccess({
+          sortBy: sortBy || 'trending',
+          category: category || 'all',
+          postsData,
+        })
+      );
+    });
+  };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-eval-source-map',
+  devtool: 'eval-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
     path.join(__dirname, 'src/index.js')
