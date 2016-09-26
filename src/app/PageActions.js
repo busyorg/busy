32e492a065a57a14337f3043ab4509257e4755aa
yamadapc <tacla.yamada@gmail.com>
2016-09-26 10:10:03 -0300

    Add messages link to the floating actions bubble

--- src/app/PageActions.js
+++ src/app/PageActions.js
@@ -1,5 +1,5 @@
 var React = require('react'),
-  {Link} = require('react-router');
+    {Link} = require('react-router');
 
 module.exports = React.createClass({
   render: function() {
@@ -16,7 +16,7 @@ module.exports = React.createClass({
           {this.props.edit && <Link to="/profile/edit" className="trigger"><i className="icon icon-md material-icons">format_paint</i></Link>}
           {this.props.likes && <a href="#replies" className="trigger"><i className="icon icon-md material-icons">thumb_up</i></a>}
           {this.props.replies && <a href="#replies" className="trigger"><i className="icon icon-md material-icons">reply</i></a>}
-          {this.props.messages && <a href="#messages" className="trigger"><i className="icon icon-md material-icons">chat_bubble_outline</i></a>}
+          {this.props.messages && <Link to={`/messages/${channel}`} className="trigger"><i className="icon icon-md material-icons">chat_bubble_outline</i></Link>}
           {this.props.add && <Link to="/write" className="trigger"><i className="icon icon-md material-icons">add</i></Link>}
         </div>
       </div>
