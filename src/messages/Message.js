busyorg/busy

commit e762b07a5f8f82df0ae438104286185249478530

Separate Messages component into MessagesCategory and MessagesUser

src/messages/MessagesUser.js
@@ -1,0 +1,55 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import './Messages.scss';
+import Header from './../app/header';
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
+import actionDecorator from '../lib/actionDecorator';
+import { fetchChannelPresence, joinChannel } from '../common/messages/actions';
+
+class MessagesPage extends Component {
+  static propTypes = {
+    auth: PropTypes.object,
+    params: PropTypes.object,
+    channels: PropTypes.object,
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      messages: [],
+      text: '',
+    };
+  }
+  render() {
+    let username = this.props.params.username;
+    const channel = this.props.channels[username] || {
+      latest: [],
+      nmembers: 0,
+    };
+    return (
+      <div className="Messages main-panel">
+        <Header menu="messages">
+          <div><Link to={`/@${username}`}>@{username}</Link> <span>away</span></div>
+        </Header>
+        <div className="messages">
+          <MessageList messages={channel.latest} />
+          <MessageForm
+            channel={username}
+            username={this.props.auth.user && this.props.auth.user.name}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+MessagesPage = actionDecorator(fetchChannelPresence, joinChannel)(MessagesPage);
+MessagesPage = connect((state, { params }) => ({
+  auth: state.auth,
+  channels: state.messages.channels,
+}))(MessagesPage);
+
+export default MessagesPage;

src/messages/MessagesCategory.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 import './Messages.scss';
 import Header from './../app/header';
@@ -23,31 +24,20 @@ class MessagesPage extends Component {
     };
   }
   render() {
-    let channelName = this.props.params.channelName;
-
-    if (!channelName) {
-      channelName = 'general';
-    }
-    const channel = this.props.channels[channelName] || {
-      latest: [],
-      nmembers: 0,
-    };
-
+    let category = this.props.params.category;
+    const channel = this.props.channels[category] || {
+        latest: [],
+        nmembers: 0,
+      };
     return (
       <div className="Messages main-panel">
         <Header menu="messages">
-          <div>
-            <b>{channelName}</b>
-            {channelName.indexOf('@') !== 0 && (<span>{channel.nmembers} members</span>)}
-            {channelName.indexOf('@') === 0 && (<span>away</span>)}
-          </div>
+          <div><Link to={`/trending/${category}`}>#{category}</Link> <span>{channel.nmembers} online</span></div>
         </Header>
         <div className="messages">
-          <MessageList
-            messages={channel.latest}
-          />
+          <MessageList messages={channel.latest} />
           <MessageForm
-            channel={channelName}
+            channel={category}
             username={this.props.auth.user && this.props.auth.user.name}
           />
         </div>

src/routes.js
@@ -34,7 +34,8 @@ var React = require('react'),
   Responses = require('./feed/responses'),
   Votes = require('./feed/votes'),
   Write = require('./post/newPost/NewPost').default,
-  Messages = require('./messages/Messages').default;
+  MessagesUser = require('./messages/MessagesUser').default,
+  MessagesCategory = require('./messages/MessagesCategory').default;
 
 export default (
   <Route path="/" component={Wrapper}>
@@ -58,8 +59,8 @@ export default (
 
     <Route path="/write" component={Write} />
 
-    <Route path="/messages" component={Messages} />
-    <Route path="/messages/:channelName" component={Messages} />
+    <Route path="/messages/@:username" component={MessagesUser} />
+    <Route path="/messages/:category" component={MessagesCategory} />
 
     <Route path="/profile/edit" component={Edit} />
     <Route path="/@:name/posts" component={Posts} />

commit 0faf40142ea5b642fe7c96da8a4e95e1a515d966

Fix messages modeling that e762b07 broke
Private messages channels are `@username`.

src/messages/Message.js
@@ -9,7 +9,7 @@ export default ({ model }) => {
   return (
     <li
       className="Message message"
-      >
+    >
       <div className="media">
         <div className="container">
           <div className="media-left">
@@ -17,8 +17,8 @@ export default ({ model }) => {
               className="media-object"
               alt=""
               style={{ maxWidth: 'none', width: '50px' }}
-              src="https://placehold.it/50x50.png"
-              />
+              src={`https://img.busy6.com/@${model[0].senderUsername}`}
+            />
           </div>
 
           <div className="media-body">
@@ -32,7 +32,7 @@ export default ({ model }) => {
                   color: '#ccc',
                   backgroundColor: 'transparent'
                 }}
-                >
+              >
                 {humanSentAt}
               </span>
             </div>
@@ -51,7 +51,7 @@ export default ({ model }) => {
             <div className="container">
               <div
                 className="media-left"
-                >
+              >
                 <div
                   className="Message__timestamp"
                   style={{
@@ -75,4 +75,4 @@ export default ({ model }) => {
       }
     </li>
   );
-}
+};

src/messages/MessagesUser.js
@@ -13,7 +13,7 @@ class MessagesPage extends Component {
   static propTypes = {
     auth: PropTypes.object,
     params: PropTypes.shape({
-      channelName: PropTypes.string,
+      username: PropTypes.string,
     }),
     channels: PropTypes.object,
   };
@@ -27,20 +27,27 @@ class MessagesPage extends Component {
     };
   }
   render() {
-    let username = this.props.params.username;
-    const channel = this.props.channels[username] || {
+    const channelName = `@${this.props.params.username}`;
+    const channel = this.props.channels[channelName] || {
       latest: [],
       nmembers: 0,
     };
     return (
       <div className="Messages main-panel">
         <Header menu="messages">
-          <div><i className="icon icon-sm material-icons">star_border</i>  <Link to={`/@${username}`}>@{username}</Link> <span>away</span></div>
+          <div>
+            <i className="icon icon-sm material-icons">star_border</i>
+            <Link to={`/${channelName}`}>
+              {channelName}
+            </Link>
+            {' '}
+            <span>away</span>
+          </div>
         </Header>
         <div className="messages">
           <MessageList messages={channel.latest} />
           <MessageForm
-            channel={username}
+            channel={channelName}
             username={this.props.auth.user && this.props.auth.user.name}
           />
         </div>
@@ -50,7 +57,7 @@ class MessagesPage extends Component {
 }
 
 MessagesPage = actionDecorator(fetchChannelPresence, joinChannel)(MessagesPage);
-MessagesPage = connect((state, { params }) => ({
+MessagesPage = connect((state) => ({
   auth: state.auth,
   channels: state.messages.channels,
 }))(MessagesPage);