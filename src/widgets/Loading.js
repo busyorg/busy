4a5503a9c859848e1a130248d35d8d38416aaada
adcpm <fabien@bonustrack.co>
2016-10-01 11:37:24 +0700

    Refactor loading

--- src/widgets/Loading.js
+++ src/widgets/Loading.js
@@ -1,11 +1,13 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  render: function(){
-    var className = (this.props.color === 'white') ? 'loading-white' : 'loading';
-    className = className + ' align-center';
+  render() {
+    let className = (this.props.color === 'white') ? 'loading-white' : 'loading';
+    className += ' align-center';
     return (
-      <div><div className={className}><span>●</span><span>●</span><span>●</span></div></div>
+      <div><div className={className}>
+        <span>●</span><span>●</span><span>●</span></div>
+      </div>
     );
   }
 });
