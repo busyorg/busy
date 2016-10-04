5d17afcdc213ddb733023cf46031fb7891da4bd1
p0o <pooria_az@ymail.com>
2016-10-05 00:23:40 +0800

    store loaded user comments in the state

--- src/comments/commentsReducer.js
+++ src/comments/commentsReducer.js
@@ -1,4 +1,5 @@
 import * as commentsTypes from './commentsActionTypes';
+import * as userProfileTypes from './../user/userProfileActionTypes';
 
 const comments = (state = {}, action) => {
   switch(action.type) {
@@ -6,6 +7,20 @@ const comments = (state = {}, action) => {
       return {
         ...state,
         [action.payload.postId]: action.payload.commentsData
+      };
+    case userProfileTypes.GET_USER_COMMENTS_SUCCESS:
+      return {
+        ...state,
+        ...action.payload.content,
+      };
+    case userProfileTypes.GET_MORE_USER_COMMENTS_SUCCESS:
+      const commentsList = {};
+      action.payload.result.forEach(comment => {
+        commentsList[`${comment.author}/${comment.permlink}`] = comment;
+      });
+      return {
+        ...state,
+        ...commentsList,
       };
     default:
       return state;
