739961b1ec4756b95a6ea6078480558cb9e24459
p0o <pooria_az@ymail.com>
2016-10-10 13:40:13 +0800

    refactor posts to use Feed to show comments

--- src/user/posts.js
+++ src/user/posts.js
@@ -1,11 +1,38 @@
-let React = require('react'),
-  Page = require('./../feed/Page');
+import React, { Component } from 'react';
+import Feed from './../feed/Feed';
+import {
+  getFeedContentFromState,
+  getFeedLoadingFromState,
+  getFeedHasMoreFromState
+} from './../helpers/stateHelpers';
 
-module.exports = React.createClass({
+export default class Posts extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.props.getUserComments(this.props.params.name);
+  }
+
   render() {
-    const account = this.props.params.name;
+    const { feed, posts, getUserComments, getMoreUserComments, limit } = this.props;
+    const username = this.props.params.name;
+
+    const content = getFeedContentFromState('comments', username, feed, posts);
+    const isFetching = getFeedLoadingFromState('comments', username, feed);
+    const hasMore = getFeedHasMoreFromState('comments', username, feed);
+    const loadContentAction = () => getUserComments(username);
+    const loadMoreContentAction = () => getMoreUserComments(username, limit);
+
     return (
-      <Page account={account} path={'@' + account + '/posts'} sortBy="created" />
+      <Feed
+        content={content}
+        isFetching={isFetching}
+        hasMore={hasMore}
+        loadContent={loadContentAction}
+        loadMoreContent={loadMoreContentAction}
+      />
     );
   }
-});
+}
