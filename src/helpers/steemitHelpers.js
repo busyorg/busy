e73afe90ec0213367c797d010c71412557501fdd
p0o <pooria_az@ymail.com>
2016-10-13 23:38:01 +0800

    add helper to create permLink

---
+++ src/helpers/steemitHelpers.js
@@ -0,0 +1,22 @@
+/**
+ * This function is extracted from steemit.com source code and does the same tasks with some slight-
+ * adjustments to meet our needs. Refer to the main one in case of future problems:
+ * https://github.com/steemit/steemit.com/blob/edac65e307bffc23f763ed91cebcb4499223b356/app/redux/TransactionSaga.js#L340
+ *
+ */
+export const createCommentPermlink = (parentAuthor, parentPermlink) => {
+  let permlink;
+
+  // comments: re-parentauthor-parentpermlink-time
+  const timeStr = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '');
+  const newParentPermlink = parentPermlink.replace(/(-\d{8}t\d{9}z)/g, '');
+  permlink = `re-${parentAuthor}-${newParentPermlink}-${timeStr}`;
+
+  if (permlink.length > 255) {
+    // STEEMIT_MAX_PERMLINK_LENGTH
+    permlink = permlink.substring(permlink.length - 255, permlink.length);
+  }
+  // only letters numbers and dashes shall survive
+  permlink = permlink.toLowerCase().replace(/[^a-z0-9-]+/g, '');
+  return permlink;
+};
