34985b89d459d7e8416ff2b4fcdee08327acd4bf
yamadapc <tacla.yamada@gmail.com>
2016-10-14 10:49:42 -0300

    Add reblog feature on feed and single post page
    - Depends on https://github.com/adcpm/steemconnect.com/issues/68
    - Should keep track of whether the reblog state was changed in the store
      (change color if the current post was already reblogged)
    - Should move actions to the post actions

--- src/app/PageActions.js
+++ src/app/PageActions.js
@@ -37,6 +37,23 @@ export default class PageActions extends Component {
     );
   }
 
+  renderReblogButton() {
+    if (!this.props.reblog) {
+      return null;
+    }
+
+    return (
+      <a
+        className="trigger"
+        onClick={this.props.onClickReblog}
+      >
+        <i className="icon material-icons">
+          autorenew
+        </i>
+      </a>
+    );
+  }
+
   render() {
     let channel = '';
     if (this.props.params && this.props.params.name) {
@@ -50,11 +67,12 @@ export default class PageActions extends Component {
        <div className="triggers">
          {this.renderFollowButton()}
 
-          {this.props.edit && <Link to="/profile/edit" className="trigger"><i className="icon icon-md material-icons">format_paint</i></Link>}
-          {this.props.likes && <a href="#replies" className="trigger"><i className="icon icon-md material-icons">thumb_up</i></a>}
-          {this.props.replies && <a href="#replies" className="trigger"><i className="icon icon-md material-icons">reply</i></a>}
-          {this.props.messages && <Link to={`/messages/${channel}`} className="trigger"><i className="icon icon-md material-icons">chat_bubble_outline</i></Link>}
-          {this.props.add && <Link to="/write" className="trigger"><i className="icon icon-md material-icons">add</i></Link>}
+         {this.renderReblogButton()}
+         {this.props.edit && <Link to="/profile/edit" className="trigger"><i className="icon icon-md material-icons">format_paint</i></Link>}
+         {this.props.likes && <a href="#replies" className="trigger"><i className="icon icon-md material-icons">thumb_up</i></a>}
+         {this.props.replies && <a href="#replies" className="trigger"><i className="icon icon-md material-icons">reply</i></a>}
+         {this.props.messages && <Link to={`/messages/${channel}`} className="trigger"><i className="icon icon-md material-icons">chat_bubble_outline</i></Link>}
+         {this.props.add && <Link to="/write" className="trigger"><i className="icon icon-md material-icons">add</i></Link>}
         </div>
       </div>
     );
