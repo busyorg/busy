51b89481124b826de28b8c266b22824d6a7ee67a
adcpm <fabien@bonustrack.co>
2016-10-18 15:43:05 +0700

    Add bookmarks actions and actionTypes

---
+++ src/bookmarks/bookmarksActions.js
@@ -0,0 +1,16 @@
+import { createAction } from 'redux-actions';
+import { toggleBookmark as toggleBookmarkHelper } from '../helpers/localStorageHelpers';
+import * as actionTypes from './bookmarksActionTypes';
+
+export const toggleBookmarkRequest = createAction(actionTypes.TOGGLE_BOOKMARK);
+export const toggleBookmarkSuccess = createAction(actionTypes.TOGGLE_BOOKMARK_SUCCESS);
+
+export const toggleBookmark = (postId) => {
+  return (dispatch) => {
+    dispatch(toggleBookmarkRequest({ postId }));
+    toggleBookmarkHelper(postId);
+    dispatch(
+      toggleBookmarkSuccess({ postId })
+    );
+  };
+};
