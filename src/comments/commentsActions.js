8a4d96ca4d500e2d9901477149cfe955a2ec131d
p0o <pooria_az@ymail.com>
2016-10-19 01:42:12 +0800

    handling state for getMoreComments actions

--- src/comments/commentsActions.js
+++ src/comments/commentsActions.js
@@ -29,6 +29,47 @@ export const getComments = (postId) => {
   };
 };
 
+export const getMoreComments = (postId) => {
+  return (dispatch, getState, { steemAPI }) => {
+    const { comments } = getState();
+
+    const list = comments.lists[postId] ? comments.lists[postId].list : [];
+    const show = comments.lists[postId] ? comments.show : 1;
+    const getMoreLimit = 10;
+
+    if (list.length === 0) {
+      // No possible to load more
+      return;
+    }
+
+    if (list.length > show && list.length - show >= 10) {
+      // The next 10 comments are already loaded in state
+      dispatch({
+        type: actionTypes.GET_MORE_COMMENTS_SUCCESS,
+        payload: [],
+        meta: {
+          id: postId,
+        },
+      });
+      return;
+    }
+
+    const lastCommentId = list[list.length - 1];
+    const startAuthor = comments.comments[lastCommentId].author;
+    const startPermlink = comments.comments[lastCommentId].permlink;
+
+    dispatch({
+      type: actionTypes.GET_MORE_COMMENTS,
+      payload: {
+        promise: steemAPI.getRepliesByLastUpdateAsync(startAuthor, startPermlink, getMoreLimit)
+      },
+      meta: {
+        id: postId,
+      },
+    });
+  };
+};
+
 export const sendComment = () => {
   return (dispatch, getState) => {
     const { auth, comments } = getState();
@@ -70,4 +111,5 @@ export const sendComment = () => {
 
   };
 };
+
 
--- src/comments/commentsReducer.js
+++ src/comments/commentsReducer.js
@@ -16,12 +16,70 @@ const initialCommentingDraftItem = {
   body: null,
 };
 
+const commentsList = (state = {}, action) => {
+  switch (action.type) {
+    case commentsTypes.GET_COMMENTS_START:
+      return {
+        ...state,
+        isFetching: true,
+        list: [],
+        show: 1,
+      };
+    case commentsTypes.GET_COMMENTS_SUCCESS:
+      const list = action.payload.map(comment => comment.id);
+      return {
+        ...state,
+        list,
+        isFetching: false,
+      };
+    case commentsTypes.GET_MORE_COMMENTS_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case commentsTypes.GET_MORE_COMMENTS_SUCCESS:
+      const moreList = action.payload.map(comment => comment.id);
+      const show = state.show === 1? 10 : state.show + 10;
+      return {
+        ...state,
+        isFetching: true,
+        list: [
+          ...state.list,
+          moreList,
+        ],
+        show,
+      };
+    default:
+      return state;
+  }
+};
+
+const commentsLists = (state = {}, action) => {
+  switch (action.type) {
+    case commentsTypes.GET_COMMENTS_START:
+    case commentsTypes.GET_COMMENTS_SUCCESS:
+    case commentsTypes.GET_MORE_COMMENTS_START:
+    case commentsTypes.GET_MORE_COMMENTS_SUCCESS:
+      return {
+        ...state,
+        [action.meta.id]: commentsList(state[action.meta.id], action),
+      };
+    default:
+      return state;
+  }
+};
+
 const commentsData = (state = {}, action) => {
   switch (action.type) {
     case commentsTypes.GET_COMMENTS_SUCCESS:
+    case commentsTypes.GET_MORE_COMMENTS_SUCCESS:
+      const data = {};
+      action.payload.forEach((comment) => {
+        data[comment.id] = comment;
+      });
       return {
         ...state,
-        [action.payload.postId]: action.payload.commentsData
+        ...data,
       };
     case userProfileTypes.GET_USER_COMMENTS_SUCCESS:
       const commentsList = {};
@@ -84,12 +142,16 @@ const commentingDraft = (state = {}, action) => {
 
 const comments = (state = initialState, action) => {
   switch (action.type) {
+    case commentsTypes.GET_COMMENTS_START:
     case commentsTypes.GET_COMMENTS_SUCCESS:
     case userProfileTypes.GET_USER_COMMENTS_SUCCESS:
     case userProfileTypes.GET_MORE_USER_COMMENTS_SUCCESS:
+    case commentsTypes.GET_MORE_COMMENTS_START:
+    case commentsTypes.GET_MORE_COMMENTS_SUCCESS:
       return {
         ...state,
-        comments: commentsData(state.comments, action)
+        comments: commentsData(state.comments, action),
+        lists: commentsLists(state.lists, action),
       };
     case commentsTypes.OPEN_COMMENTING_DRAFT:
     case commentsTypes.UPDATE_COMMENTING_DRAFT:
