b92421c9660546eb25522f53a6f7909d4964587c
adcpm <fabien@bonustrack.co>
2016-10-18 16:45:35 +0700

    Update reducer

--- src/bookmarks/bookmarksReducer.js
+++ src/bookmarks/bookmarksReducer.js
@@ -1,8 +1,14 @@
+import _ from 'lodash';
 import * as bookmarksTypes from './bookmarksActionTypes';
 
 const bookmarks = (state = {}, action) => {
   switch (action.type) {
-    case bookmarksTypes.TOGGLE_BOOKMARK:
+    case bookmarksTypes.TOGGLE_BOOKMARK_SUCCESS:
+      if (_.has(state, action.payload.postId)) {
+        delete state[action.payload.postId];
+      } else {
+        state[action.payload.postId] = {};
+      }
       return state;
     default:
       return state;
