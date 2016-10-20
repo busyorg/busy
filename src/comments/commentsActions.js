0ff4a9233374dff7b976fbf25e4b22dcfc8308bf
p0o <pooria_az@ymail.com>
2016-10-20 23:52:10 +0800

    make the action work with getState API

--- src/comments/commentsActions.js
+++ src/comments/commentsActions.js
@@ -10,17 +10,63 @@ export const openCommentingDraft = createAction(actionTypes.OPEN_COMMENTING_DRAF
 export const updateCommentingDraft = createAction(actionTypes.UPDATE_COMMENTING_DRAFT);
 export const closeCommentingDraft = createAction(actionTypes.CLOSE_COMMENTING_DRAFT);
 
+export const showMoreComments = createAction(
+  actionTypes.SHOW_MORE_COMMENTS,
+  () => null,
+  meta => ({ id: meta, })
+);
+
+/**
+ * Will recursively create a tree of comments with their children like this:
+ * { id: '2.1.85555', children: [ { id: '2.1.55444', children: [...] ], }
+ * @param commentKey - the key of rootComment we want to re-structure
+ * @param allComments - all comments received from getState api (usually res.content)
+ * @returns {{id: *, children: *}}
+ */
+const nestCommentsChildren = (commentKey, allComments) => {
+  const currentComment = allComments[commentKey];
+  return {
+    id: currentComment.id,
+    children: currentComment.children > 0
+      ? currentComment.replies.map(childCommentKey => nestCommentsChildren(childCommentKey, allComments))
+      : {},
+  };
+};
+
+/**
+ * Will restructure and sort comments with only id and its children
+ * @param apiRes - The result of getState for an article full path with steemAPI
+ */
+const sortCommentsList = (apiRes) => {
+  const rootComments = Object.entries(apiRes.content).filter(([key, comment]) => {
+    return comment.id === comment.root_comment;
+  });
+  return rootComments.map(([key, comment]) => {
+    const nestedComments = [];
+    if(comment.children > 0) {
+      nestedComments.push(nestCommentsChildren(key, apiRes.content));
+    }
+
+    return nestedComments;
+  });
+};
+
 
 export const getComments = (postId) => {
   return (dispatch, getState, { steemAPI }) => {
     const { posts } = getState();
 
-    const { author, permlink } = posts[postId];
+    const { author, permlink, category } = posts[postId];
 
     dispatch({
       type: actionTypes.GET_COMMENTS,
       payload: {
-        promise: steemAPI.getContentRepliesAsync(author, permlink)
+        promise: steemAPI.getStateAsync(`/${category}/@${author}/${permlink}`).then((apiRes) => {
+          return {
+            list: sortCommentsList(apiRes),
+            content: apiRes.content,
+          };
+        }),
       },
       meta: {
         id: postId,
@@ -34,23 +80,16 @@ export const getMoreComments = (postId) => {
     const { comments } = getState();
 
     const list = comments.lists[postId] ? comments.lists[postId].list : [];
-    const show = comments.lists[postId] ? comments.show : 1;
-    const getMoreLimit = 10;
+    const show = comments.lists[postId] ? comments.lists[postId].show : 1;
 
-    if (list.length === 0) {
-      // No possible to load more
+    if (list.length === 0 || !comments.lists[postId].hasMore) {
+      // Not possible to load more
       return;
     }
 
-    if (list.length > show && list.length - show >= 10) {
+    if (list.length > show + 10) {
       // The next 10 comments are already loaded in state
-      dispatch({
-        type: actionTypes.GET_MORE_COMMENTS_SUCCESS,
-        payload: [],
-        meta: {
-          id: postId,
-        },
-      });
+      dispatch(showMoreComments(postId));
       return;
     }
 
@@ -61,12 +100,15 @@ export const getMoreComments = (postId) => {
     dispatch({
       type: actionTypes.GET_MORE_COMMENTS,
       payload: {
-        promise: steemAPI.getRepliesByLastUpdateAsync(startAuthor, startPermlink, getMoreLimit)
+        promise: steemAPI.getRepliesByLastUpdateAsync(startAuthor, startPermlink, 10),
       },
       meta: {
         id: postId,
       },
-    });
+    }).then(() => dispatch(
+        showMoreComments(postId)
+      )
+    );
   };
 };
 
