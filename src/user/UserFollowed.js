d4c63dc480ce94a7a57f929bbd786e1473e2992e
adcpm <fabien@bonustrack.co>
2016-10-20 18:51:30 +0700

    Refactor followed

---
+++ src/user/UserFollowed.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import api from './../steemAPI';
+import Loading from './../widgets/Loading';
+import UserList from './UserList';
+
+module.exports = React.createClass({
+  componentWillMount() {
+    this.setState({ users: [] });
+    api.getFollowing(this.props.params.name, 0, 'blog', 100, (err, following) => {
+      this.setState({ users: following });
+    });
+  },
+  render() {
+    return (
+      <div className="container">
+        <center className="users">
+          {this.state.users.length > 0 && <ul>
+            {this.state.users.map((user, key) => {
+              return (<UserList username={user.following} key={key} />);
+            })}
+          </ul>}
+          {this.state.users.length === 0 && <Loading />}
+        </center>
+      </div>
+    );
+  }
+});
