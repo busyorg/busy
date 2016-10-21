255ed14bf057c9c8003313d7522ba39cac2a34ff
adcpm <fabien@bonustrack.co>
2016-10-21 12:19:36 +0700

    Remove sub menu from header

--- src/app/header.js
+++ src/app/header.js
@@ -1,17 +1,10 @@
 let React = require('react'),
   ReactRedux = require('react-redux'),
   actions = require('./../actions'),
-  MenuFeed = require('./Menu/MenuFeed'),
-  MenuUser = require('./Menu/MenuUser'),
-  MenuAbout = require('./Menu/MenuAbout'),
-  MenuSteem = require('./Menu/MenuSteem'),
   Link = require('react-router').Link;
 
 const Header = React.createClass({
   render() {
-    const menu = this.props.menu || this.props.header.menu;
-    const username = this.props.account || this.props.auth.user.name;
-    const category = (this.props.category) ? `/${this.props.category}` : '';
     return (
       <header>
         <div className="top-nav">
@@ -22,29 +15,20 @@ const Header = React.createClass({
           </div>
           <a><i className="icon icon-md icon-menu material-icons">notifications</i></a>
         </div>
-        {menu === 'primary' && <MenuFeed category={category} />}
-        {menu === 'secondary' && <MenuUser username={username} />}
-        {menu === 'about' && <MenuAbout />}
-        {menu === 'steem' && <MenuSteem />}
         {this.props.children && <div className="app-nav">{this.props.children}</div>}
       </header>
     );
   }
 });
 
-const mapStateToProps = function (state) {
-  return {
-    app: state.app,
-    auth: state.auth,
-    header: state.header
-  };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = function (dispatch) {
+const mapDispatchToProps = (dispatch) => {
   return {
     showSidebar() { dispatch(actions.showSidebar()); },
     hideSidebar() { dispatch(actions.hideSidebar()); },
-    setMenu(menu) { dispatch(actions.setMenu(menu)); }
   };
 };
 
