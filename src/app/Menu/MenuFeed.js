7af6c2261ecd031e20d7f513399c97c1a2931a43
adcpm <fabien@bonustrack.co>
2016-10-22 06:01:52 +0700

    Revert change on MenuFeed

--- src/app/Menu/MenuFeed.js
+++ src/app/Menu/MenuFeed.js
@@ -5,20 +5,17 @@ const HeaderTriggers = React.createClass({
   render() {
     const category = this.props.category;
     return (
-      <div className="sort-nav">
-        <ul>
-          <li><Link to={`/trending${category}`} onlyActiveOnIndex activeClassName="active"><i className="icon icon-md material-icons">show_chart</i></Link></li>
-          <li><Link to={`/hot${category}`} activeClassName="active"><i className="icon icon-md material-icons">whatshot</i></Link></li>
-          <li><Link to={`/cashout${category}`} activeClassName="active"><i className="icon icon-md material-icons">schedule</i></Link></li>
-          <li><Link to={`/created${category}`} activeClassName="active"><i className="icon icon-md material-icons">fiber_new</i></Link></li>
-          <li><Link to={`/active${category}`} activeClassName="active"><i className="icon icon-md material-icons">track_changes</i></Link></li>
-          <li className="hide"><Link to={`/responses${category}`} activeClassName="active"><i className="icon icon-md material-icons">comment</i></Link></li>
-          <li className="hide"><Link to={`/votes${category}`} activeClassName="active"><i className="icon icon-md material-icons">gavel</i></Link></li>
-        </ul>
-      </div>
+      <ul className="app-nav">
+        <li><Link to={`/trending${category}`} onlyActiveOnIndex activeClassName="active"><i className="icon icon-md material-icons">show_chart</i><span className="hidden-xs"> Trending</span></Link></li>
+        <li><Link to={`/hot${category}`} activeClassName="active"><i className="icon icon-md material-icons">whatshot</i><span className="hidden-xs"> Hot</span></Link></li>
+        <li><Link to={`/cashout${category}`} activeClassName="active"><i className="icon icon-md material-icons">schedule</i><span className="hidden-xs"> Payout Time</span></Link></li>
+        <li><Link to={`/created${category}`} activeClassName="active"><i className="icon icon-md material-icons">fiber_new</i><span className="hidden-xs"> New</span></Link></li>
+        <li><Link to={`/active${category}`} activeClassName="active"><i className="icon icon-md material-icons">track_changes</i><span className="hidden-xs"> Active</span></Link></li>
+        <li className="hide"><Link to={`/responses${category}`} activeClassName="active"><i className="icon icon-md material-icons">comment</i><span className="hidden-xs"> Responses</span></Link></li>
+        <li className="hide"><Link to={`/votes${category}`} activeClassName="active"><i className="icon icon-md material-icons">gavel</i><span className="hidden-xs"> Popular</span></Link></li>
+      </ul>
     );
   }
 });
 
 module.exports = HeaderTriggers;
-
