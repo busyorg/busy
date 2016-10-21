bc537641e895073551a8c43a2e86efd8f9b3229d
p0o <pooria_az@ymail.com>
2016-10-21 19:35:43 +0800

    import bindActionCreators

--- src/feed/Page.js
+++ src/feed/Page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Header from './../app/header';
 import PageActions from './../app/PageActions';
