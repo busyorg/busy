a707369a96aa892764775146c0ca081c9b95c712
Nilesh Suthar <nileshsuthar@live.in>
2016-10-23 21:29:05 +0530

    Remove token endPoint; need to redeploy.

--- routes/front.js
+++ routes/front.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/token', function (req, res) {
-  res.redirect('/');
-});
-
 router.get('/*', function (req, res, next) {
   res.render('user/dashboard', { layout: 'user', title: 'Busy' });
 });
