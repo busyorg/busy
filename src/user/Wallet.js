e95053b4b0e8d69a17153c821dc0bf9398e7aba8
adcpm <fabien@bonustrack.co>
2016-10-23 09:17:41 +0700

    Update wallet text and add loading

--- src/user/Wallet.js
+++ src/user/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import numeral from 'numeral';
 import { formatter } from 'steem';
 import api from '../steemAPI';
+import Loading from '../widgets/Loading';
 
 module.exports = React.createClass({
   componentWillMount() {
@@ -35,15 +36,26 @@ module.exports = React.createClass({
     }
     return (
       <div className="container ptm">
-        <h1>Wallet</h1>
-        {!_.isEmpty(this.state.feedPrice) && !_.isEmpty(account) &&
+        {(!_.isEmpty(this.state.feedPrice) && !_.isEmpty(account)) ?
           <ul>
-            <li><h3>1 Steem <span className="pull-right">{numeral(base).format('$0,0.00')}</span></h3></li>
-            <li><h3>Steem <span className="pull-right">{numeral(account.balance).format('0,0.00')}</span></h3></li>
-            <li><h3>Steem Power <span className="pull-right">{numeral(power).format('0,0.00')}</span></h3></li>
-            <li><h3>Steem Dollars <span className="pull-right">{numeral(account.sbd_balance).format('0,0.00')}</span></h3></li>
-            <li><h3>Steem Dollars <span className="pull-right">{numeral(dollar).format('$0,0.00')}</span></h3></li>
-          </ul>
+            <li>
+              <h3>Steem <span className="pull-right">{numeral(account.balance).format('0,0.00')} STEEM</span></h3>
+              <p>Tradeable tokens that may be transferred anywhere at anytime.<br />
+                Steem can be converted to Steem Power in a process called powering up.</p>
+            </li>
+            <li>
+              <h3>Steem Power <span className="pull-right">{numeral(power).format('0,0.00')} STEEM</span></h3>
+              <p>Influence tokens which give you more control over post payouts and allow you to earn on curation rewards.</p>
+            </li>
+            <li>
+              <h3>Steem Dollars <span className="pull-right">{numeral(account.sbd_balance).format('$0,0.00')}</span></h3>
+              <p>Tokens worth about $1.00 of STEEM, currently collecting 10% APR.</p>
+            </li>
+            <li>
+              <h3>Estimated Account Value <span className="pull-right">{numeral(dollar).format('$0,0.00')}</span></h3>
+              <p>The estimated value is based on a 7 day average value of Steem in US Dollars.</p>
+            </li>
+          </ul> : <Loading />
         }
       </div>
     );
