8fbb875ae3d02db5eb759c1006ef474faf89a199
adcpm <fabien@bonustrack.co>
2016-10-28 00:31:01 +0700

    Improve render

--- src/categories/Category.js
+++ src/categories/Category.js
@@ -11,11 +11,11 @@ export default class Category extends Component {
         <div className="block">
           <h1>
             <Link to={`/trending/${category.name}`}>#{category.name}</Link>
-            <i className="icon icon-lg material-icons">star_border</i>
+            {' '}<i className="icon icon-lg material-icons">star_border</i>
           </h1>
           <h2>
             <i className="icon icon-lg material-icons">library_books</i> {numeral(category.discussions).format('0,0')}
-            , <i className="icon icon-lg material-icons">attach_money</i> {numeral(category.total_payouts).format('$0,0')}
+            {' '}<i className="icon icon-lg material-icons">attach_money</i> {numeral(category.total_payouts).format('$0,0')}
           </h2>
           <p>Last update {moment(category.last_update).fromNow()}</p>
         </div>
