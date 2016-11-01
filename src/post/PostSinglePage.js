b5464a2ea380dd9b08c9a6525f83dad199972db1
p0o <pooria_az@ymail.com>
2016-11-01 22:24:24 +0800

    refactor PostSinglePage into an stateless component

--- src/post/PostSinglePage.js
+++ src/post/PostSinglePage.js
@@ -1,136 +1,64 @@
 import React from 'react';
-import _ from 'lodash';
 import moment from 'moment';
 import { Link } from 'react-router';
-import { connect } from 'react-redux';
-
-import Promise from 'bluebird';
-import request from 'superagent';
-
-import api from './../steemAPI';
 import Header from '../app/Header';
 import Loading from './../widgets/Loading';
 import Body from './body';
 import Comments from './../comments/Comments';
 import TriggerPost from './../app/Trigger/TriggerPost';
 
-class PostSinglePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: {},
-    };
+export default PostSinglePage = ({ content, onClickReblog }) => {
+  if (!content.author) {
+    return <Loading />;
   }
 
-  componentWillMount() {
-    this.setState({ content: {} });
-    api.getContent(this.props.params.author, this.props.params.permlink, (err, content) => {
-      this.setState({ content });
-    });
-  }
-
-  hasReblog() {
-    const { content } = this.state;
-    if (!content) return false;
-    const { isAuthenticated, user } = this.props.auth;
-    if (!isAuthenticated || !user) return false;
-    return content.author !== user.name;
-  }
-
-  onClickReblog = (e) => {
-    if (e && e.preventDefault) e.preventDefault();
-    const { content } = this.state;
-
-    if (!content) {
-      // TODO wait
-      return;
-    }
-
-    this.props.reblog({
-      account: content.author, // TODO What is this
-      author: content.author,
-      permlink: content.permlink,
-    });
-  };
-
-  renderContent() {
-    const { content } = this.state;
-    if (!_.has(content, 'author')) {
-      return <Loading />;
-    }
-
-    return (
-      <div className="container">
-        <div
-          style={{
-            overflow: 'hidden',
-            height: '40px',
-            lineHeight: '40px',
-            marginTop: '15px',
-          }}
-        >
-          <Link to={'/@' + content.author}>
-            <span className="avatar avatar-sm pull-left">
-              <img src={'https://img.busy6.com/@' + content.author} width="40" height="40" />
-            </span>
-            <span className="pull-left mls">
-              @{content.author}
-            </span>
-          </Link>
-
-          <span className="pull-right">
-            {moment(content.created).fromNow()}
-            <a><i className="icon icon-md material-icons">bookmark_border</i></a>
-          </span>
-        </div>
-
-        <div className="single-content">
-          <h1 className="mvl">{content.title}</h1>
-          <Body body={content.body} jsonMetadata={content.json_metadata} />
-        </div>
-
-        {content.children > 0 && (
-          <div className="single-replies">
-            <h2>Comments</h2>
-            <Comments postId={content.id} />
-          </div>
-        )}
-      </div>
-    );
-  }
-
-  render() {
-    return (
+  return (
+    <div>
+      <TriggerPost
+        onClickReblog={onClickReblog}
+      />
+      <Header />
       <div className="main-panel">
-        <TriggerPost
-          params={this.props.params}
-          onClickReblog={this.onClickReblog}
-        />
-        <Header />
         <div className="single">
-          {this.renderContent()}
+          <div className="container">
+            <div
+              style={{
+                  overflow: 'hidden',
+                  height: '40px',
+                  lineHeight: '40px',
+                  marginTop: '15px',
+                }}
+            >
+              <Link to={'/@' + content.author}>
+              <span className="avatar avatar-sm pull-left">
+                <img src={'https://img.busy6.com/@' + content.author} width="40" height="40" />
+              </span>
+              <span className="pull-left mls">
+                @{content.author}
+              </span>
+              </Link>
+
+            <span className="pull-right">
+              {moment(content.created).fromNow()}
+              <a><i className="icon icon-md material-icons">bookmark_border</i></a>
+            </span>
+            </div>
+
+            <div className="single-content">
+              <h1 className="mvl">{content.title}</h1>
+              <Body body={content.body} jsonMetadata={content.json_metadata} />
+            </div>
+
+            { content.children > 0 && (
+              <div className="single-replies">
+                <h2>Comments</h2>
+                <Comments postId={content.id} />
+              </div>
+             )
+            }
+          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-Promise.promisifyAll(request.Request.prototype);
-
-function reblog(query) {
-  return (dispatch) => dispatch({
-    type: 'REBLOG',
-    payload: {
-      promise: request.get(`${process.env.STEEMCONNECT_API_HOST}/reblog`)
-        .query(query)
-        .withCredentials()
-        .endAsync(),
-    }
-  });
-}
-
-PostSinglePage = connect(() => ({}), {
-  reblog,
-})(PostSinglePage);
-
-export default PostSinglePage;
