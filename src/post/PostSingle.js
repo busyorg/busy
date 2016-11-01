23be08874e28f164981400b7154e5e4b3a28f35e
p0o <pooria_az@ymail.com>
2016-11-02 00:27:49 +0800

    create PostSingle as a container

---
+++ src/post/PostSingle.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import moment from 'moment';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import api from './../steemAPI';
+import Header from '../app/Header';
+import Loading from './../widgets/Loading';
+import Body from './body';
+import Comments from './../comments/Comments';
+import TriggerPost from './../app/Trigger/TriggerPost';
+import * as postActions from './postActions';
+import PostSingleModal from './PostSingleModal';
+import PostSinglePage from './PostSinglePage';
+
+@connect(
+  () => ({}),
+  dispatch => ({
+    reblog: (q) => dispatch(postActions.reblog(q))
+  })
+)
+class PostSingle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: {},
+    };
+  }
+
+  componentWillMount() {
+    if (!this.props.modal) {
+      // TODO(p0o): refactor this later with redux
+      api.getContent(this.props.params.author, this.props.params.permlink, (err, content) => {
+        this.setState({content});
+      });
+    }
+  }
+
+  handleReblog = (e) => {
+    if (e && e.preventDefault) e.preventDefault();
+    const content = this.props.modal ? this.props.content : this.state.content;
+
+    if (!content) {
+      // TODO wait
+      return;
+    }
+
+    this.props.reblog({
+      account: content.author, // TODO What is this
+      author: content.author,
+      permlink: content.permlink,
+    });
+  };
+
+  render() {
+    const { modal } = this.props;
+    const content = this.props.modal ? this.props.content : this.state.content;
+
+    return (
+      <div>
+        { modal
+          ? <PostSingleModal content={content} onClickReblog={this.handleReblog} />
+          : <PostSinglePage content={content} onClickReblog={this.handleReblog} />
+        }
+      </div>
+    );
+  }
+}
