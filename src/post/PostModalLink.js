0441044bc30c05766d469f4e2f81f96392571939
p0o <pooria_az@ymail.com>
2016-11-03 00:58:22 +0800

    move history manipulation to PostSingleModal

--- src/post/PostModalLink.js
+++ src/post/PostModalLink.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import PostSingleModal from './PostSingleModal';
 
-const handleClick = (e, onClick, post, postPath) => {
+const handleClick = (e, onClick) => {
   e.preventDefault();
-  if (window.history) {
-    window.history.pushState({}, post.title, postPath);
-  }
   onClick();
 };
 
@@ -13,7 +10,7 @@ const PostModalLink = ({ post, onClick, children }) => {
   const postPath = `/${post.parent_permlink}/@${post.author}/${post.permlink}`;
 
   return (
-    <a href={postPath} onClick={(e) => handleClick(e, onClick, post, postPath)}>
+    <a href={postPath} onClick={(e) => handleClick(e, onClick)}>
       { children }
     </a>
   );
--- src/post/PostSingleModal.js
+++ src/post/PostSingleModal.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 import './PostSingleModal.scss';
 
-const PostSingleModal = ({ content, onClickReblog }) => {
-  return (
-    <div className="PostSingleModal">
-      <h1 className="mvl">{content.title}</h1>
-      @{content.author}
-      <p>
-        {content.body}
-      </p>
-    </div>
-  );
-};
+@withRouter
+export default class PostSingleModal extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default PostSingleModal;
+  componentDidMount() {
+    if (window.history) {
+      // manipulate address bar to show the article's address
+      const { content } = this.props;
+      const postPath = `/${content.parent_permlink}/@${content.author}/${content.permlink}`;
+      window.history.pushState({}, content.title, postPath);
+    }
+  }
+
+  handleClose = (e) => {
+    if (window.history) {
+      window.history.back();
+    }
+  };
+
+  render() {
+    const { content, onClickReblog } = this.props;
+
+    return (
+      <div className="PostSingleModal">
+        <h1 className="mvl">{content.title}</h1>
+        <button onClick={this.handleClose}>Close</button>
+        @{content.author}
+        <p>
+          {content.body}
+        </p>
+      </div>
+    );
+  }
+}
