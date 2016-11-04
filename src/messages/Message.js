17bbff341ed626addc2a50a6846403d5302f9a50
yamadapc <tacla.yamada@gmail.com>
2016-11-04 12:15:40 -0200

    Lint Message.js

--- src/messages/Message.js
+++ src/messages/Message.js
@@ -1,70 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 import moment from 'moment';
-import { connect } from 'react-redux';
 
-class Message extends Component {
-  render() {
-    const { model } = this.props;
-    const sentAt = model[0].sentAt;
-    const senderUsername = (model[0].senderUsername || model[0].sentBy);
-    const humanSentAt = moment(sentAt).format('hh:mm');
+function Message(props) {
+  const { model } = props;
+  const sentAt = model[0].sentAt;
+  const senderUsername = (model[0].senderUsername || model[0].sentBy);
+  const humanSentAt = moment(sentAt).format('hh:mm');
 
-    return (
-      <li className="Message message">
-        <div className="media" data-uuid={model[0].uuid}>
-          <div className="container">
-            <div className="media-left">
-              <div className="avatar avatar-md">
-                <img src={`https://img.busy.org/@${senderUsername}`} />
-              </div>
-            </div>
-            <div className="media-body">
-              <div className="media-heading">
-                <b>@{senderUsername}</b>
-                <span
-                  style={{
-                    textTransform: 'uppercase',
-                    paddingLeft: '10px',
-                    color: '#ccc',
-                    backgroundColor: 'transparent'
-                  }}
-                >
-                  {humanSentAt}
-                </span>
-              </div>
-              <p>
-                {model[0].text}
-              </p>
+  return (
+    <li className="Message message">
+      <div className="media" data-uuid={model[0].uuid}>
+        <div className="container">
+          <div className="media-left">
+            <div className="avatar avatar-md">
+              <img
+                role="presentation"
+                src={`https://img.busy.org/@${senderUsername}`}
+              />
             </div>
           </div>
+          <div className="media-body">
+            <div className="media-heading">
+              <b>@{senderUsername}</b>
+              <span
+                style={{
+                  textTransform: 'uppercase',
+                  paddingLeft: '10px',
+                  color: '#ccc',
+                  backgroundColor: 'transparent'
+                }}
+              >
+                {humanSentAt}
+              </span>
+            </div>
+            <p>
+              {model[0].text}
+            </p>
+          </div>
         </div>
+      </div>
 
-        {
-          model.slice(1).map(({ uuid, text }, i) => (
-            <div className="media" key={i} data-uuid={uuid}>
-              <div className="container">
-                <div className="media-left">
-                  <div
-                    className="Message__timestamp"
-                    style={{
-                      width: '50px',
-                      textTransform: 'uppercase',
-                      color: '#ccc'
-                    }}
-                  >
-                    {moment(sentAt).format('hh:mm')}
-                  </div>
-                </div>
-                <div className="media-body">
-                  <p>{text}</p>
+      {
+        model.slice(1).map(({ uuid, text }, i) => (
+          <div className="media" key={i} data-uuid={uuid}>
+            <div className="container">
+              <div className="media-left">
+                <div
+                  className="Message__timestamp"
+                  style={{
+                    width: '50px',
+                    textTransform: 'uppercase',
+                    color: '#ccc'
+                  }}
+                >
+                  {moment(sentAt).format('hh:mm')}
                 </div>
               </div>
+              <div className="media-body">
+                <p>{text}</p>
+              </div>
             </div>
-          ))
-        }
-      </li>
-    );
-  }
+          </div>
+        ))
+      }
+    </li>
+  );
 }
 
 export default Message;
