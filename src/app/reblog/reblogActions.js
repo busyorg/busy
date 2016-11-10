a5b05280b82c3d58b5db3fc7b553ba86ca048923
p0o <pooria_az@ymail.com>
2016-11-10 12:07:50 +0800

    create the actions for reblogging

---
+++ src/app/reblog/reblogActions.js
@@ -0,0 +1,40 @@
+import store from 'store';
+import SteemConnect from 'steemconnect';
+import Promise from 'bluebird';
+import { createAction } from 'redux-actions';
+
+SteemConnect.reblog = Promise.promisify(SteemConnect.reblog, { context: SteemConnect });
+
+export const GET_REBLOGGED_LIST = '@reblog/GET_REBLOGGED_LIST';
+
+const getRebloggedListAction = createAction(GET_REBLOGGED_LIST);
+
+const storePostId = (postId) => {
+  const reblogged = store.get('reblogged') || [];
+  const newReblogged = [ ...reblogged, postId ];
+  store.set('reblogged', newReblogged);
+  return newReblogged;
+};
+
+export const reblog = (postId) => {
+  return (dispatch, getState) => {
+    const { auth, posts } = getState();
+    const post = posts[postId];
+
+    if (!auth.isAuthenticated || !post || auth.user.name === post.author) {
+      return;
+    }
+
+    SteemConnect.reblog(auth.user.name, post.author, post.permlink).then(() => {
+      const list = storePostId(postId);
+      dispatch(getRebloggedListAction(list));
+    }).catch(err => console.log(err));
+  }
+};
+
+export const getRebloggedList = () => {
+  return (dispatch) => {
+    const list = store.get('reblogged') || [];
+    dispatch(getRebloggedListAction(list));
+  }
+};
