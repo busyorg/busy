90b5c387379f5f51c06ba9948adf6f906fee3802
p0o <pooria_az@ymail.com>
2016-11-12 01:28:00 +0800

    fix UI changes on userProfile

--- src/app/Trigger/TriggerProfile.js
+++ src/app/Trigger/TriggerProfile.js
@@ -6,16 +6,21 @@ const TriggerProfile = React.createClass({
   render() {
     const {
       isFollowing,
-      isFollowingIsLoading,
-      onClickFollow
+      followingIsFetching,
+      onClickFollow,
+      hasFollow
     } = this.props;
 
     return (
       <div className="actions">
         <div className="triggers">
-          <a className={classNames('trigger', { disabled: isFollowingIsLoading })} onClick={onClickFollow}>
-            <i className="icon material-icons">{isFollowing ? 'person_outline' : 'person_add'}</i>
-          </a>
+
+          { hasFollow &&
+            <a className={classNames('trigger', { disabled: followingIsFetching })} onClick={onClickFollow}>
+              <i className="icon material-icons">{isFollowing ? 'person_outline' : 'person_add'}</i>
+            </a>
+          }
+
           <Link to={`/messages/@${this.props.username}`} className="trigger"><i className="icon icon-md material-icons">chat_bubble_outline</i></Link>
         </div>
       </div>
