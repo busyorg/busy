005af7757740ff71eb8805765853a78ac196d2c7
p0o <pooria_az@ymail.com>
2016-11-20 17:50:25 +0800

    handling post likes optimistically on reducer

--- src/post/postsReducers.js
+++ src/post/postsReducers.js
@@ -2,6 +2,25 @@ import * as feedTypes from '../feed/feedActions';
 import * as bookmarksActions from '../bookmarks/bookmarksActions';
 import * as postsActions from './postActions';
 
+const postItem = (state = {}, action) => {
+  switch (action.type) {
+    case postsActions.LIKE_POST_START:
+      const optimisticVoterPayload = {
+        voter: action.meta.voter
+      };
+      return {
+        ...state,
+        active_votes: [
+          ...state.active_votes,
+          optimisticVoterPayload,
+        ],
+        net_votes: parseInt(state.net_votes) + 1,
+      };
+    default:
+      return state;
+  }
+};
+
 const posts = (state = {}, action) => {
   switch (action.type) {
     case feedTypes.GET_FEED_CONTENT_SUCCESS:
@@ -21,6 +40,11 @@ const posts = (state = {}, action) => {
         [action.payload.id]: {
           ...action.payload
         },
+      };
+    case postsActions.LIKE_POST_START:
+      return {
+        ...state,
+        [action.meta.postId]: postItem(state[action.meta.postId], action),
       };
     default:
       return state;
