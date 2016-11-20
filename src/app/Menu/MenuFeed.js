864e3a314b6a6be9e8e56ad03f7ac97f3e237093
adcpm <fabien@bonustrack.co>
2016-11-20 16:34:10 +0100

    Remove unwanted menu, refactor the rest

--- src/app/Menu/MenuFeed.js
+++ src/app/Menu/MenuFeed.js
@@ -2,35 +2,37 @@ import React from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
 
-module.exports = React.createClass({
-  render() {
-    const category = (this.props.category) ? `/${this.props.category}` : '';
-    return (
-      <ul className="app-nav">
-        <li>
-          <Link to={`/trending${category}`} onlyActiveOnIndex activeClassName="active">
-            <i className="icon icon-md material-icons">show_chart</i>{' '}
-            <span className="hidden-xs"><FormattedMessage id="trending" /></span>
-          </Link>
-        </li>
-        <li>
-          <Link to={`/hot${category}`} activeClassName="active">
-            <i className="icon icon-md material-icons">whatshot</i>{' '}
-            <span className="hidden-xs"><FormattedMessage id="hot" /></span>
-          </Link>
-        </li>
-        <li>
-          <Link to={`/created${category}`} activeClassName="active">
-            <i className="icon icon-md material-icons">fiber_new</i>{' '}
-            <span className="hidden-xs"><FormattedMessage id="new" /></span></Link>
-        </li>
-        <li>
-          <Link to={`/active${category}`} activeClassName="active">
-            <i className="icon icon-md material-icons">track_changes</i>{' '}
-            <span className="hidden-xs"><FormattedMessage id="active" /></span>
-          </Link>
-        </li>
-      </ul>
-    );
-  }
-});
+import Icon from '../../widgets/Icon';
+
+const MenuFeed = ({ category }) => {
+  const categoryUrl = category ? `/${category}` : '';
+  return (
+    <ul className="app-nav">
+      <li>
+        <Link to={`/trending${categoryUrl}`} onlyActiveOnIndex activeClassName="active">
+          <Icon name="show_chart" />{' '}
+          <span className="hidden-xs"><FormattedMessage id="trending" /></span>
+        </Link>
+      </li>
+      <li>
+        <Link to={`/hot${categoryUrl}`} activeClassName="active">
+          <Icon name="whatshot" />{' '}
+          <span className="hidden-xs"><FormattedMessage id="hot" /></span>
+        </Link>
+      </li>
+      <li>
+        <Link to={`/created${categoryUrl}`} activeClassName="active">
+          <Icon name="fiber_new" />{' '}
+          <span className="hidden-xs"><FormattedMessage id="new" /></span></Link>
+      </li>
+      <li>
+        <Link to={`/active${categoryUrl}`} activeClassName="active">
+          <Icon name="track_changes" />{' '}
+          <span className="hidden-xs"><FormattedMessage id="active" /></span>
+        </Link>
+      </li>
+    </ul>
+  );
+};
+
+export default MenuFeed;
