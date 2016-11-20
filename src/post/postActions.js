7e1691e12e2d26c7dc63ea3073b1d39806e47b65
p0o <pooria_az@ymail.com>
2016-11-20 12:40:22 +0800

    add action for liking posts

--- src/post/postActions.js
+++ src/post/postActions.js
@@ -1,7 +1,17 @@
+import steemConnect from 'steemconnect';
+import Promise from 'bluebird';
+
 export const GET_CONTENT = 'GET_CONTENT';
 export const GET_CONTENT_START = 'GET_CONTENT_START';
 export const GET_CONTENT_SUCCESS = 'GET_CONTENT_SUCCESS';
 export const GET_CONTENT_ERROR = 'GET_CONTENT_ERROR';
+
+export const LIKE_POST = '@post/LIKE_POST';
+export const LIKE_POST_START = '@post/LIKE_POST_START';
+export const LIKE_POST_SUCCESS = '@post/LIKE_POST_SUCCESS';
+export const LIKE_POST_ERROR = '@post/LIKE_POST_ERROR';
+
+steemConnect.vote = Promise.promisify(steemConnect.vote, { context: steemConnect });
 
 
 export const getContent = (postAuthor, postPermlink) => {
@@ -18,4 +28,23 @@ export const getContent = (postAuthor, postPermlink) => {
     });
 
   };
+};
+
+export const likePost = (postId) => {
+  return (dispatch, getState) => {
+    const { auth, posts } = getState();
+
+    if (!auth.isAuthenticated) {
+      return;
+    }
+
+    const voter = auth.user.name;
+
+    dispatch({
+      type: LIKE_POST,
+      payload: {
+        promise: steemConnect.vote(voter, posts[postId].author, posts[postId].permlink, 10000),
+      },
+    });
+  }
 };
