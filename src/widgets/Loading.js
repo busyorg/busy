e745592b9f462ade86410ee737892993284e4a9e
adcpm <fabien@bonustrack.co>
2016-11-22 20:48:55 +0100

    Separate Loading.sass

--- src/widgets/Loading.js
+++ src/widgets/Loading.js
@@ -1,13 +1,17 @@
-const React = require('react');
+import React from 'react';
 
-module.exports = React.createClass({
-  render() {
-    let className = (this.props.color === 'white') ? 'loading-white' : 'loading';
-    className += ' align-center';
-    return (
-      <div><div className={className}>
-        <span>●</span><span>●</span><span>●</span></div>
+import './Loading.sass';
+
+const Loading = ({ color }) => {
+  let className = (color === 'white') ? 'loading-white' : 'loading';
+  className += ' align-center';
+  return (
+    <div>
+      <div className={className}>
+        <span>●</span><span>●</span><span>●</span>
       </div>
-    );
-  }
-});
+    </div>
+  );
+};
+
+export default Loading;
