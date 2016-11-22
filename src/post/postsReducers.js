93db013f447ea4fb3655ec2b0c06902ab2d582af
p0o <pooria_az@ymail.com>
2016-11-23 00:29:27 +0800

    define variables with const

--- src/post/postsReducers.js
+++ src/post/postsReducers.js
@@ -5,17 +5,14 @@ import * as postsActions from './postActions';
 const postItem = (state = {}, action) => {
   switch (action.type) {
     case postsActions.LIKE_POST_START:
-      let optimisticActiveVotes = [];
-      let optimisticNetVotes = 0;
-
-      optimisticActiveVotes = [
+      const optimisticActiveVotes = [
         ...state.active_votes.filter(vote => vote.voter !== action.meta.voter),
         {
           voter: action.meta.voter,
           percent: action.meta.weight,
         },
       ];
-      optimisticNetVotes = action.meta.weight > 0
+      const optimisticNetVotes = action.meta.weight > 0
         ? parseInt(state.net_votes) + 1
         : parseInt(state.net_votes) - 1;
 
