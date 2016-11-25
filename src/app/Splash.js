f63f78aa93a4fdb497285d27e3e642bfae5266be
adcpm <fabien@bonustrack.co>
2016-11-25 15:01:57 +0100

    Improve Splash

--- src/app/Splash.js
+++ src/app/Splash.js
@@ -2,24 +2,34 @@ import React from 'react';
 import steemconnect from 'steemconnect';
 
 import Modal from '../widgets/Modal';
+import Icon from '../widgets/Icon';
+
+import './Splash.sass';
 
 const Splash = () => {
   const loginURL = steemconnect.getLoginUrl(process.env.STEEMCONNECT_REDIRECT_URL);
   return (
-    <Modal>
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <img src="/img/logo.svg" height="40" />
-          </div>
-          <div className="modal-body">
-            <h3>Please log in with you Steem account</h3>
-            <a href={loginURL} className="btn btn-lg btn-primary">Log In With Steem Connect</a>
-            <p>New on Steem?</p>
+    <div className="Splash">
+      <Modal>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <img src="/img/logo.svg" height="40" />
+            </div>
+            <div className="modal-body">
+              <h2>We're currently in beta.</h2>
+              <p>Utque proeliorum periti rectores primo catervas
+                Nemo quaeso miretur, si post exsudatos labores
+                Erat autem diritatis eius hoc quoque indicium</p>
+              <a href={loginURL} className="btn btn-lg btn-primary">
+                <Icon name="lock" sm />{ ' ' }
+                Log In
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    </div>
   );
 };
 
