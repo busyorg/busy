822c49a458c68454a69909af638fe7559675bdcb
adcpm <fabien@bonustrack.co>
2016-11-26 23:17:58 +0100

    Hide activity link

--- src/app/Menu/MenuUser.js
+++ src/app/Menu/MenuUser.js
@@ -9,25 +9,25 @@ const MenuUser = ({ username }) => {
     <ul className="app-nav">
       <li>
         <Link to={`/@${username}`} activeClassName="active">
-          <Icon name="assignment_ind" />{' '}
+          <Icon name="assignment_ind" />{ ' ' }
           <span className="hidden-xs"><FormattedMessage id="profile" /></span>
         </Link>
       </li>
-      <li>
-        <Link to={`#@${username}/activity`} activeClassName="active">
-          <Icon name="history" />{' '}
-          <span className="hidden-xs"><FormattedMessage id="activity" /></span>
-        </Link>
-      </li>
+      {/* <li>
+          <Link to={`@${username}/activity`} activeClassName="active">
+            <Icon name="history" />{ ' ' }
+            <span className="hidden-xs"><FormattedMessage id="activity" /></span>
+          </Link>
+        </li> */}
       <li>
         <Link to={`/@${username}/feed`} activeClassName="active">
-          <Icon name="subject" />{' '}
+          <Icon name="subject" />{ ' ' }
           <span className="hidden-xs"><FormattedMessage id="feed" /></span>
         </Link>
       </li>
       <li>
         <Link to={`/@${username}/transfers`} activeClassName="active">
-          <Icon name="account_balance_wallet" />{' '}
+          <Icon name="account_balance_wallet" />{ ' ' }
           <span className="hidden-xs"><FormattedMessage id="wallet" /></span>
         </Link>
       </li>
