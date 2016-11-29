5a24336065088de35c9fcf9d956abc6f602b142c
p0o <pooria_az@ymail.com>
2016-11-29 14:14:40 +0800

    add dollar sign to payout formatting

--- src/app/Trigger/TriggerPost.js
+++ src/app/Trigger/TriggerPost.js
@@ -15,7 +15,7 @@ const TriggerPost = ({
   const payout = numeral(
     parseFloat(content.total_payout_value) +
     parseFloat(content.total_pending_payout_value)
-  ).format('0,0.00');
+  ).format('$0,0.00');
 
   const numberOfComments = numeral(content.children).format('0,0');
   const numberOfVotes = numeral(content.net_votes).format('0,0');
