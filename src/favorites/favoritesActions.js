846b7f5b403b09495c1966fa00ff69a9a0010be7
p0o <pooria_az@ymail.com>
2016-12-01 18:03:54 +0800

    fill action creators strings (was empty as a mistake)

--- src/favorites/favoritesActions.js
+++ src/favorites/favoritesActions.js
@@ -1,10 +1,10 @@
 import { createAction } from 'redux-actions';
 
-export const ADD_USER_FAVORITE = '';
-export const REMOVE_USER_FAVORITE = '';
+export const ADD_USER_FAVORITE = '@favorites/ADD_USER_FAVORITE';
+export const REMOVE_USER_FAVORITE = '@favorites/REMOVE_USER_FAVORITE';
 
-export const ADD_CATEGORY_FAVORITE = '';
-export const REMOVE_CATEGORY_FAVORITE = '';
+export const ADD_CATEGORY_FAVORITE = '@favorites/ADD_CATEGORY_FAVORITE';
+export const REMOVE_CATEGORY_FAVORITE = '@favorites/REMOVE_CATEGORY_FAVORITE';
 
 export const addUserFavorite = createAction(ADD_USER_FAVORITE);
 export const removeUserFavorite = createAction(REMOVE_USER_FAVORITE);
