1e0fedc9ea0e3009acedd7b7fac6582c2bcc0879
adcpm <fabien@bonustrack.co>
2016-12-14 14:27:25 +0100

    Add donor list

---
+++ src/helpers/donors.js
@@ -0,0 +1,10 @@
+
+export default {
+  'steemit': 'gold',
+  'smooth': 'gold',
+  'cass': 'silver',
+  'bhuz': 'silver',
+  'theprophet0': 'bronze',
+  'furion': 'bronze',
+  'liondani': 'bronze',
+};
