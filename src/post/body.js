d39b79a404b3c0e657ce0cae61374d7f2611af97
Nilesh Suthar <nileshsuthar@live.in>
2016-12-15 12:55:06 +0530

    [Markdown] remove striptags and use marked sanitize

--- src/post/body.js
+++ src/post/body.js
@@ -1,32 +1,34 @@
-let React = require('react'),
-  _ = require('lodash'),
-  steemembed = require('steemembed'),
-  striptags = require('striptags'),
-  marked = require('marked');
+import React from 'react';
+import { has } from 'lodash';
+import steemembed from 'steemembed';
+import marked from 'marked';
 
-module.exports = React.createClass({
-  render() {
-    const embeds = steemembed.getAll(this.props.body);
-    let body = this.props.body;
-    body = striptags(marked(body), ['a', 'p', 'h1', 'h2', 'h3', 'img', 'code', 'pre']);
-    let jsonMetadata = {};
-    try { jsonMetadata = JSON.parse(this.props.jsonMetadata); }
-    catch (e) { }
-    if (_.has(jsonMetadata, 'image[0]')) {
-      jsonMetadata.image.forEach((image) => {
-        const newUrl = 'https://img1.steemit.com/870x600/' + image;
-        body = body.replace(new RegExp(image, 'g'), newUrl);
-        body = body.replace(new RegExp('<a href="' + newUrl + '">' + newUrl + '</a>', 'g'),
-          '<img src="' + newUrl + '">');
-      });
-    }
-    return (
-      <div>
-        {_.has(embeds, '[0].embed') &&
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  sanitize: true,
+  smartypants: true
+});
+
+export default (props) => {
+  const embeds = steemembed.getAll(props.body);
+  let body = props.body;
+  body = marked(body);
+  let jsonMetadata = {};
+  try { jsonMetadata = JSON.parse(props.jsonMetadata); } catch (e) { }
+
+  if (has(jsonMetadata, 'image[0]')) {
+    jsonMetadata.image.forEach((image) => {
+      const newUrl = `https://img1.steemit.com/870x600/${image}`;
+      body = body.replace(new RegExp(image, 'g'), newUrl);
+      body = body.replace(new RegExp(`<a href="${newUrl}">${newUrl}</a>`, 'g'), `<img src="${newUrl}">`);
+    });
+  }
+  return (
+    <div>
+      {has(embeds, '[0].embed') &&
         <div className="thumbs">
           <div dangerouslySetInnerHTML={{ __html: embeds[0].embed }} />
         </div>}
-        <span dangerouslySetInnerHTML={{ __html: body }} />
-      </div>);
-  }
-});
+      <span dangerouslySetInnerHTML={{ __html: body }} />
+    </div>);
+};
