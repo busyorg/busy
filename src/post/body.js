ac8ac5ef989a1689b80dfdbc823798daab4bd0c0
Nilesh Suthar <nileshsuthar@live.in>
2016-12-17 11:11:29 +0530

    [Markdown] Fix body rendering

--- src/post/body.js
+++ src/post/body.js
@@ -1,46 +1,44 @@
 import React from 'react';
 import { has } from 'lodash';
 import steemembed from 'steemembed';
-import striptags from 'striptags';
+import sanitizeHtml from 'sanitize-html';
 import marked from 'marked';
 
-marked.setOptions({
-  renderer: new marked.Renderer(),
-  sanitize: true,
-  smartypants: true
-});
+const sanitizeConfig = {
+  allowedTags: `
+    div, iframe, del,
+    a, p, b, q, br, ul, li, ol, img, h1, h2, h3, h4, h5, h6, hr,
+    blockquote, pre, code, em, strong, center, table, thead, tbody, tr, th, td,
+    strike, sup, sub`.trim().split(/,\s*/)
+};
 
 export default (props) => {
   const embeds = steemembed.getAll(props.body);
   let body = props.body;
-  // body = striptags(remarkable.render(body), ['a', 'p', 'h1', 'h2', 'h3', 'img', 'code', 'pre']);
-  // body = striptags(remarkable.render(body), ['a', 'p', 'h1', 'h2', 'h3', 'img', 'code', 'pre']);
-  // body = remarkable.render(body);
   let jsonMetadata = {};
   try { jsonMetadata = JSON.parse(props.jsonMetadata); } catch (e) { }
 
-  if (jsonMetadata.format === 'markdown') {
-    body = marked(body);
-    if (has(jsonMetadata, 'image[0]')) {
-      jsonMetadata.image.forEach((image) => {
-        const newUrl = `https://img1.steemit.com/870x600/${image}`;
-        body = body.replace(new RegExp(image, 'g'), newUrl);
-        body = body.replace(new RegExp(`<a href="${newUrl}">${newUrl}</a>`, 'g'), `<img src="${newUrl}">`);
-      });
-    }
-    if (has(jsonMetadata, 'users[0]')) {
-      body = body.replace(/(^|\s)(@[-.a-z\d]+)/ig, (user) => {
-        const space = /^\s/.test(user) ? user[0] : '';
-        user = user.trim().substring(1);
-        return `${space}<a href="/@${user}">@${user}</a>`;
-      });
-    }
-  } else {
-    // format html.
-    body = marked(body, { sanitize: false });
-    // body = striptags(marked(body, { sanitize: false }),
-    //   ['a', 'b', 'p', 'h1', 'h2', 'h3', 'img', 'code', 'pre']);
+  body = body.replace(/<!--([\s\S]+?)(-->|$)/g, '(html comment removed: $1)');
+
+  if (has(jsonMetadata, 'users[0]')) {
+    body = body.replace(/(^|\s)(@[-.a-z\d]+)/ig, (user) => {
+      const space = /^\s/.test(user) ? user[0] : '';
+      user = user.trim().substring(1);
+      return `${space}<a href="/@${user}">@${user}</a>`;
+    });
   }
+
+  body = marked(body);
+
+  if (has(jsonMetadata, 'image[0]')) {
+    jsonMetadata.image.forEach((image) => {
+      const newUrl = `https://img1.steemit.com/870x600/${image}`;
+      body = body.replace(new RegExp(image, 'g'), newUrl);
+      body = body.replace(new RegExp(`<a href="${newUrl}">${newUrl}</a>`, 'g'), `<img src="${newUrl}">`);
+    });
+  }
+
+  body = sanitizeHtml(body, sanitizeConfig);
 
   return (
     <div>
