a7b39fafd933a707372e45b0fd946c6bdfafd80f
p0o <pooria_az@ymail.com>
2016-12-31 23:52:26 +0800

    make the style inline on no title

--- src/favorites/FavoriteCategoryButton.js
+++ src/favorites/FavoriteCategoryButton.js
@@ -3,7 +3,10 @@ import Icon from '../widgets/Icon';
 
 const FavoriteCategoryButton = ({ name, onClick, isFavorited, noTitle }) => {
   return (
-    <h2 className="mt-1 mb-0 text-xs-center">
+    <h2
+      className="mt-1 mb-0 text-xs-center"
+      style={noTitle ? { display: 'inline-block' } : {}}
+    >
       <a onClick={onClick}>
         <Icon name={isFavorited ? 'star' : 'star_border'} sm />{ ' ' }
         { noTitle ? '' : `#${name}` }
