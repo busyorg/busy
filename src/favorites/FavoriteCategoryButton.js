f975479e1f3af7999c2a1670ea075c20b7c0edd5
p0o <pooria_az@ymail.com>
2016-12-31 23:49:09 +0800

    add noTitle prop to Favorite button

--- src/favorites/FavoriteCategoryButton.js
+++ src/favorites/FavoriteCategoryButton.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Icon from '../widgets/Icon';
 
-const FavoriteCategoryButton = ({ name, onClick, isFavorited }) => {
+const FavoriteCategoryButton = ({ name, onClick, isFavorited, noTitle }) => {
   return (
     <h2 className="mt-1 mb-0 text-xs-center">
       <a onClick={onClick}>
         <Icon name={isFavorited ? 'star' : 'star_border'} sm />{ ' ' }
-        #{ name }
+        { noTitle ? '' : `#${name}` }
       </a>
     </h2>
   );
