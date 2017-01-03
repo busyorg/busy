5baccae7423e90cebde41cdde04438998cd3c23c
p0o <pooria_az@ymail.com>
2017-01-03 23:03:10 +0800

    use react-portal-tooltip for the overlay

--- src/user/profileTooltip/ProfileTooltip.js
+++ src/user/profileTooltip/ProfileTooltip.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import ToolTip from 'react-portal-tooltip';
 
 import './ProfileTooltip.scss';
 
@@ -22,12 +23,38 @@ export default class UserProfile extends Component {
     username: PropTypes.string
   };
 
+  openTooltip = () => {
+    this.setState({ isOpen: true });
+  };
+
+  closeTooltip = () => {
+    this.setState({ isOpen: false });
+  };
+
   render() {
+    const { username } = this.props;
     return (
       <div className="ProfileTooltip">
-        <Link to={`/@${this.props.username}`}>
+        <Link
+          onMouseEnter={this.openTooltip}
+          onMouseLeave={this.closeTooltip}
+          to={`/@${username}`}
+          id={`${username}`}
+        >
           { this.props.children }
         </Link>
+
+        <ToolTip
+          active={this.state.isOpen}
+          position="right"
+          arrow="top"
+          parent={`#${username}`}
+        >
+          <div>
+            <p>This is the content of the tooltip</p>
+            <img src="image.png"/>
+          </div>
+        </ToolTip>
       </div>
     );
   }
