868250d2a533a9a5add6c6225a0eb59003bd8659
p0o <pooria_az@ymail.com>
2017-01-06 01:52:10 +0800

    make cover image to keep the size

--- src/user/UserCoverImage.js
+++ src/user/UserCoverImage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Img from 'react-advanced-img';
 
 const UserCoverImage = ({ username, width, height }) => (
-  <Img
-    src={`${process.env.STEEMCONNECT_IMG_HOST}/@${username}/cover`}
-    placeholder="busy.org"
-    width={width}
-    height={height}
-  />
+  <div style={{ width: `${width}px`, height: `${height}px` }}>
+    <Img
+      src={`${process.env.STEEMCONNECT_IMG_HOST}/@${username}/cover`}
+      placeholder="busy.org"
+      width={width}
+      height={height}
+    />
+  </div>
 );
 
 export default UserCoverImage;
