e3694dacb11b3ac849bdbf70236f030ac1e82048
Nilesh Suthar <nileshsuthar@live.in>
2017-01-07 16:09:47 +0530

    Fix updating follow btn state

--- src/post/AuthorBio.js
+++ src/post/AuthorBio.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import steemdb from 'steemdb';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import Avatar from '../widgets/Avatar';
 import Loading from '../widgets/Loading';
 import Follow from '../widgets/Follow';
-import { followUser, unfollowUser } from '../user/userActions';
+import { followUser, unfollowUser, getFollowing } from '../user/userActions';
 
 @connect(
-  ({ auth }) => ({ auth }),
-  dispatch => ({
-    followUser: (...args) => dispatch(followUser(...args)),
-    unfollowUser: (...args) => dispatch(unfollowUser(...args)),
-  })
+  ({ auth, userProfile }) => ({ auth, following: userProfile.following }),
+  dispatch => bindActionCreators({ followUser, unfollowUser, getFollowing }, dispatch)
 )
 class AuthorBio extends Component {
   constructor(props) {
@@ -23,48 +21,47 @@ class AuthorBio extends Component {
   }
 
   componentDidMount() {
+    const loggedInUser = this.props.auth.user.name;
+    this.props.getFollowing(loggedInUser);
     steemdb.accounts({
       account: this.props.authorName
     }, (err, result) => {
       if (result.length) {
-        const jsonMetaData = result[0].json_metadata;
         const author = result[0];
+
         try {
-          author.json_metadata = JSON.parse(jsonMetaData);
+          author.json_metadata = JSON.parse(result[0].json_metadata);
         } catch (e) {
           author.json_metadata = {};
         }
-
         this.setState({ author });
       }
     });
   }
 
   render() {
-    const { authorName, auth } = this.props;
+    const { authorName, auth, following } = this.props;
     const { author } = this.state;
     const loggedInUser = auth.user.name;
 
-    if (author) {
-      const isFollowing = author ? author.followers.indexOf(loggedInUser) >= 0 : false;
-      const onClickFollowFn = isFollowing ? this.props.unfollowUser : this.props.followUser;
+    if (author && !following.isFetching) {
+      const isFollowing = following.list.indexOf(authorName) >= 0;
       const { about, name } = author.json_metadata.profile || {};
       const displayName = name || `@${authorName}`;
+      const onClickFollowFn = isFollowing ? this.props.unfollowUser : this.props.followUser;
 
       return (
         <div className="col-md-4">
           <div>
             <Avatar lg username={authorName} />
-            {displayName}
+            <span>{displayName}</span>
             <Follow
               hasFollow={authorName !== loggedInUser}
               isFollowing={isFollowing}
               onClickFollow={() => onClickFollowFn(authorName)}
             />
           </div>
-          <div style={{ display: 'inline-block' }}>
-            <span>{about}</span>
-          </div>
+          <div>{about}</div>
         </div>
       );
     }
