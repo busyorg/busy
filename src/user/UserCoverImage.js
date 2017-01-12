5877c5246ead062157329b844be12932581ff684
p0o <pooria_az@ymail.com>
2017-01-12 13:18:41 +0800

    remove react-advanced-img

--- src/user/UserCoverImage.js
+++ src/user/UserCoverImage.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import Img from 'react-advanced-img';
 
 const UserCoverImage = ({ username, width, height }) => (
   <div style={{ width: `${width}px`, height: `${height}px` }}>
-    <Img
-      src={`${process.env.STEEMCONNECT_IMG_HOST}/@${username}/cover`}
-      placeholder="busy.org"
-      width={width}
-      height={height}
-    />
+    <div
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        background: `url(${process.env.STEEMCONNECT_IMG_HOST}/@${username}/cover)`,
+        backgroundSize: 'cover',
+      }}
+    >
+      {' '}
+    </div>
   </div>
 );
 
