b9225baa2c102fe3c41676bca7de1f1ae3400d24
p0o <pooria_az@ymail.com>
2017-01-14 16:58:55 +0800

    add simple Tooltip setup for regular usage

---
+++ src/app/TooltipOrigin.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Origin } from 'redux-tooltip';
+
+const TooltipOrigin = ({ children, content, active }) => {
+  if (!active) {
+    return (
+      <Origin
+        content={content}
+        delay={500}
+        delayOn="show"
+      >
+        { children }
+      </Origin>
+    );
+  }
+
+  return children;
+};
+
+export default TooltipOrigin;
