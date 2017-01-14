bec08e1771f180d9d75865794044fceee97fc80a
adcpm <fabien@bonustrack.co>
2017-01-14 11:16:03 +0700

    Add gold color on favorite icon

--- src/favorites/FavoriteUserButton.js
+++ src/favorites/FavoriteUserButton.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Icon from '../widgets/Icon';
+import './FavoriteButton.scss';
 
-const FavoriteUserButton = ({ onClick, isFavorited }) => {
-  return (
-    <a onClick={onClick}>
-      <Icon name={isFavorited ? 'star' : 'star_border'} md />
-      { ' ' }
-    </a>
-  );
-};
+const FavoriteUserButton = ({ onClick, isFavorited }) =>
+  <a className={isFavorited ? 'FavoriteButton FavoriteButton--active' : 'FavoriteButton'} onClick={onClick}>
+    <Icon name={isFavorited ? 'star' : 'star_border'} sm />
+    { ' ' }
+  </a>;
 
 export default FavoriteUserButton;
