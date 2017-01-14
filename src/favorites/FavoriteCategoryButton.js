453ed141c394b87fb2792134353aaad7fe2a722d
p0o <pooria_az@ymail.com>
2017-01-14 17:30:42 +0800

    add tooltip for favorite buttons

--- src/favorites/FavoriteCategoryButton.js
+++ src/favorites/FavoriteCategoryButton.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Icon from '../widgets/Icon';
 import './FavoriteButton.scss';
+import TooltipOrigin from '../app/TooltipOrigin';
 
 const FavoriteCategoryButton = ({ children, onClick, isFavorited }) =>
-  <a className={isFavorited ? 'FavoriteButton FavoriteButton--active' : 'FavoriteButton'} onClick={onClick}>
-    <Icon name={isFavorited ? 'star' : 'star_border'} sm />
-    { children && ' ' }
-    { children }
-  </a>;
+  <TooltipOrigin
+    content="Add in your favorites"
+    active={!isFavorited}
+  >
+    <a
+      className={isFavorited ? 'FavoriteButton FavoriteButton--active' : 'FavoriteButton'}
+      onClick={onClick}
+    >
+      <Icon name={isFavorited ? 'star' : 'star_border'} sm />
+      { children && ' ' }
+      { children }
+    </a>
+  </TooltipOrigin>;
 
 export default FavoriteCategoryButton;
--- src/favorites/FavoriteUserButton.js
+++ src/favorites/FavoriteUserButton.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Icon from '../widgets/Icon';
+import TooltipOrigin from '../app/TooltipOrigin';
 import './FavoriteButton.scss';
 
 const FavoriteUserButton = ({ onClick, isFavorited }) =>
-  <a className={isFavorited ? 'FavoriteButton FavoriteButton--active' : 'FavoriteButton'} onClick={onClick}>
-    <Icon name={isFavorited ? 'star' : 'star_border'} sm />
-    { ' ' }
-  </a>;
+  <TooltipOrigin
+    content="Add in your favorites"
+    active={!isFavorited}
+  >
+    <a
+      className={isFavorited ? 'FavoriteButton FavoriteButton--active' : 'FavoriteButton'}
+      onClick={onClick}
+    >
+      <Icon name={isFavorited ? 'star' : 'star_border'} sm />
+      { ' ' }
+    </a>
+  </TooltipOrigin>;
 
 export default FavoriteUserButton;
