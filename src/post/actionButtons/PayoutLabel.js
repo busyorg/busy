f53c14ea5b6a9661a1d085c3442c1bb4bb854252
adcpm <fabien@bonustrack.co>
2017-01-18 13:34:39 +0700

    Add line-through when author refuse reward

--- src/post/actionButtons/PayoutLabel.js
+++ src/post/actionButtons/PayoutLabel.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import numeral from 'numeral';
 import Icon from '../../widgets/Icon';
 
-const PayoutLabel = ({ value, onClick }) =>
-  <a onClick={onClick && onClick} >
-    <span className="hidden-xs">
-      <Icon name="attach_money" sm />
-    </span>
-    {value}
-  </a>;
+const PayoutLabel = ({ post, onClick }) => {
+  const payout = parseFloat(post.total_payout_value)
+    + parseFloat(post.total_pending_payout_value)
+    + parseFloat(post.curator_payout_value);
+  const maxAcceptedPayout = parseFloat(post.max_accepted_payout);
+  return (
+    <a onClick={onClick && onClick}>
+      <span className="hidden-xs">
+        <Icon name="attach_money" sm />
+      </span>
+      <span className={maxAcceptedPayout === 0 && 'del'}>
+        {numeral(payout).format('$0,0.00')}
+      </span>
+    </a>
+  );
+};
 
 export default PayoutLabel;
