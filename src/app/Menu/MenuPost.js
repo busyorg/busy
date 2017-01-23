6ae757de4111f7d191af7113b3303b042469c1de
adcpm <fabien@bonustrack.co>
2017-01-23 15:13:08 +0700

    Move next story on PostSingleModal header

--- src/app/Menu/MenuPost.js
+++ src/app/Menu/MenuPost.js
@@ -1,104 +1,89 @@
-import React, { Component } from 'react';
+import React from 'react';
 import numeral from 'numeral';
 import { FormattedMessage } from 'react-intl';
 import Icon from '../../widgets/Icon';
 import './MenuPost.scss';
 
-export default class MenuPost extends Component {
+const MenuPost = ({
+  reblog,
+  isReblogged,
+  openCommentingDraft,
+  isPostLiked,
+  isPostDisliked,
+  likePost,
+  unlikePost,
+  dislikePost,
+  content,
+  isScrolling,
+}) => {
+  const payout = numeral(
+    parseFloat(content.total_payout_value) +
+    parseFloat(content.total_pending_payout_value)
+  ).format('$0,0.00');
+  const numberOfComments = numeral(content.children).format('0,0');
+  const numberOfVotes = numeral(content.net_votes).format('0,0');
+  const numberOfDislikes = content.active_votes.filter(vote => vote.percent < 0).length;
+  return (
+    <div className="secondary-nav">
+      <ul
+        className="container text-left"
+        style={isScrolling ? { display: 'none' } : {}}
+      >
+        <li>
+          <a
+            className={isPostLiked ? 'active' : ''}
+            onClick={isPostLiked ? unlikePost : likePost}
+          >
+            <Icon name="thumb_up" />
+          </a>
+          {` ${numberOfVotes}`}
+          <span className="hidden-xs">
+            {' '}<FormattedMessage id="likes" />
+          </span>
+        </li>
 
-  nextStory = () => {
-    this.props.openPostModal(this.props.nextStory.id);
-    this.props.pushUrlState(this.props.nextStory);
-    this.props.scrollToTop();
-  }
+        <li>
+          <a
+            className={isPostDisliked ? 'active' : ''}
+            onClick={isPostDisliked ? unlikePost : dislikePost}
+          >
+            <Icon name="thumb_down" />
+          </a>
+          {` ${numberOfDislikes}`}
+          <span className="hidden-xs">
+            {' '}<FormattedMessage id="dislikes" />
+          </span>
+        </li>
 
-  render() {
-    const {
-      reblog,
-      isReblogged,
-      openCommentingDraft,
-      isPostLiked,
-      isPostDisliked,
-      likePost,
-      unlikePost,
-      dislikePost,
-      content,
-      nextStory
-    } = this.props;
-    const payout = numeral(
-      parseFloat(content.total_payout_value) +
-      parseFloat(content.total_pending_payout_value)
-    ).format('$0,0.00');
-
-    const numberOfComments = numeral(content.children).format('0,0');
-    const numberOfVotes = numeral(content.net_votes).format('0,0');
-    const numberOfDislikes = content.active_votes.filter(vote => vote.percent < 0).length;
-    return (
-      <div className="secondary-nav">
-        <ul
-          className="container text-left"
-          style={this.props.isScrolling ? { display: 'none' } : {}}
-        >
-          <li>
-            <a
-              className={isPostLiked ? 'active' : ''}
-              onClick={isPostLiked ? unlikePost : likePost}
-            >
-              <Icon name="thumb_up" />
-            </a>
-            {` ${numberOfVotes}`}
-            <span className="hidden-xs">
-              {' '}<FormattedMessage id="likes" />
-            </span>
-          </li>
-
-          <li>
-            <a
-              className={isPostDisliked ? 'active' : ''}
-              onClick={isPostDisliked ? unlikePost : dislikePost}
-            >
-              <Icon name="thumb_down" />
-            </a>
-            {` ${numberOfDislikes}`}
-            <span className="hidden-xs">
-              {' '}<FormattedMessage id="dislikes" />
-            </span>
-          </li>
-
-          <li>
-            <Icon name="attach_money" />
-            {' '}{payout}
-          </li>
-          <li>
-            <a
-              onClick={(e) => {
-                e.stopPropagation();
-                openCommentingDraft();
-              }}
-            >
-              <Icon name="reply" />
-            </a>
-            {` ${numberOfComments}`}
-            <span className="hidden-xs">
-              {' '}<FormattedMessage id="comments" />
-            </span>
-          </li>
-          <li>
-            <a
-              className={isReblogged ? 'active' : ''}
-              onClick={reblog}
-            >
-              <Icon name="repeat" />
-            </a>
-          </li>
-          {nextStory && <li className="pull-right">
-            <a className="next-story" onClick={this.nextStory}>
-              <span><FormattedMessage id="next_story" /><Icon name="navigate_next" /></span>
-              <span>{nextStory.title}</span>
-            </a>
-          </li>}
-        </ul>
-      </div>
-    );
-  }
+        <li>
+          <Icon name="attach_money" />
+          {' '}{payout}
+        </li>
+        <li>
+          <a
+            onClick={(e) => {
+              e.stopPropagation();
+              openCommentingDraft();
+            }}
+          >
+            <Icon name="reply" />
+          </a>
+          {` ${numberOfComments}`}
+          <span className="hidden-xs">
+            {' '}<FormattedMessage id="comments" />
+          </span>
+        </li>
+        <li>
+          <a
+            className={isReblogged ? 'active' : ''}
+            onClick={reblog}
+          >
+            <Icon name="repeat" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
+
+export default MenuPost;
