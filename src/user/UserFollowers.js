8fa2e57914d071e7fdce4eb9d82875d319ef4349
adcpm <fabien@bonustrack.co>
2017-01-30 02:07:27 +0700

    Fix loading

--- src/user/UserFollowers.js
+++ src/user/UserFollowers.js
@@ -7,13 +7,20 @@ export default class UserFollowers extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading: false,
+      isLoaded: false,
       users: [],
     };
   }
 
   componentWillMount() {
+    this.setState({ isLoading: true });
     getAllFollowers(this.props.params.name)
-      .then(users => this.setState({ users: users.sort() }));
+      .then(users => this.setState({
+        isLoading: false,
+        isLoaded: true,
+        users: users.sort()
+      }));
   }
 
   render() {
@@ -21,7 +28,7 @@ export default class UserFollowers extends Component {
       <div>
         <div className="container">
           {this.state.users && <UserList users={this.state.users} />}
-          {!this.state.users && <Loading />}
+          {this.state.isLoading && <Loading />}
         </div>
       </div>
     );
