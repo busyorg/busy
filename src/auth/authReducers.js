c5e23b6aa6159fd837043c2fdf76bdb12fe03b97
adcpm <fabien@bonustrack.co>
2017-01-29 15:58:06 +0700

    Save steemconnect token on redux state

--- src/auth/authReducers.js
+++ src/auth/authReducers.js
@@ -4,34 +4,40 @@ const initialState = {
   isAuthenticated: false,
   isFetching: false,
   user: {},
+  token: '',
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.LOGIN_REQUEST:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: true,
         isAuthenticated: false,
         user: {}
-      });
+      };
     case types.LOGIN_SUCCESS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         isAuthenticated: true,
         errorMessage: '',
-        user: action.user
-      });
+        user: action.user,
+        token: action.token,
+      };
     case types.LOGIN_FAILURE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         isAuthenticated: false,
         errorMessage: action.message
-      });
+      };
     case types.LOGOUT_SUCCESS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: true,
         isAuthenticated: false
-      });
+      };
     default:
       return state;
   }
