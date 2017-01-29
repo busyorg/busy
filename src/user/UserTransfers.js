b2283eb16a1dec9c7219ea297c65ae3fe073efbf
adcpm <fabien@bonustrack.co>
2017-01-29 12:58:22 +0700

    Close #284

--- src/user/UserTransfers.js
+++ src/user/UserTransfers.js
@@ -4,7 +4,6 @@ import { FormattedMessage } from 'react-intl';
 import numeral from 'numeral';
 import { formatter } from 'steem';
 import { connect } from 'react-redux';
-import _ from 'lodash';
 import api from '../steemAPI';
 import Loading from '../widgets/Loading';
 import * as walletActions from '../wallet/walletActions';
@@ -12,6 +11,7 @@ import TransferHistory from './TransferHistory';
 
 @connect(
   state => ({
+    app: state.app,
     wallet: state.wallet,
   }),
   dispatch => bindActionCreators({
@@ -22,46 +22,35 @@ export default class UserTransfers extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      account: {},
       props: {},
-      feedPrice: {},
     };
   }
 
   componentDidMount() {
     const username = this.props.params.name;
     this.props.getWallet(username);
-
-    api.getAccounts([username], (err, result) => {
-      this.setState({ account: result[0] });
-    });
     api.getState('trending/busy', (err, result) => {
-      this.setState({
-        props: result.props,
-        feedPrice: result.feed_price
-      });
+      this.setState({ props: result.props });
     });
   }
 
   render() {
+    const rate = this.props.app.rate;
     const username = this.props.params.name;
-    const account = this.state.account;
-    const base = (!_.isEmpty(this.state.feedPrice))
-      ? (this.state.feedPrice.base).replace(' SBD', '').replace(',', '')
-      : 0;
+    const account = this.props.user;
     let power = 0;
     let dollar = 0;
-    if (!_.isEmpty(this.state.feedPrice) && !_.isEmpty(account)) {
+    if (rate && account && this.state.props) {
       power = formatter.vestToSteem(account.vesting_shares,
         this.state.props.total_vesting_shares,
         this.state.props.total_vesting_fund_steem);
-      dollar = (parseFloat(base) *
+      dollar = (parseFloat(rate) *
         (parseFloat(account.balance) + parseFloat(power)))
         + parseFloat(account.sbd_balance);
     }
     return (
       <div className="container my-5">
-        {(!_.isEmpty(this.state.feedPrice) && !_.isEmpty(account)) ?
+        {power ?
           <div className="ptl text-center">
             <ul className="row text-center">
               <li className="col col-lg-3">
@@ -85,7 +74,6 @@ export default class UserTransfers extends Component {
           :
           <Loading />
         }
-
         <TransferHistory
           list={this.props.wallet.history[username] || []}
           username={username}
