cc864b6ff934b7a6c8cba9b3388e16e4c23a4551
adcpm <fabien@bonustrack.co>
2017-01-31 04:14:15 +0700

    Update contributors list

--- src/helpers/contributors.js
+++ src/helpers/contributors.js
@@ -8,4 +8,17 @@ export default [
     name: 'Nate',
     username: 'picokernel',
   },
+  {
+    name: 'Adrien',
+    username: 'heimindanger',
+  },
+  {
+    username: 'kaptainkrayola',
+  },
+  {
+    username: 'snowflake',
+  },
+  {
+    username: 'the-ego-is-you',
+  },
 ];
