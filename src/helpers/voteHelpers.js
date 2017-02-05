6a24e2360a4c65a09921c27d3683476aae07e160
adcpm <fabien@bonustrack.co>
2017-02-06 00:25:58 +0700

    Fix getFollowingUpvote and getFollowingDownvote

--- src/helpers/voteHelpers.js
+++ src/helpers/voteHelpers.js
@@ -7,11 +7,11 @@ export const getDownvotes = activeVotes =>
   activeVotes.filter(vote => vote.percent < 0);
 
 export const getFollowingUpvotes = (activeVotes, following) =>
-  this.getUpvotes(activeVotes)
+  getUpvotes(activeVotes)
     .filter(vote => _.includes(following, vote.voter));
 
 export const getFollowingDownvotes = (activeVotes, following) =>
-  this.getDownvotes(activeVotes)
+  getDownvotes(activeVotes)
     .filter(vote => _.includes(following, vote.voter));
 
 export const sortVotes = (votes, sortBy) =>
