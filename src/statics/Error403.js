817a7487da462d5dc60edd569521b9788b1c4e15
p0o <pooria_az@ymail.com>
2017-02-09 13:16:44 +0800

    add Error403 page to show in Chat un-authenticated page

---
+++ src/statics/Error403.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router';
+import steemconnect from 'steemconnect';
+import { FormattedMessage } from 'react-intl';
+import Header from '../app/Header';
+import Icon from '../widgets/Icon';
+
+const Error403 = () =>
+  <div className="main-panel">
+    <Header />
+    <div className="container my-5">
+      <div className="ptl text-center">
+        <h1><FormattedMessage id="page_forbidden" /></h1>
+        <h2><FormattedMessage id="page_forbidden_message" /></h2>
+        <p>You need to login to use the Chat feature, please
+          { ' ' }
+          <a href={steemconnect.getLoginURL()}>
+            <Icon name="lock_outline" />
+            {' '}<FormattedMessage id="login" />
+          </a>
+          <br />
+          Here's a link to the <Link to="/">home page</Link>.</p>
+      </div>
+    </div>
+  </div>;
+
+export default Error403;
