59eaaa45d3fd5ad9fdf04fe8e9b4e7794a796820
Nilesh Suthar <nileshsuthar@live.in>
2017-02-09 10:21:42 +0530

    Use Steemit HtmlReady

---
+++ src/helpers/steemitLinks.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase,no-param-reassign,consistent-return,no-console,new-cap,no-useless-escape,no-unneeded-ternary,max-len */
+
+/**
+ * This function is extracted from steemit.com source code and does the same tasks with some slight-
+ * adjustments to meet our needs. Refer to the main one in case of future problems:
+ * https://github.com/steemit/steemit.com/blob/4d4fe1f7da37d3dbb35bd0a131d9e5b44bad316d/app/utils/Links.js
+ */
+
+const urlChar = '[^\\s"<>\\]\\[\\(\\)]';
+const urlCharEnd = urlChar.replace(/\]$/, '.,\']'); // insert bad chars to end on
+const imagePath = '(?:(?:\\.(?:tiff?|jpe?g|gif|png|svg|ico)|ipfs/[a-z\\d]{40,}))';
+const domainPath = '(?:[-a-zA-Z0-9\\._]*[-a-zA-Z0-9])';
+const urlChars = `(?:${urlChar}*${urlCharEnd})?`;
+
+const urlSet = ({ domain = domainPath, path } = {}) =>
+  // urlChars is everything but html or markdown stop chars
+  `https?:\/\/${domain}(?::\\d{2,5})?(?:[/\\?#]${urlChars}${path ? path : ''})${path ? '' : '?'}`;
+
+/**
+    Unless your using a 'g' (glob) flag you can store and re-use your regular expression.  Use the cache below.  If your using a glob (for example: replace all), the regex object becomes stateful and continues where it left off when called with the same string so naturally the regexp object can't be cached for long.
+*/
+export const any = (flags = 'i') => new RegExp(urlSet(), flags);
+export const local = (flags = 'i') => new RegExp(urlSet({ domain: '(?:localhost|(?:.*\\.)?steemit.com|(?:.*\\.)?busy.org)' }), flags);
+export const remote = (flags = 'i') => new RegExp(urlSet({ domain: `(?!localhost|(?:.*\\.)?steemit.com|(?:.*\\.)?busy.org)${domainPath}` }), flags);
+export const youTube = (flags = 'i') => new RegExp(urlSet({ domain: '(?:(?:.*\.)?youtube.com|youtu.be)' }), flags);
+export const image = (flags = 'i') => new RegExp(urlSet({ path: imagePath }), flags);
+export const imageFile = (flags = 'i') => new RegExp(imagePath, flags);
+// export const nonImage = (flags = 'i') => new RegExp(urlSet({path: '!' + imageFile}), flags)
+// export const markDownImageRegExp = (flags = 'i') => new RegExp('\!\[[\w\s]*\]\(([^\)]+)\)', flags);
+
+export default {
+  any: any(),
+  local: local(),
+  remote: remote(),
+  image: image(),
+  imageFile: imageFile(),
+  youTube: youTube(),
+  youTubeId: /(?:(?:youtube.com\/watch\?v=)|(?:youtu.be\/)|(?:youtube.com\/embed\/))([A-Za-z0-9\_\-]+)/i,
+  vimeoId: /(?:vimeo.com\/|player.vimeo.com\/video\/)([0-9]+)/,
+  // simpleLink: new RegExp(`<a href="(.*)">(.*)<\/a>`, 'ig'),
+  ipfsPrefix: /(https?:\/\/.*)?\/ipfs/i,
+};
+
+// Original regex
+// const urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+
+// About performance
+// Using exec on the same regex object requires a new regex to be created and compile for each text (ex: post).  Instead replace can be used `body.replace(remoteRe, l => {` discarding the result for better performance`}).  Re-compiling is a chrome bottleneck but did not effect nodejs.
