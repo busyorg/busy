8dbd5b3bf41d3293dff08ab0853a2ea360ade751
yamadapc <tacla.yamada@gmail.com>
2017-02-10 11:32:47 -0200

    Separate messages by day groups
    - Uses `react-intl` and a new `dateHelpers` module to separate messages by
      date group
    - Renames messages components so they make more sense
    - Documents the grouping functions
    - Simplifies message sorting
    - Offsets `startOfDay`s so `react-intl` doesn't need to handle timezones
      propertly (which it isn't)

---
+++ src/messages/MessageDateGroup.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { FormattedDate, FormattedRelative } from 'react-intl';
+
+import './MessageList.scss';
+import MessageGroup from './MessageGroup';
+import { diffDays } from '../helpers/dateHelpers';
+import { propMessageDateGroup } from './messageGroupHelpers';
+
+export default class MessageDateGroup extends Component {
+  static propTypes = {
+    model: propMessageDateGroup.isRequired,
+  };
+
+  renderDay() {
+    if (diffDays(new Date(), this.props.model.day) < 2) {
+      return (
+        <FormattedRelative
+          value={this.props.model.day}
+          units="day"
+        />
+      );
+    }
+
+    return (
+      <FormattedDate
+        value={this.props.model.day}
+        year="numeric"
+        month="long"
+        day="numeric"
+      />
+    );
+  }
+
+  render() {
+    const day = this.renderDay();
+    return (
+      <div>
+        <div
+          style={{
+            position: 'relative',
+          }}
+        >
+          <h3
+            style={{
+              textAlign: 'center',
+            }}
+          >
+            <span
+              style={{
+                background: 'white',
+                borderRadius: 10,
+                padding: 10,
+                position: 'relative',
+                zIndex: 1,
+                textTransform: 'capitalize'
+              }}
+            >
+              {day}
+            </span>
+          </h3>
+
+          <hr
+            style={{
+              position: 'absolute',
+              top: '50%',
+              width: '100%',
+              margin: 0,
+              zIndex: 0,
+            }}
+          />
+        </div>
+
+        {this.props.model.messages.map((userGroup, i) => (
+          <MessageGroup
+            key={[userGroup.key, i]}
+            model={userGroup.messages}
+          />
+        ))}
+      </div>
+    );
+  }
+}
