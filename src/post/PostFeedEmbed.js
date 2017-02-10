ea8926f289b20b2bcd8c9f2969e821712c27f4c3
p0o <pooria_az@ymail.com>
2017-02-10 14:16:37 +0800

    use PostFeedEmbed to use embeds in feed

---
+++ src/post/PostFeedEmbed.js
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react';
+import embedjs from 'embedjs';
+
+export default class PostFeedEmbed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showIframe: false,
+    };
+  }
+
+  static propTypes = {
+    post: PropTypes.object.isRequired,
+  };
+
+  renderThumbFirst(thumb) {
+    return (
+      <div className="PostFeedCard__thumbs" onClick={this.handleThumbClick}>
+        <img src={thumb} />
+      </div>
+    );
+  }
+
+  handleThumbClick = (e) => {
+    e.stopPropagation();
+    this.setState({ showIframe: true })
+  };
+
+  renderWithIframe(embed) {
+    return (
+      <div className="PostFeedCard__thumbs" dangerouslySetInnerHTML={{ __html: embed }} />
+    );
+  }
+
+  render() {
+    const { post } = this.props;
+    const embeds = embedjs.getAll(post.body);
+
+    if (embeds[0].provider_name === 'YouTube' && !this.state.showIframe) {
+      return this.renderThumbFirst(embeds[0].thumbnail);
+    } else if (embeds[0].embed) {
+      return this.renderWithIframe(embeds[0].embed);
+    }
+  }
+}
