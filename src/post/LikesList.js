7085a4a2886c6b87c4cbb1066e88df34ac674468
adcpm <fabien@bonustrack.co>
2017-02-13 22:22:12 +0700

    Show like and dislikes separated on 2 columns

--- src/post/LikesList.js
+++ src/post/LikesList.js
@@ -3,6 +3,10 @@ import { Link } from 'react-router';
 import numeral from 'numeral';
 import Avatar from '../widgets/Avatar';
 import ProfileTooltipOrigin from '../user/profileTooltip/ProfileTooltipOrigin';
+import {
+  getUpvotes,
+  getDownvotes,
+} from '../helpers/voteHelpers';
 import './LikesList.scss';
 
 export default class LikesList extends Component {
@@ -22,13 +26,13 @@ export default class LikesList extends Component {
   render() {
     const { activeVotes } = this.props;
     const hasMore = activeVotes.length > this.state.show;
+    const upvotes = getUpvotes(activeVotes);
+    const downvotes = getDownvotes(activeVotes);
     return (
       <div className="LikesList">
-        {
-          activeVotes
-            .sort((a, b) => a.rshares - b.rshares)
-            .reverse()
-            .slice(0, this.state.show).map(vote =>
+        <div className="row">
+          <div className="col-6">
+            {upvotes.slice(0, this.state.show).map(vote =>
               <div className="LikesList__item" key={vote.voter}>
                 <ProfileTooltipOrigin username={vote.voter} >
                   <Link to={`/@${vote.voter}`}>
@@ -38,18 +42,35 @@ export default class LikesList extends Component {
                   </Link>
                 </ProfileTooltipOrigin>
                 {' '}
-                {vote.percent < 0
-                  ? <span className="text-danger">Disliked</span>
-                  : <span>
-                    Liked{' '}
-                    <span className="text-info">
-                      {numeral((vote.percent / 10000)).format('0%')}
-                    </span>
+                <span>
+                  Liked{' '}
+                  <span className="text-info">
+                    {numeral((vote.percent / 10000)).format('0%')}
                   </span>
-                }
+                </span>
               </div>
-          )
-        }
+            )}
+          </div>
+          <div className="col-6">
+            {downvotes.slice(0, this.state.show).map(vote =>
+              <div className="LikesList__item" key={vote.voter}>
+                <ProfileTooltipOrigin username={vote.voter} >
+                  <Link to={`/@${vote.voter}`}>
+                    <Avatar xs username={vote.voter} />
+                    {' '}
+                    {vote.voter}
+                  </Link>
+                </ProfileTooltipOrigin>
+                {' '}
+                <span className="text-danger">Disliked</span>
+                {' '}
+                <span className="text-info">
+                    {numeral((vote.percent / 10000)).format('0%')}
+                  </span>
+              </div>
+            )}
+          </div>
+        </div>
         { hasMore &&
           <a
             className="LikesList__showMore"
