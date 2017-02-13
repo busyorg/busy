4b89cb1b3f6d3e56b9a2e53ed8cd1fffaa023f44
p0o <pooria_az@ymail.com>
2017-02-13 21:08:03 +0800

    map array to keyed object for state.posts

--- src/post/postsReducers.js
+++ src/post/postsReducers.js
@@ -47,23 +47,28 @@ const postItem = (state = {}, action) => {
 };
 
 const posts = (state = {}, action) => {
+  let posts = {};
   switch (action.type) {
     case feedTypes.GET_FEED_CONTENT_SUCCESS:
     case feedTypes.GET_MORE_FEED_CONTENT_SUCCESS:
     case feedTypes.GET_USER_FEED_CONTENT_SUCCESS:
     case feedTypes.GET_MORE_USER_FEED_CONTENT_SUCCESS:
     case bookmarksActions.GET_BOOKMARKS_SUCCESS:
-      const posts = {};
       action.payload.postsData.forEach(post => posts[post.id] = post);
       return {
         ...state,
         ...posts,
       };
     case userActions.GET_USER_REPLIES_SUCCESS:
-    case userActions.GET_MORE_USER_REPLIES_SUCCESS:
       return {
         ...state,
         ...action.payload,
+      };
+    case userActions.GET_MORE_USER_REPLIES_SUCCESS:
+      action.payload.forEach((post) => { posts[post.id] = post; });
+      return {
+        ...state,
+        ...posts,
       };
     case postsActions.GET_CONTENT_SUCCESS:
       return {
