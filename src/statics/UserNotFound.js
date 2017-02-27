b56d44f56216c9f54bbcb46b2dda204e06883a65
Fabien <adcpm@users.noreply.github.com>
2017-02-27 16:26:15 +0700

    Add some margin

--- src/statics/UserNotFound.js
+++ src/statics/UserNotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 const UserNotFound = () =>
-  <div className="text-center">
+  <div className="text-center my-5">
     <h3>
       <FormattedMessage id="user_not_found" />
     </h3>
