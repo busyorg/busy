ce00b197f69ddde3b34b32a95c919d5989a511c2
adcpm <fabien@bonustrack.co>
2017-03-02 17:29:57 +0700

    Add Spanish language

--- src/translations/Translations.js
+++ src/translations/Translations.js
@@ -1,5 +1,6 @@
 import { addLocaleData } from 'react-intl';
 import en from 'react-intl/locale-data/en';
+import es from 'react-intl/locale-data/es';
 import zh from 'react-intl/locale-data/zh';
 import fr from 'react-intl/locale-data/fr';
 import de from 'react-intl/locale-data/de';
@@ -8,4 +9,4 @@ import ko from 'react-intl/locale-data/ko';
 import nl from 'react-intl/locale-data/nl';
 import se from 'react-intl/locale-data/se';
 
-addLocaleData([...en, ...zh, ...fr, ...de, ...ru, ...ko, ...nl, ...se]);
+addLocaleData([...en, ...es, ...zh, ...fr, ...de, ...ru, ...ko, ...nl, ...se]);
