66e72197d55fa64697b4477021101834bae17f3f
Nilesh Suthar <nileshsuthar@live.in>
2017-03-19 17:04:34 +0530

    Sort Items in post feed card

--- src/post/Body.js
+++ src/post/Body.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign,no-empty */
 import React from 'react';
 import _ from 'lodash';
 import embedjs from 'embedjs';
@@ -17,11 +18,9 @@ const remarkable = new Remarkable({
   quotes: '“”‘’'
 });
 
-export default (props) => {
-  const embeds = embedjs.getAll(props.body);
-  let body = props.body;
-  let jsonMetadata = {};
-  try { jsonMetadata = JSON.parse(props.jsonMetadata); } catch (e) { }
+export function getHtml(body, jsonMetadata = {}) {
+  const embeds = embedjs.getAll(body);
+  try { jsonMetadata = JSON.parse(jsonMetadata); } catch (e) { }
   jsonMetadata.image = jsonMetadata.image || [];
 
   body = body.replace(/<!--([\s\S]+?)(-->|$)/g, '(html comment removed: $1)');
@@ -46,7 +45,10 @@ export default (props) => {
   }
 
   body = sanitizeHtml(body, sanitizeConfig({}));
-  const bodyWithEmojis = emojione.shortnameToImage(body);
+  return emojione.shortnameToImage(body);
+}
 
+export default (props) => {
+  const bodyWithEmojis = getHtml(props.body, props.jsonMetadata);
   return <div dangerouslySetInnerHTML={{ __html: bodyWithEmojis }} />;
 };
