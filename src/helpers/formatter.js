26fe5e97c54a5bc98f63d4bd0dd5c4512336d3d5
adcpm <fabien@bonustrack.co>
2017-03-20 17:09:52 +0700

    Simplify jsonParse

--- src/helpers/formatter.js
+++ src/helpers/formatter.js
@@ -1,10 +1,8 @@
 
 export const jsonParse = (str) => {
-  let obj = {};
   try {
-    obj = JSON.parse(str);
-    obj = typeof obj === 'object' ? obj : JSON.parse(obj);
-    obj = typeof obj === 'object' ? obj : {};
-  } catch (e) { }
-  return obj;
+    return jsonParse(JSON.parse(str));
+  } catch (e) {
+    return str;
+  }
 };
