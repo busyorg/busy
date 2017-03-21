8a8c45ca3d2f37e35af7212f9f775e117cd29552
p0o <pooria_az@ymail.com>
2017-03-21 18:57:45 +0800

    no need for handling 404 errors on server

--- app.js
+++ app.js
@@ -42,36 +42,4 @@ app.use(cors());
 
 app.locals.env = process.env;
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
 module.exports = { app, server };
