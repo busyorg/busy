5d515dab98f2c8101a54fc42180767e6b57ad4d3
Nilesh Suthar <nileshsuthar@live.in>
2017-04-03 17:04:14 +0530

    Refactor

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
+/* eslint-disable new-cap,global-require,no-param-reassign */
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const http = require('http');
 const https = require('https');
+const cors = require('cors');
+
 http.globalAgent.maxSockets = Infinity;
 https.globalAgent.maxSockets = Infinity;
 
@@ -14,11 +16,9 @@ const app = express();
 const server = http.Server(app);
 const io = require('socket.io')(server);
 
-const cors = require('cors');
+const rootDir = path.join(__dirname, '..');
 
-if (process.env.NODE_ENV !== 'production') {
-  require('./webpack')(app);
-}
+if (process.env.NODE_ENV !== 'production') { require('../webpack')(app); }
 
 app.enable('trust proxy');
 app.set('view engine', 'ejs');
@@ -33,25 +33,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'public'), {
+  app.use(express.static(path.join(rootDir, 'public'), {
     maxAge: OneWeek,
   }));
 
-  app.get('/*', function (req, res) {
+  app.get('/*', (req, res) => {
     res.sendFile('index.html', {
-      root: __dirname + '/public/',
+      root: `${rootDir}/public/`,
     });
   });
 } else {
-  app.use(express.static(path.join(__dirname, 'assets')));
+  app.use(express.static(path.join(rootDir, 'assets')));
 
-  app.get('/*', function (req, res) {
+  app.get('/*', (req, res) => {
     res.sendFile('development_index.html', {
-      root: path.resolve(__dirname + '/templates/'),
+      root: path.resolve(`${rootDir}/templates/`),
     });
   });
 }
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(rootDir, 'node_modules')));
 
 app.use(cors());
 
