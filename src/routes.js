d9cb0b66bde3e3ce53d846dee4cca82d299e9856
adcpm <fabien@bonustrack.co>
2017-04-04 20:11:30 +0700

    Add route /signup /login

--- src/routes.js
+++ src/routes.js
@@ -23,6 +23,8 @@ import Drafts from './post/Write/Drafts';
 import About from './statics/About';
 import Help from './statics/Help';
 import Team from './statics/Team';
+import Signup from './auth/Signup';
+import Login from './auth/Login';
 import RequireLogin from './auth/RequireLogin';
 
 var MessagesUser = require('./messages/MessagesUser').default,
@@ -31,6 +33,8 @@ var MessagesUser = require('./messages/MessagesUser').default,
 export default (
   <Route path="/" component={Wrapper}>
     <IndexRoute component={Page} />
+    <Route path="/login" component={Login} />
+    <Route path="/signup" component={Signup} />
     <Route path="/help" component={Help} />
     <Route path="/about" component={About} />
     <Route path="/team" component={Team} />
