9c06e571112a0c3be21b2651e1d8399dc3076378
adcpm <fabien@bonustrack.co>
2017-04-04 20:11:11 +0700

    init Login and Signup form

---
+++ src/auth/Login.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
+import config from '../../config.json';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  onEmailChange = (e) => {
+    this.setState({ email: e.target.value });
+  };
+
+  onPasswordChange = (e) => {
+    this.setState({ password: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    fetch(`${config.api}/auth/login?email=${email}&password=${password}`)
+      .then(res => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+
+  render() {
+    return (
+      <div className="main-panel">
+        <h1 className="text-center my-5">
+          Log In
+        </h1>
+        <form
+          onSubmit={this.onSubmit}
+          className="container container-small"
+        >
+          <div className="form-group">
+            <input
+              onChange={this.onEmailChange}
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              onChange={this.onPasswordChange}
+              type="password"
+              className="form-control form-control-lg"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-group">
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-primary btn-lg">
+                Log In
+              </button>
+            </span>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
