60d5679386dec907a7cdfaaa1226c7ff8976f946
adcpm <fabien@bonustrack.co>
2017-04-05 05:20:51 +0700

    Display errors and add translations

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import fetch from 'isomorphic-fetch';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import config from '../../config.json';
 
+@injectIntl
 export default class Signup extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
       password: '',
+      emailErrors: [],
+      passwordErrors: [],
     };
   }
 
@@ -25,40 +29,58 @@ export default class Signup extends Component {
     fetch(`${config.api}/auth/signup?email=${email}&password=${password}`)
       .then(res => res.json())
       .then((json) => {
-        console.log(json);
+        if (!json.token) {
+          this.setState({
+            emailErrors: json.email || [],
+            passwordErrors: json.password || [],
+          });
+        } else {
+          alert('Success!');
+        }
       });
   };
 
   render() {
+    const { emailErrors, passwordErrors } = this.state;
     return (
       <div className="main-panel">
         <h1 className="text-center my-5">
-          Sign Up
+          <FormattedMessage id="signup" defaultMessage="Sign Up" />
         </h1>
         <form
           onSubmit={this.onSubmit}
           className="container container-small"
         >
-          <div className="form-group">
+          <div className={emailErrors[0] ? 'form-group has-danger' : 'form-group'}>
             <input
               onChange={this.onEmailChange}
               type="email"
               className="form-control form-control-lg"
-              placeholder="Enter email"
+              placeholder={
+                this.props.intl.formatMessage({ id: 'email', defaultMessage: 'Email address' })
+              }
             />
+            {emailErrors[0] &&
+              <div className="form-control-feedback">{emailErrors[0]}</div>
+            }
           </div>
-          <div className="form-group">
+          <div className={passwordErrors[0] ? 'form-group has-danger' : 'form-group'}>
             <input
               onChange={this.onPasswordChange}
               type="password"
               className="form-control form-control-lg"
-              placeholder="Password"
+              placeholder={
+                this.props.intl.formatMessage({ id: 'password', defaultMessage: 'Password' })
+              }
             />
+            {passwordErrors[0] &&
+              <div className="form-control-feedback">{passwordErrors[0]}</div>
+            }
           </div>
           <div className="form-group">
             <span className="input-group-btn">
               <button type="submit" className="btn btn-primary btn-lg">
-                Sign Up
+                <FormattedMessage id="signup" defaultMessage="Sign Up" />
               </button>
             </span>
           </div>
