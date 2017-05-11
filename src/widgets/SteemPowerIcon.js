7795e46ff0a93668f3119812f6c219f032835cba
adcpm <fabien@bonustrack.co>
2017-05-11 18:15:51 +0700

    Hide if not 100%

--- src/widgets/SteemPowerIcon.js
+++ src/widgets/SteemPowerIcon.js
@@ -3,18 +3,20 @@ import { injectIntl } from 'react-intl';
 import { SimpleTooltipOrigin } from '../widgets/tooltip/SimpleTooltip';
 
 const SteemPowerIcon = ({ active = true, className = '', intl }) =>
-  <SimpleTooltipOrigin
-    message={
-      intl.formatMessage({
-        id: 'full_steem_power',
-        defaultMessage: '100% Steem Power'
-      })}
-  >
-    <img
-      className={className}
-      style={{ height: '0.8em' }}
-      src="/img/steem.svg"
-    />
-  </SimpleTooltipOrigin>;
+  active
+    ? <SimpleTooltipOrigin
+      message={
+        intl.formatMessage({
+          id: 'full_steem_power',
+          defaultMessage: '100% Steem Power'
+        })}
+    >
+      <img
+        className={className}
+        style={{ height: '0.8em' }}
+        src="/img/steem.svg"
+      />
+    </SimpleTooltipOrigin>
+    : false;
 
 export default injectIntl(SteemPowerIcon);
