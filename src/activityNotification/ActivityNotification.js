50cccb5bdcf1f834762e3deeff37825e8852e721
p0o <pooria_az@ymail.com>
2017-05-22 02:44:45 +0800

    wire the component to show the data

--- src/activityNotification/ActivityNotification.js
+++ src/activityNotification/ActivityNotification.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { FormattedRelative } from 'react-intl';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+import fetch from 'isomorphic-fetch';
 import Avatar from '../widgets/Avatar';
+import * as activityNotificationsActions from './activityNotificationActions';
+import Loading from '../widgets/Loading'
 import './ActivityNotification.scss';
 
+@connect(
+  ({ activityNotification }) => ({
+    activityNotification,
+  }),
+  (dispatch) => bindActionCreators({
+    fetchActivityNotifications: activityNotificationsActions.fetchActivityNotifications,
+  }, dispatch)
+)
+@withRouter
 export default class ActivityNotification extends Component {
+  componentDidMount() {
+    const { activityNotification } = this.props;
+
+    if (!activityNotification.isFetching) {
+      this.props.fetchActivityNotifications();
+    }
+  }
+
+  handleClick = (e, notification) => {
+    e.stopPropagation();
+    this.props.router.push(notification.url);
+  }
+
   render() {
+    const { activityNotification } = this.props;
+
     return (
-      <div className="ActivityNotifList">
-        <div className="ActivityNotifList__item">
-          <Avatar
-            username="p0o"
-            sm
-            className="ActivityNotifList__item__photo"
-          />
-          <div className="ActivityNotifList__item__desc">
-            <p>p0o sent a new comment to you</p>
-            <i className="ActivityNotifList__item___date">10 minutes ago</i>
-          </div>
+      <div>
+        {activityNotification.isFetching && <Loading />}
+        <div className="ActivityNotifList">
+          {activityNotification.list && activityNotification.list.map((notification) =>
+            <div className="ActivityNotifList__item" onClick={e => { this.handleClick(e, notification) }}>
+              <Avatar
+                username={notification.from_username}
+                sm
+                className="ActivityNotifList__item__photo"
+              />
+              <div className="ActivityNotifList__item__desc">
+                <span>{notification.title}</span>
+                <p>{notification.body}</p>
+                <i className="ActivityNotifList__item___date">
+                  1 minute ago
+                </i>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     );
