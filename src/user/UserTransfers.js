3eb920db8fdb90f9cc11eb03c9238f04d19b6184
Nilesh Suthar <nileshsuthar@live.in>
2017-05-22 13:47:15 +0530

    Show reward to claim

--- src/user/UserTransfers.js
+++ src/user/UserTransfers.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { FormattedMessage } from 'react-intl';
@@ -12,6 +13,7 @@ import TransferHistory from './TransferHistory';
 @connect(
   state => ({
     app: state.app,
+    auth: state.auth,
     wallet: state.wallet,
   }),
   dispatch => bindActionCreators({
@@ -48,8 +50,49 @@ export default class UserTransfers extends Component {
         (parseFloat(account.balance) + parseFloat(power)))
         + parseFloat(account.sbd_balance);
     }
+
+    const isMyAccount = _.get(this.props.auth, 'user.name') === account.name;
+
+    // refer https://github.com/steemit/condenser/blob/b0fc795ed321d712a4c0b6656197d6f8ff952063/app/components/modules/UserWallet.jsx#L214
+    const rewardSteem = parseFloat(account.reward_steem_balance.split(' ')[0]) > 0 ? account.reward_steem_balance : null;
+    const rewardSbd = parseFloat(account.reward_sbd_balance.split(' ')[0]) > 0 ? account.reward_sbd_balance : null;
+    const rewardSp = parseFloat(account.reward_vesting_steem.split(' ')[0]) > 0 ? account.reward_vesting_steem.replace('STEEM', 'SP') : null;
+
+
+    const rewards = [];
+    if (rewardSteem) rewards.push(rewardSteem);
+    if (rewardSbd) rewards.push(rewardSbd);
+    if (rewardSp) rewards.push(rewardSp);
+
+    let rewardsStr;
+    switch (rewards.length) {
+      case 3:
+        rewardsStr = `${rewards[0]}, ${rewards[1]} and ${rewards[2]}`;
+        break;
+      case 2:
+        rewardsStr = `${rewards[0]} and ${rewards[1]}`;
+        break;
+      case 1:
+        rewardsStr = `${rewards[0]}`;
+        break;
+      default:
+        rewardsStr = '';
+    }
+
     return (
       <div className="container my-5">
+        {rewardsStr && <div style={{ textAlign: 'center', padding: '10px' }}>Current reward: {rewardsStr}
+          {isMyAccount && <button
+            style={{
+              margin: '0 10px',
+              borderRadius: '5px',
+              border: 'none',
+              padding: '5px 10px',
+              color: '#fff',
+              background: '#4275f4'
+            }}
+          >Claim Reward</button>}
+        </div>}
         {power ?
           <div className="ptl text-center">
             <ul className="row text-center">
