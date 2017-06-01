0cb6e4da3c88e9cad4f02cb4e6c41d7d83a78ce5
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-01 12:59:29 +0200

    Style InterestingPeople section using AntDesign

---
+++ src/components/Sidebar/User.js
@@ -0,0 +1,28 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import Avatar from '../Avatar';
+import Follow from '../Button/Follow';
+import './User.less';
+
+const User = ({ user }) =>
+  <div key={user.name}>
+    <div className="User__top">
+      <div className="User__links">
+        <Link to={`/${user.name}`}>
+          <Avatar name={user.name} size="34" />
+        </Link>
+        <Link to={`/${user.name}`}>
+          <b className="User__name">{user.name}</b>
+        </Link>
+      </div>
+      <Follow username={user.name} />
+    </div>
+    <div className="User__about">{user.about}</div>
+    <div className="User__divider" />
+  </div>;
+
+User.propTypes = {
+  user: PropTypes.shape(),
+};
+
+export default User;
