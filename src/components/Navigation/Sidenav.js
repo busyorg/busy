66231b042416bf39021fcdd3ff8ef29ae82c4321
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-02 12:49:51 +0200

    Style Sidenav

--- src/components/Navigation/Sidenav.js
+++ src/components/Navigation/Sidenav.js
@@ -1,36 +1,35 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
+import './Sidenav.less';
 
 const Sidenav = () =>
-  <nav className="Sidenav navbar">
-    <ul className="navbar-nav">
-      <li className="nav-item">
-        <Link className="nav-link" to="#">
-          <span className="icon-smile" /> My Profile
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="#">
-          <span className="icon-timer" /> Timeline
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/trending">
-          <span className="icon-calendar" /> News
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/messages">
-          <span className="icon-chat" /> Messages
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/wallet">
-          <span className="icon-touch_id" /> Wallet
-        </Link>
-      </li>
-    </ul>
-  </nav>;
+  <ul className="Sidenav">
+    <li className="Sidenav__item">
+      <Link to="#">
+        <span className="icon-smile" /> My Profile
+      </Link>
+    </li>
+    <li className="Sidenav__item">
+      <Link to="#">
+        <span className="icon-timer" /> Timeline
+      </Link>
+    </li>
+    <li className="Sidenav__item Sidenav__item--active">
+      <Link to="/trending">
+        <span className="icon-calendar" /> News
+      </Link>
+    </li>
+    <li className="Sidenav__item">
+      <Link to="/messages">
+        <span className="icon-chat" /> Messages
+      </Link>
+    </li>
+    <li className="Sidenav__item">
+      <Link to="/wallet">
+        <span className="icon-touch_id" /> Wallet
+      </Link>
+    </li>
+  </ul>;
 
 Sidenav.propTypes = {};
 
