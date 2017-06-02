dc24480244a78483ff13479af1667249f99b2923
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-02 11:59:41 +0200

    Add logo and input to Navigation

--- src/components/Navigation/Topnav.js
+++ src/components/Navigation/Topnav.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import { Menu, Popover, Icon } from 'antd';
+import { Menu, Popover, Icon, Input } from 'antd';
 import Avatar from '../Avatar';
 import './Topnav.less';
 
@@ -29,29 +29,37 @@ class Topnav extends React.Component {
   render() {
     const { username } = this.props;
     return (
-      <Menu
-        className="Topnav"
-        mode="horizontal"
-        onOpenChange={openKeys => this.onNotificationChange(openKeys)}
-      >
-        <Menu.Item key="user" className="Topnav__item">
-          <Link className="Topnav__user" to={`/@${username}`}>
-            <Avatar username={username} size={36} />
-            <span>{username}</span>
-          </Link>
-        </Menu.Item>
-        <SubMenu
-          key="notifications"
-          className="Topnav__item Topnav__notifications-menu"
-          title={
-            <Popover visible={this.state.notificationsVisible} content={<span>Notifications</span>} title="Notifications"><span className="icon-beanie" /></Popover>
-          }
-        />
-        <SubMenu key="more" className="Topnav__item" title={<Icon type="ellipsis" />}>
-          <Menu.Item key="more:1">Option 1</Menu.Item>
-          <Menu.Item key="more:2">Option 2</Menu.Item>
-        </SubMenu>
-      </Menu>
+      <div className="Topnav">
+        <div className="Topnav__container">
+          <span className="Topnav__brand">busy</span>
+          <Input placeholder="Search..." className="Topnav__input" />
+          <div className="Topnav__menu-container">
+            <Menu
+              className="Topnav__menu"
+              mode="horizontal"
+              onOpenChange={openKeys => this.onNotificationChange(openKeys)}
+            >
+              <Menu.Item key="user" className="Topnav__item">
+                <Link className="Topnav__user" to={`/@${username}`}>
+                  <Avatar username={username} size={36} />
+                  <span>{username}</span>
+                </Link>
+              </Menu.Item>
+              <SubMenu
+                key="notifications"
+                className="Topnav__item Topnav__item--dropdown"
+                title={
+                  <Popover visible={this.state.notificationsVisible} content={<span>Notifications</span>} title="Notifications"><span className="icon-beanie" /></Popover>
+                }
+              />
+              <SubMenu key="more" className="Topnav__item Topnav__item--dropdown" title={<Icon type="ellipsis" />}>
+                <Menu.Item key="more:1">Option 1</Menu.Item>
+                <Menu.Item key="more:2">Option 2</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </div>
+        </div>
+      </div>
     );
   }
 }
