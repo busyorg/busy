5589e09760f70a16737b582e9ef9d1d5552eb2f1
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-02 12:11:52 +0200

    Add unlogged user menu

--- src/components/Navigation/Topnav.js
+++ src/components/Navigation/Topnav.js
@@ -28,36 +28,60 @@ class Topnav extends React.Component {
 
   render() {
     const { username } = this.props;
+
+    let content;
+
+    if (username) {
+      content = (
+        <div className="Topnav__menu-container">
+          <Menu
+            className="Topnav__menu"
+            mode="horizontal"
+            onOpenChange={openKeys => this.onNotificationChange(openKeys)}
+          >
+            <Menu.Item key="user" className="Topnav__item">
+              <Link className="Topnav__user" to={`/@${username}`}>
+                <Avatar username={username} size={36} />
+                <span>{username}</span>
+              </Link>
+            </Menu.Item>
+            <SubMenu
+              key="notifications"
+              className="Topnav__item Topnav__item--dropdown"
+              title={
+                <Popover visible={this.state.notificationsVisible} content={<span>Notifications</span>} title="Notifications"><span className="icon-beanie" /></Popover>
+              }
+            />
+            <SubMenu key="more" className="Topnav__item Topnav__item--dropdown" title={<Icon type="ellipsis" />}>
+              <Menu.Item key="more:1">Option 1</Menu.Item>
+              <Menu.Item key="more:2">Option 2</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </div>);
+    } else {
+      content = (
+        <div className="Topnav__menu-container">
+          <Menu className="Topnav__menu" mode="horizontal">
+            <Menu.Item key="signin" className="Topnav__item">
+              <Link to="/signin">
+                Sign in
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="signup" className="Topnav__item">
+              <Link to="/signup">
+                Sign up
+              </Link>
+            </Menu.Item>
+          </Menu>
+        </div>);
+    }
+
     return (
       <div className="Topnav">
         <div className="Topnav__container">
           <span className="Topnav__brand">busy</span>
           <Input placeholder="Search..." className="Topnav__input" />
-          <div className="Topnav__menu-container">
-            <Menu
-              className="Topnav__menu"
-              mode="horizontal"
-              onOpenChange={openKeys => this.onNotificationChange(openKeys)}
-            >
-              <Menu.Item key="user" className="Topnav__item">
-                <Link className="Topnav__user" to={`/@${username}`}>
-                  <Avatar username={username} size={36} />
-                  <span>{username}</span>
-                </Link>
-              </Menu.Item>
-              <SubMenu
-                key="notifications"
-                className="Topnav__item Topnav__item--dropdown"
-                title={
-                  <Popover visible={this.state.notificationsVisible} content={<span>Notifications</span>} title="Notifications"><span className="icon-beanie" /></Popover>
-                }
-              />
-              <SubMenu key="more" className="Topnav__item Topnav__item--dropdown" title={<Icon type="ellipsis" />}>
-                <Menu.Item key="more:1">Option 1</Menu.Item>
-                <Menu.Item key="more:2">Option 2</Menu.Item>
-              </SubMenu>
-            </Menu>
-          </div>
+          {content}
         </div>
       </div>
     );
