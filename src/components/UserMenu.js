f249b4fe57feb1266faad1254c6fe5b4ff96c612
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-05 15:16:04 +0200

    Add current selection to state

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -2,36 +2,55 @@ import React from 'react';
 import { Menu } from 'antd';
 import './UserMenu.less';
 
-const UserMenu = () =>
-  <div className="UserMenu">
-    <Menu
-      className="UserMenu__menu"
-      selectedKeys={['discussions']}
-      mode="horizontal"
-    >
-      <Menu.Item key="discussions" className="UserMenu__item">
-        Discussions
-        <span className="UserMenu__badge">765</span>
-      </Menu.Item>
-      <Menu.Item key="comments" className="UserMenu__item">
-        <strong>
-          Comments
-          <span className="UserMenu__badge">54</span>
-        </strong>
-      </Menu.Item>
-      <Menu.Item key="followers" className="UserMenu__item">
-        <strong>
-          Followers
-          <span className="UserMenu__badge">1,350</span>
-        </strong>
-      </Menu.Item>
-      <Menu.Item key="following" className="UserMenu__item">
-        <strong>
-          Following
-          <span className="UserMenu__badge">250</span>
-        </strong>
-      </Menu.Item>
-    </Menu>
-  </div>;
+class UserMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 'discussions',
+    };
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      current: e.key,
+    });
+  }
+
+  render() {
+    return (
+      <div className="UserMenu">
+        <Menu
+          className="UserMenu__menu"
+          onClick={this.handleClick}
+          selectedKeys={[this.state.current]}
+          mode="horizontal"
+        >
+          <Menu.Item key="discussions" className="UserMenu__item">
+            Discussions
+            <span className="UserMenu__badge">765</span>
+          </Menu.Item>
+          <Menu.Item key="comments" className="UserMenu__item">
+            <strong>
+              Comments
+              <span className="UserMenu__badge">54</span>
+            </strong>
+          </Menu.Item>
+          <Menu.Item key="followers" className="UserMenu__item">
+            <strong>
+              Followers
+              <span className="UserMenu__badge">1,350</span>
+            </strong>
+          </Menu.Item>
+          <Menu.Item key="following" className="UserMenu__item">
+            <strong>
+              Following
+              <span className="UserMenu__badge">250</span>
+            </strong>
+          </Menu.Item>
+        </Menu>
+      </div>
+    );
+  }
+}
 
 export default UserMenu;
