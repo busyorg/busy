8c72728c74233437650fc08a47eaafff89e8c77f
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-06 18:27:57 +0200

    Add Notifications styles

--- src/components/Navigation/Notifications.js
+++ src/components/Navigation/Notifications.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Avatar from '../Avatar';
 import './Notifications.less';
 
 const Notifications = () =>
   <div className="Notifications">
-    Notifications content
+    <div className="Notifications__notification Notifications__notification--unread">
+      <Avatar username="guest123" size={40} />
+      <div className="Notifications__notification__text">
+        <div className="Notifications__notification__text__message">
+          <a>guest123</a> is now following you.
+        </div>
+        <div className="Notifications__notification__text__date">
+          2 days ago
+        </div>
+      </div>
+    </div>
+    <div className="Notifications__notification">
+      <Avatar username="guest123" size={40} />
+      <div className="Notifications__notification__text">
+        <div className="Notifications__notification__text__message">
+          <a>guest123</a> is now following you.
+        </div>
+        <div className="Notifications__notification__text__date">
+          2 days ago
+        </div>
+      </div>
+    </div>
   </div>;
 
 export default Notifications;
