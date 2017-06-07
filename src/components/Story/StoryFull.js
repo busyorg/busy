7883bdc3eaa71c69631298ad923e1ac255e79b6e
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-07 20:11:11 +0200

    Add lightbox to StoryFull

--- src/components/Story/StoryFull.js
+++ src/components/Story/StoryFull.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { FormattedRelative } from 'react-intl';
 import { Link } from 'react-router';
 import { Menu } from 'antd';
+import Lightbox from 'react-image-lightbox';
 import Body from '../../post/Body';
 import StoryFooter from './StoryFooter';
 import Avatar from '../Avatar';
@@ -33,6 +34,16 @@ class StoryFull extends React.Component {
     userFollowed: false,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      lightbox: {
+        open: false,
+        index: 0,
+      },
+    };
+  }
+
   handleClick = (e) => {
     switch (e.key) {
       case 'follow':
@@ -49,6 +60,22 @@ class StoryFull extends React.Component {
     }
   }
 
+  onContentClick = (e) => {
+    if (e.target.tagName === 'IMG') {
+      const tags = this.contentDiv.getElementsByTagName('img');
+      for (let i = 0; i < tags.length; i++) {
+        if (tags[i] === e.target) {
+          this.setState({
+            lightbox: {
+              open: true,
+              index: i,
+            },
+          });
+        }
+      }
+    }
+  }
+
   render() {
     const {
       post,
@@ -58,6 +85,9 @@ class StoryFull extends React.Component {
       onCommentClick,
       onShareClick
     } = this.props;
+
+    const { open, index } = this.state.lightbox;
+    const images = JSON.parse(post.json_metadata).image;
 
     return (
       <div className="StoryFull">
@@ -87,9 +117,36 @@ class StoryFull extends React.Component {
             </SubMenu>
           </Menu>
         </div>
-        <div className="StoryFull__content">
+        <div className="StoryFull__content" ref={(div) => { this.contentDiv = div; }} onClick={this.onContentClick}>
           <Body body={post.body} json_metadata={post.json_metadata} />
         </div>
+        {
+          open && <Lightbox
+            mainSrc={images[index]}
+            nextSrc={images[(index + 1) % images.length]}
+            prevSrc={images[(index + (images.length - 1)) % images.length]}
+            onCloseRequest={() => {
+              this.setState({
+                lightbox: {
+                  ...this.state.lightbox,
+                  open: false,
+                },
+              });
+            }}
+            onMovePrevRequest={() => this.setState({
+              lightbox: {
+                ...this.state.lightbox,
+                index: (index + (images.length - 1)) % images.length,
+              },
+            })}
+            onMoveNextRequest={() => this.setState({
+              lightbox: {
+                ...this.state.lightbox,
+                index: (index + (images.length + 1)) % images.length,
+              },
+            })}
+          />
+        }
         <StoryFooter
           post={post}
           onLikeClick={onLikeClick}
