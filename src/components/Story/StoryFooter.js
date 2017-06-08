d13bf4c07be6a03c2931237b780f89bdfa994290
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-08 11:03:40 +0200

    Add PayoutDetail tooltip

--- src/components/Story/StoryFooter.js
+++ src/components/Story/StoryFooter.js
@@ -1,6 +1,43 @@
 import React, { PropTypes } from 'react';
 import numeral from 'numeral';
+import _ from 'lodash';
+import { FormattedRelative } from 'react-intl';
+import { Tooltip } from 'antd';
+import { calculatePayout } from '../../helpers/steemitHelpers';
 import './StoryFooter.less';
+
+const AmountWithLabel = ({ label, amount }) => (
+  _.isNumber(amount)
+    ? <div>{label}: {numeral(amount).format('$0,0.00')}</div>
+    : null
+);
+
+const PayoutDetail = ({ post }) => {
+  const {
+    payoutLimitHit,
+    potentialPayout,
+    promotionCost,
+    cashoutInTime,
+    isPayoutDeclined,
+    maxAcceptedPayout,
+    pastPayouts,
+    authorPayouts,
+    curatorPayouts,
+  } = calculatePayout(post);
+
+  return (<div>
+    {payoutLimitHit && <div>Payout limit reached on this post</div>}
+    <AmountWithLabel label="Potential Payout" amount={potentialPayout} />
+    <AmountWithLabel label="Promoted" amount={promotionCost} />
+    {!isPayoutDeclined && cashoutInTime &&
+      <div>Will release <FormattedRelative value={cashoutInTime} /></div>}
+    {isPayoutDeclined && <div>Declined Payout</div>}
+    <AmountWithLabel label="Max Accepted Payout" amount={maxAcceptedPayout} />
+    <AmountWithLabel label="Total Past Payouts" amount={pastPayouts} />
+    <AmountWithLabel label="Authors Payout" amount={authorPayouts} />
+    <AmountWithLabel label="Curators Payout" amount={curatorPayouts} />
+  </div>);
+};
 
 const StoryFooter = ({ post, onLikeClick, onDislikeClick, onCommentClick, onShareClick }) => {
   const payout = parseFloat(post.pending_payout_value) || parseFloat(post.total_payout_value);
@@ -10,7 +47,14 @@ const StoryFooter = ({ post, onLikeClick, onDislikeClick, onCommentClick, onShar
   const commentsValue = numeral(post.children).format('0,0');
 
   return (<div className="StoryFooter">
-    <span className="StoryFooter__payout">{payoutValue}</span>
+    <span className="StoryFooter__payout">
+      <Tooltip
+        title={<PayoutDetail post={post} />}
+        placement="bottom"
+      >
+        {payoutValue}
+      </Tooltip>
+    </span>
     <a className="StoryFooter__link" onClick={() => onLikeClick()}>
       <i className="iconfont icon-praise_fill" />
       <span className="StoryFooter__number">{likesValue}</span>
