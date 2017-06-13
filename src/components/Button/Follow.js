d3ed4607634ceffb20b7f5016b3f07f5b5bafa0e
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-13 11:08:55 +0200

    Add unfollow text on Following button on hover

--- src/components/Button/Follow.js
+++ src/components/Button/Follow.js
@@ -1,16 +1,37 @@
 import React, { PropTypes } from 'react';
 import './Follow.less';
 
-const Follow = ({ isFollowed }) =>
-  <button className="Follow">
-    {isFollowed
-      ? 'Following'
-      : 'Follow'
-    }
-  </button>;
+class Follow extends React.Component {
+  static propTypes = {
+    isFollowed: PropTypes.bool
+  };
 
-Follow.propTypes = {
-  isFollowed: PropTypes.bool
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHovered: false,
+    };
+  }
+
+  onMouseOver = () => this.setState({ isHovered: true })
+
+  onMouseOut = () => this.setState({ isHovered: false })
+
+  render() {
+    const { isFollowed } = this.props;
+    const { isHovered } = this.state;
+
+    const followingText = (isHovered ? 'Unfollow' : 'Following');
+
+    return (
+      <button className="Follow" onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
+        {isFollowed
+          ? followingText
+          : 'Follow'
+        }
+      </button>
+    );
+  }
+}
 
 export default Follow;
