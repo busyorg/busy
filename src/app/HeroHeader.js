519fb9da77b58f372432694f384d11d6d553b4c7
Nilesh Suthar <nileshsuthar@live.in>
2017-06-13 16:45:15 +0530

    Show userheader

--- src/app/HeroHeader.js
+++ src/app/HeroHeader.js
@@ -1,13 +1,16 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import UserHeader from '../components/UserHeader';
 import Hero from '../components/Hero';
 
-const HeroHeader = ({ auth, style }) => {
-  console.log('props HeroHeader', auth);
-  return (
-    <div style={style}>
-      {auth.user.name === undefined ? <Hero /> : null}
-    </div>
+const HeroHeader = ({ auth, style }) => (
+  <div style={style}>
+    <Switch>
+      <Route path="/@:name" render={({ match }) => <UserHeader username={match.params.name} />} />
+      <Route render={() => (auth.user.name === undefined ? <Hero /> : <div />)} />
+    </Switch>
+  </div>
   );
-};
 
 export default HeroHeader;
