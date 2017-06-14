2a1a915426f2f7184fe153c9507b9b9d3e756f09
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-14 15:53:03 +0200

    Add Heading dropdown

--- src/components/Editor/EditorToolbar.js
+++ src/components/Editor/EditorToolbar.js
@@ -1,46 +1,58 @@
 import React, { PropTypes } from 'react';
-import { Button, Tooltip } from 'antd';
+import { Button, Tooltip, Menu, Dropdown, Icon } from 'antd';
 import './EditorToolbar.less';
 
 const tooltip = (description, shortcut) =>
   <span>{description}<br /><b>{shortcut}</b></span>;
 
-const EditorToolbar = ({ onSelect }) =>
-  <div className="EditorToolbar">
-    <Tooltip title={tooltip('Add heading 1', 'Ctrl+Shift+1')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('h1')}>h1</Button>
-    </Tooltip>
-    <Tooltip title={tooltip('Add heading 2', 'Ctrl+Shift+2')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('h2')}>h2</Button>
-    </Tooltip>
-    <Tooltip title={tooltip('Add heading 3', 'Ctrl+Shift+3')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('h3')}>h3</Button>
-    </Tooltip>
-    <Tooltip title={tooltip('Add heading 4', 'Ctrl+Shift+4')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('h4')}>h4</Button>
-    </Tooltip>
-    <Tooltip title={tooltip('Add heading 5', 'Ctrl+Shift+5')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('h5')}>h5</Button>
-    </Tooltip>
-    <Tooltip title={tooltip('Add heading 6', 'Ctrl+Shift+6')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('h6')}>h6</Button>
-    </Tooltip>
-    <Tooltip title={tooltip('Add bold', 'Ctrl+b')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('b')}>b</Button>
-    </Tooltip>
-    <Tooltip title={tooltip('Add italic', 'Ctrl+i')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('i')}>i</Button>
-    </Tooltip>
-    <Tooltip title={tooltip('Add quote', 'Ctrl+q')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('q')}>q</Button>
-    </Tooltip>
-    <Tooltip title={tooltip('Add link', 'Ctrl+k')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('link')}>Link</Button>
-    </Tooltip>
-    <Tooltip title={tooltip('Add image', 'Ctrl+m')}>
-      <Button className="EditorToolbar__button" onClick={() => onSelect('image')}>Image</Button>
-    </Tooltip>
-  </div>;
+const EditorToolbar = ({ onSelect }) => {
+  const menu = (
+    <Menu onClick={e => onSelect(e.key)}>
+      <Menu.Item key="h1">
+        Heading 1
+      </Menu.Item>
+      <Menu.Item key="h2">
+        Heading 2
+      </Menu.Item>
+      <Menu.Item key="h3">
+        Heading 3
+      </Menu.Item>
+      <Menu.Item key="h4">
+        Heading 4
+      </Menu.Item>
+      <Menu.Item key="h5">
+        Heading 5
+      </Menu.Item>
+      <Menu.Item key="h6">
+        Heading 6
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <div className="EditorToolbar">
+      <Dropdown overlay={menu}>
+        <Button>
+          Heading <Icon type="down" />
+        </Button>
+      </Dropdown>
+      <Tooltip title={tooltip('Add bold', 'Ctrl+b')}>
+        <Button className="EditorToolbar__button" onClick={() => onSelect('b')}>b</Button>
+      </Tooltip>
+      <Tooltip title={tooltip('Add italic', 'Ctrl+i')}>
+        <Button className="EditorToolbar__button" onClick={() => onSelect('i')}>i</Button>
+      </Tooltip>
+      <Tooltip title={tooltip('Add quote', 'Ctrl+q')}>
+        <Button className="EditorToolbar__button" onClick={() => onSelect('q')}>q</Button>
+      </Tooltip>
+      <Tooltip title={tooltip('Add link', 'Ctrl+k')}>
+        <Button className="EditorToolbar__button" onClick={() => onSelect('link')}>Link</Button>
+      </Tooltip>
+      <Tooltip title={tooltip('Add image', 'Ctrl+m')}>
+        <Button className="EditorToolbar__button" onClick={() => onSelect('image')}>Image</Button>
+      </Tooltip>
+    </div>);
+};
 
 EditorToolbar.propTypes = {
   onSelect: PropTypes.func,
