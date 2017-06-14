fd57ac84a85f94bdb7d12ee80ab53651584b0130
p0o <pooria_az@ymail.com>
2017-06-14 21:50:01 +0800

    group notifications based on notificationId

--- src/activityNotification/ActivityNotification.js
+++ src/activityNotification/ActivityNotification.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { FormattedRelative } from 'react-intl';
 import { withRouter, Link } from 'react-router';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import fetch from 'isomorphic-fetch';
 import Avatar from '../widgets/Avatar';
 import * as activityNotificationsActions from './activityNotificationActions';
 import Loading from '../widgets/Loading'
+import { makeNotificationTitle, uniqNotifications } from './activityNotificationHelpers';
 import './ActivityNotification.scss';
 
 @connect(
@@ -34,12 +36,15 @@ export default class ActivityNotification extends Component {
 
   render() {
     const { activityNotification } = this.props;
+    const { list, isFetching } = activityNotification;
+
+    const listByNotificationId = _.groupBy(list, 'notification_id');
 
     return (
       <div>
-        {activityNotification.isFetching && <Loading />}
+        {isFetching && <Loading />}
         <div className="ActivityNotifList">
-          {activityNotification.list && activityNotification.list.map((notification) =>
+          {list && uniqNotifications(list).map((notification) =>
             <div className="ActivityNotifList__item" onClick={e => { this.handleClick(e, notification) }}>
               <Avatar
                 username={notification.from_username}
@@ -47,7 +52,7 @@ export default class ActivityNotification extends Component {
                 className="ActivityNotifList__item__photo"
               />
               <div className="ActivityNotifList__item__desc">
-                <span>{notification.title}</span>
+                <span>{makeNotificationTitle(notification, listByNotificationId)}</span>
                 <p>{notification.body}</p>
                 <i className="ActivityNotifList__item___date">
                   1 minute ago
