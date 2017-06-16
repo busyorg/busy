9934b9ce3dabc22224571f2431b796ca3eb3527d
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-16 10:36:05 +0200

    Force trailing comma on multiline

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     "react/sort-comp": 0,
     "react/prop-types": 0,
     "no-underscore-dangle": 0,
-    "comma-dangle": 0,
+    "comma-dangle": ["error", "always-multiline"],
     "no-static-element-interactions": 0,
     "no-plusplus": 0,
     "no-continue": 0,
