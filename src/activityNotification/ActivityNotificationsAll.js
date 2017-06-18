8395250fcfcf40f4bb790756b3e5a3c34e62da2e
p0o <pooria_az@ymail.com>
2017-06-18 19:44:57 +0800

    add notification grouping feature

--- src/activityNotification/ActivityNotificationsAll.js
+++ src/activityNotification/ActivityNotificationsAll.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { FormattedRelative } from 'react-intl';
 import { withRouter, Link } from 'react-router';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import fetch from 'isomorphic-fetch';
 import Avatar from '../widgets/Avatar';
 import * as activityNotificationsActions from './activityNotificationActions';
+import { makeNotificationTitle, uniqNotifications } from './activityNotificationHelpers';
+import ActivityNotificationAllItem from './ActivityNotificationAllItem';
 import Loading from '../widgets/Loading'
 import './ActivityNotification.scss';
 
@@ -14,45 +17,30 @@ import './ActivityNotification.scss';
     activityNotification,
   }),
   (dispatch) => bindActionCreators({
+    markAsSeen: activityNotificationsActions.markAsSeen,
     fetchActivityNotifications: activityNotificationsActions.fetchActivityNotifications,
   }, dispatch)
 )
 @withRouter
-export default class ActivityNotification extends Component {
+export default class ActivityNotificationAll extends Component {
   componentDidMount() {
     this.props.fetchActivityNotifications(50);
   }
 
-  handleClick = (e, notification) => {
-    e.stopPropagation();
-    this.props.router.push(notification.url);
-  }
-
   render() {
     const { activityNotification } = this.props;
+    const { list } = activityNotification;
+    const listByNotificationId = _.groupBy(list, 'notification_id');
 
     return (
       <div className="main-panel">
         <div className="ActivityNotifList ActivityNotifList--all">
-          {activityNotification.list && activityNotification.list.map((notification) =>
-            <div className="ActivityNotifList__item" >
-              <Link to={`/@${notification.from_username}`}>
-                <Avatar
-                  username={notification.from_username}
-                  sm
-                  className="ActivityNotifList__item__photo"
-                />
-              </Link>
-              <div className="ActivityNotifList__item__desc">
-                <span>
-                  <Link to={notification.url}>{notification.title}</Link>
-                </span>
-                <p>{notification.body}</p>
-                <i className="ActivityNotifList__item___date">
-                  1 minute ago
-                </i>
-              </div>
-            </div>
+           {list && uniqNotifications(list).map((notification) =>
+            <ActivityNotificationAllItem
+              listByNotificationId={listByNotificationId}
+              notification={notification}
+              markAsSeen={this.props.markAsSeen}
+            />
           )}
         </div>
       </div>
