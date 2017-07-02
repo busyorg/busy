b7aedb853195d0a6fb9b67b1b704e4e37ef690d3
Nilesh Suthar <nileshsuthar@live.in>
2017-07-02 11:06:59 +0530

    Added pm2 config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: pm2 startOrReload ecosystem.config.js --update-env && pm2 logs all
---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'busy',
+      script: 'busy.server.js',
+      instances: 4,
+      exec_mode: 'cluster',
+      max_memory_restart: '500M'
+    }
+  ]
+};
