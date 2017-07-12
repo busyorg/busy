b42c1f744b7eb9676f130ef850f92d8c282322cd
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-07-12 13:16:28 +0200

    Add webpack-dev-server for development

---
+++ webpack/webpack-dev-server.js
@@ -0,0 +1,77 @@
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const postcss_flexbugs = require('postcss-flexbugs-fixes');
+
+const baseDir = path.resolve(__dirname, '..');
+
+module.exports = {
+  entry: path.resolve(baseDir, './src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(baseDir, 'dist'),
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+        ENABLE_LOGGER: JSON.stringify(process.env.ENABLE_LOGGER),
+        BUSYWS_HOST: JSON.stringify(process.env.BUSYWS_HOST || 'https://ws.busy.org'),
+        STEEMCONNECT_IMG_HOST: JSON.stringify(process.env.STEEMCONNECT_IMG_HOST || 'https://img.steemconnect.com'),
+        SENTRY_PUBLIC_DSN: null,
+        STEEMCONNECT_HOST: JSON.stringify(process.env.STEEMCONNECT_HOST || 'https://v2.steemconnect.com'),
+        STEEMCONNECT_REDIRECT_URL: JSON.stringify(process.env.STEEMCONNECT_REDIRECT_URL || 'https://busy.org/callback'),
+        WS: JSON.stringify(process.env.WS || 'wss://steemd.steemit.com'),
+        IS_BROWSER: JSON.stringify(true),
+        PUSHPAD_PROJECT_ID: process.env.PUSHPAD_PROJECT_ID,
+        BUSYPUSH_ENDPOINT: process.env.BUSYPUSH_ENDPOINT,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|svg)(\?.+)?$/,
+        loader: 'url-loader',
+      },
+      {
+        test: /\.css|.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+              plugins: () => [
+                postcss_flexbugs,
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                }),
+              ],
+            },
+          },
+          'less-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    contentBase: path.resolve(baseDir, 'dist'),
+    historyApiFallback: true,
+    proxy: {
+      '/callback': 'http://localhost:3001',
+    },
+  },
+};
