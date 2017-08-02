29a1bba5cf53efbf042a4c5601d369332fceaa3e
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-08-02 18:13:51 +0200

    Clean eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,23 +14,4 @@ module.exports = {
     "jest": true,
   },
   "extends": "airbnb",
-  "rules": {
-    "no-func-assign": 0,
-    "no-class-assign": 0,
-    "react/jsx-filename-extension": 0,
-    "react/sort-comp": 0,
-    "react/prop-types": 0,
-    "no-underscore-dangle": 0,
-    "comma-dangle": ["error", "always-multiline"],
-    "jsx-a11y/no-static-element-interactions": 0,
-    "react/forbid-prop-types": 0,
-    "no-plusplus": 0,
-    "no-continue": 0,
-    "camelcase": 0
-  },
-  "plugins": [
-    "react",
-    "jest",
-    "import"
-  ]
 };
