ff01b4b57f96866a9c40511b21b0a407a6faa986
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-08-03 08:25:29 +0200

    Update eslintrc style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaVersion": 7,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true,
-      "experimentalObjectRestSpread": true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
     },
   },
-  "env": {
-    "node": true,
-    "browser": true,
-    "jest": true,
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
   },
-  "extends": "airbnb",
+  extends: 'airbnb',
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
+  },
 };
