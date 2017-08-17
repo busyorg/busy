ab883c363e76ed721fa8eb6072a8a020ee3cadba
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-08-17 16:20:26 +0200

    Add comment sorting to subcomments

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,46 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { sortComments } from '../../helpers/sortHelpers';
 import Loading from '../Icon/Loading';
 import CommentForm from './CommentForm';
 import Comment from './Comment';
 import './Comments.less';
-
-const sortComments = (comments, sortType = 'BEST') => {
-  const sortedComments = [...comments];
-
-  const netNegative = a => a.net_rshares < 0;
-  const totalPayout = a =>
-    parseFloat(a.pending_payout_value) + parseFloat(a.total_payout_value)
-      + parseFloat(a.curator_payout_value);
-  const netRshares = a => a.net_rshares;
-
-  switch (sortType) {
-    case 'BEST':
-      return sortedComments.sort((a, b) => {
-        if (netNegative(a)) {
-          return 1;
-        } else if (netNegative(b)) {
-          return -1;
-        }
-
-        const aPayout = totalPayout(a);
-        const bPayout = totalPayout(b);
-
-        if (aPayout !== bPayout) {
-          return bPayout - aPayout;
-        }
-
-        return netRshares(b) - netRshares(a);
-      });
-    case 'NEWEST':
-      return sortedComments.sort((a, b) => Date.parse(a.created) - Date.parse(b.created)).reverse();
-    case 'OLDEST':
-      return sortedComments.sort((a, b) => Date.parse(a.created) - Date.parse(b.created));
-    default:
-      return sortedComments;
-  }
-};
 
 class Comments extends React.Component {
   static propTypes = {
@@ -170,6 +135,7 @@ class Comments extends React.Component {
               authenticated={authenticated}
               username={username}
               comment={comment}
+              sort={sort}
               rootPostAuthor={this.props.parentPost && this.props.parentPost.author}
               commentsChildren={commentsChildren}
               onLikeClick={onLikeClick}
---
+++ src/helpers/sortHelpers.js
@@ -0,0 +1,37 @@
+export const sortComments = (comments, sortType = 'BEST') => {
+  const sortedComments = [...comments];
+
+  const netNegative = a => a.net_rshares < 0;
+  const totalPayout = a =>
+    parseFloat(a.pending_payout_value) + parseFloat(a.total_payout_value)
+      + parseFloat(a.curator_payout_value);
+  const netRshares = a => a.net_rshares;
+
+  switch (sortType) {
+    case 'BEST':
+      return sortedComments.sort((a, b) => {
+        if (netNegative(a)) {
+          return 1;
+        } else if (netNegative(b)) {
+          return -1;
+        }
+
+        const aPayout = totalPayout(a);
+        const bPayout = totalPayout(b);
+
+        if (aPayout !== bPayout) {
+          return bPayout - aPayout;
+        }
+
+        return netRshares(b) - netRshares(a);
+      });
+    case 'NEWEST':
+      return sortedComments.sort((a, b) => Date.parse(a.created) - Date.parse(b.created)).reverse();
+    case 'OLDEST':
+      return sortedComments.sort((a, b) => Date.parse(a.created) - Date.parse(b.created));
+    default:
+      return sortedComments;
+  }
+};
+
+export default null;
