333bda5493d2b99bba99837d89fc15c9e4b6aff7
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-08-22 12:18:25 +0200

    Fix rank condition

--- src/helpers/ranks.js
+++ src/helpers/ranks.js
@@ -1,14 +1,14 @@
 export const getUserRank = (vestingShares) => {
   const vests = parseFloat(vestingShares, 10);
   let rank = 'Plankton';
-  if (vests >= 1000000) {
-    rank = 'Minnow';
-  } else if (vests >= 10000000) {
-    rank = 'Dolphin';
+  if (vests >= 1000000000) {
+    rank = 'Whale';
   } else if (vests >= 100000000) {
     rank = 'Orca';
-  } else if (vests >= 1000000000) {
-    rank = 'Whale';
+  } else if (vests >= 10000000) {
+    rank = 'Dolphin';
+  } else if (vests >= 1000000) {
+    rank = 'Minnow';
   }
   return rank;
 };
