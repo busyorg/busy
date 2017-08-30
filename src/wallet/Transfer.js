c3404710f4e3c5eca2c03ba34415deb79335a0c9
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-08-30 15:46:16 +0200

    Add Transfer translations

--- src/wallet/Transfer.js
+++ src/wallet/Transfer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { injectIntl, FormattedMessage } from 'react-intl';
 import steemConnect from 'sc2-sdk';
 import { Form, Input, Radio, Modal } from 'antd';
 import './Transfer.less';
@@ -8,6 +9,7 @@ import './Transfer.less';
 import { closeTransfer } from './walletActions';
 import { getAuthenticatedUser } from '../reducers';
 
+@injectIntl
 @connect(state => ({
   visible: state.wallet.transferVisible,
   to: state.wallet.transferTo,
@@ -18,6 +20,7 @@ import { getAuthenticatedUser } from '../reducers';
 @Form.create()
 export default class Transfer extends React.Component {
   static propTypes = {
+    intl: PropTypes.shape().isRequired,
     visible: PropTypes.bool,
     to: PropTypes.string,
     user: PropTypes.shape().isRequired,
@@ -81,14 +84,14 @@ export default class Transfer extends React.Component {
   handleCancelClick = () => this.props.closeTransfer();
 
   validateBalance = (rule, value, callback) => {
-    const { user } = this.props;
+    const { intl, user } = this.props;
 
     const currentValue = parseFloat(value);
     const selectedBalance = this.state.currency === 'STEEM' ? user.balance : user.sbd_balance;
 
     if (currentValue !== 0 && currentValue > parseFloat(selectedBalance)) {
       callback([
-        new Error('Insufficient funds'),
+        new Error(intl.formatMessage({ id: 'amount_error_funds', defaultMessage: 'Insufficient funds.' })),
       ]);
     } else {
       callback();
@@ -96,7 +99,7 @@ export default class Transfer extends React.Component {
   }
 
   render() {
-    const { visible, user } = this.props;
+    const { intl, visible, user } = this.props;
     const { getFieldDecorator } = this.props.form;
 
     const balance = this.state.currency === 'STEEM' ? user.balance : user.sbd_balance;
@@ -111,31 +114,57 @@ export default class Transfer extends React.Component {
     );
 
     return (
-      <Modal visible={visible} title="Send STEEM or SBD" okText="Continue" onOk={this.handleContinueClick} onCancel={this.handleCancelClick}>
+      <Modal
+        visible={visible}
+        title={intl.formatMessage({ id: 'transfer_modal_title', defaultMessage: 'Transfer funds' })}
+        okText={intl.formatMessage({ id: 'continue', defaultMessage: 'Continue' })}
+        cancelText={intl.formatMessage({ id: 'cancel', defaultMessage: 'Cancel' })}
+        onOk={this.handleContinueClick}
+        onCancel={this.handleCancelClick}
+      >
         <Form className="Transfer container">
-          <Form.Item label={<b>To</b>}>
+          <Form.Item label={<FormattedMessage id="to" defaultMessage="To" />}>
             {getFieldDecorator('to', {
-              rules: [{ required: true, message: 'Recipient is required' }],
-            })(<Input type="text" placeholder="Payment recipient" />)}
+              rules: [{ required: true, message: intl.formatMessage({ id: 'to_error_empty', defaultMessage: 'Recipient is required.' }) }],
+            })(<Input
+              type="text"
+              placeholder={intl.formatMessage({ id: 'to_placeholder', defaultMessage: 'Payment recipient' })}
+            />)}
           </Form.Item>
-          <Form.Item label={<b>Amount</b>}>
+          <Form.Item label={<FormattedMessage id="amount" defaultMessage="Amount" />}>
             {getFieldDecorator('amount', {
               rules: [
-                { required: true, message: 'Amount is required' },
-                { pattern: /^[0-9]*[.,]{0,1}[0-9]{0,3}$/, message: 'Incorrect format. Use comma or dot as decimal separator. Use at most 3 decimal places.' },
+                { required: true, message: intl.formatMessage({ id: 'amount_error_empty', defaultMessage: 'Amount is required.' }) },
+                {
+                  pattern: /^[0-9]*[.,]{0,1}[0-9]{0,3}$/,
+                  message: intl.formatMessage({
+                    id: 'amount_error_format',
+                    defaultMessage: 'Incorrect format. Use comma or dot as decimal separator. Use at most 3 decimal places.',
+                  }),
+                },
                 { validator: this.validateBalance },
               ],
-            })(<Input addonAfter={currencyPrefix} placeholder="How much do you want to send" style={{ width: '100%' }} />)}
-            Your balance: <span role="presentation" onClick={this.handleBalanceClick} className="balance">{balance}</span>
+            })(<Input
+              addonAfter={currencyPrefix}
+              placeholder={intl.formatMessage({ id: 'amount_placeholder', defaultMessage: 'How much do you want to send' })}
+              style={{ width: '100%' }}
+            />)}
+            <FormattedMessage
+              id="balance_amount"
+              defaultMessage="Your balance: {amount}"
+              values={{
+                amount: <span role="presentation" onClick={this.handleBalanceClick} className="balance">{balance}</span>,
+              }}
+            />
           </Form.Item>
-          <Form.Item label={<b>Memo</b>}>
-            {getFieldDecorator('memo')(<Input.TextArea rows={3} placeholder="Additional message to include in this payment (optional)" />)}
+          <Form.Item label={<FormattedMessage id="memo" defaultMessage="Memo" />}>
+            {getFieldDecorator('memo')(<Input.TextArea
+              rows={3}
+              placeholder={intl.formatMessage({ id: 'memo_placeholder', defaultMessage: 'Additional message to include in this payment (optional)' })}
+            />)}
           </Form.Item>
         </Form>
-        <p>
-          Click the button below to be redirected to
-          SteemConnect to complete your transaction.
-        </p>
+        <FormattedMessage id="transfer_modal_info" defaultMessage="Click the button below to be redirected to SteemConnect to complete your transaction." />
       </Modal>
     );
   }
