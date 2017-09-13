c60fb070f513b00ec349f75c38833f7965ef8851
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-09-13 14:53:56 +0200

    Add reloading to settings

--- src/app/AppSettings.js
+++ src/app/AppSettings.js
@@ -3,60 +3,75 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import { Select } from 'antd';
-import { getLocale, getIsLocaleLoading } from '../reducers';
+import Loading from '../components/Icon/Loading';
+import { getLocale, getIsLocaleLoading, getIsReloading } from '../reducers';
 import { setLocale } from './appActions';
+import { reload } from '../auth/authActions';
 
 // NOTE: We are using injectIntl because without it locale won't update: see https://github.com/yahoo/react-intl/issues/371
 @injectIntl
 @connect(
   state => ({
+    reloading: getIsReloading(state),
     locale: getLocale(state),
     localeLoading: getIsLocaleLoading(state),
   }),
-  { setLocale },
+  { reload, setLocale },
 )
 export default class AppSettings extends React.Component {
   static propTypes = {
+    reloading: PropTypes.bool,
     locale: PropTypes.string,
-    setLocale: PropTypes.func,
     localeLoading: PropTypes.bool,
+    setLocale: PropTypes.func,
+    reload: PropTypes.func,
   };
 
   static defaultProps = {
+    reloading: false,
     locale: 'auto',
-    setLocale: () => {},
     localeLoading: false,
+    setLocale: () => {},
+    reload: () => {},
   };
+
+  componentDidMount() {
+    this.props.reload();
+  }
 
   handleLocaleChange = locale => this.props.setLocale(locale);
 
   render() {
-    const { locale, localeLoading } = this.props;
+    const { reloading, locale, localeLoading } = this.props;
     return (
       <div className="shifted">
         <div className="container">
           <h1>
             <FormattedMessage id="settings" defaultMessage="Settings" />
           </h1>
-          <h2>
-            <FormattedMessage id="language" defaultMessage="Language" />
-          </h2>
-          <div>
-            <Select disabled={localeLoading} value={locale} style={{ width: '100%', maxWidth: 240 }} onChange={this.handleLocaleChange}>
-              <Select.Option value="auto">Auto</Select.Option>
-              <Select.Option value="en">English</Select.Option>
-              <Select.Option value="zh">简体中文</Select.Option>
-              <Select.Option value="cs">Čeština</Select.Option>
-              <Select.Option value="es">Español</Select.Option>
-              <Select.Option value="fr">Français</Select.Option>
-              <Select.Option value="pl">Polski</Select.Option>
-              <Select.Option value="de">Deutsch</Select.Option>
-              <Select.Option value="ru">Русский</Select.Option>
-              <Select.Option value="ko">한국어</Select.Option>
-              <Select.Option value="nl">Nederlands</Select.Option>
-              <Select.Option value="sv">Svenska</Select.Option>
-            </Select>
-          </div>
+          {reloading ? <Loading />
+            : <div>
+              <h2>
+                <FormattedMessage id="language" defaultMessage="Language" />
+              </h2>
+              <div>
+                <Select disabled={localeLoading} value={locale} style={{ width: '100%', maxWidth: 240 }} onChange={this.handleLocaleChange}>
+                  <Select.Option value="auto">Auto</Select.Option>
+                  <Select.Option value="en">English</Select.Option>
+                  <Select.Option value="zh">简体中文</Select.Option>
+                  <Select.Option value="cs">Čeština</Select.Option>
+                  <Select.Option value="es">Español</Select.Option>
+                  <Select.Option value="fr">Français</Select.Option>
+                  <Select.Option value="pl">Polski</Select.Option>
+                  <Select.Option value="de">Deutsch</Select.Option>
+                  <Select.Option value="ru">Русский</Select.Option>
+                  <Select.Option value="ko">한국어</Select.Option>
+                  <Select.Option value="nl">Nederlands</Select.Option>
+                  <Select.Option value="sv">Svenska</Select.Option>
+                </Select>
+              </div>
+            </div>
+          }
         </div>
       </div>
     );
