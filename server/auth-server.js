3b10c0e9ab570362afd29b09d3e8ff23687323bf
adcpm <fabien@bonustrack.co>
2017-09-15 02:08:28 +0700

    Redirect user to state url if available

--- server/auth-server.js
+++ server/auth-server.js
@@ -7,17 +7,19 @@ const express = require('express');
 const app = express();
 
 app.get('/callback', (req, res) => {
-  const access_token = req.query.access_token;
-  const expires_in = req.query.expires_in;
-  if (access_token && expires_in) {
-    res.cookie('access_token', access_token, { maxAge: expires_in * 1000 });
-    res.redirect('/');
+  const accessToken = req.query.access_token;
+  const expiresIn = req.query.expires_in;
+  const state = req.query.state;
+  const next = state && state[0] === '/' ? state : '/';
+  if (accessToken && expiresIn) {
+    res.cookie('access_token', accessToken, { maxAge: expiresIn * 1000 });
+    res.redirect(next);
   } else {
     res.status(401).send({ error: 'access_token or expires_in Missing' });
   }
 });
 
-const server = app.listen(3001, 'localhost', function() {
+const server = app.listen(3001, 'localhost', () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Auth server running at http://%s:%s/', host, port);
