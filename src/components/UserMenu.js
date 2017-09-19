2dbc7d6847404cb83f02b468fef69847b42d6b91
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-09-19 13:32:11 +0200

    Migrate from antd Menu to own logic
    Antd prevents setting inline-block on menu items so it's impossible
    to create horizontaly scrollable menu

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { FormattedMessage, FormattedNumber } from 'react-intl';
-import { Menu } from 'antd';
 import './UserMenu.less';
 
 class UserMenu extends React.Component {
@@ -26,17 +26,11 @@ class UserMenu extends React.Component {
     };
   }
 
+  getItemClasses = key => classNames('UserMenu__item', { 'UserMenu__item--active': this.state.current === key });
+
   handleClick = (e) => {
-    this.setState(
-      {
-        current: e.key,
-      },
-      () => {
-        if (this.props.onChange) {
-          this.props.onChange(e.key);
-        }
-      },
-    );
+    const key = e.currentTarget.dataset.key;
+    this.setState({ current: key }, () => this.props.onChange(key));
   };
 
   render() {
@@ -44,31 +38,26 @@ class UserMenu extends React.Component {
       <div className="UserMenu">
         <div className="container menu-layout">
           <div className="left" />
-          <Menu
-            className="UserMenu__menu center"
-            onClick={this.handleClick}
-            selectedKeys={[this.state.current]}
-            mode="horizontal"
-          >
-            <Menu.Item key="discussions" className="UserMenu__item">
+          <ul className="UserMenu__menu center">
+            <li className={this.getItemClasses('discussions')} onClick={this.handleClick} role="presentation" data-key="discussions">
               <FormattedMessage id="discussions" defaultMessage="Discussions" />
-            </Menu.Item>
-            <Menu.Item key="comments" className="UserMenu__item">
+            </li>
+            <li className={this.getItemClasses('comments')} onClick={this.handleClick} role="presentation" data-key="comments">
               <FormattedMessage id="comments" defaultMessage="Comments" />
-            </Menu.Item>
-            <Menu.Item key="followers" className="UserMenu__item">
+            </li>
+            <li className={this.getItemClasses('followers')} onClick={this.handleClick} role="presentation" data-key="followers">
               <FormattedMessage id="followers" defaultMessage="Followers" />
               <span className="UserMenu__badge">
                 <FormattedNumber value={this.props.followers} />
               </span>
-            </Menu.Item>
-            <Menu.Item key="followed" className="UserMenu__item">
+            </li>
+            <li className={this.getItemClasses('followed')} onClick={this.handleClick} role="presentation" data-key="followed">
               <FormattedMessage id="following" defaultMessage="Following" />
               <span className="UserMenu__badge">
                 <FormattedNumber value={this.props.following} />
               </span>
-            </Menu.Item>
-          </Menu>
+            </li>
+          </ul>
         </div>
       </div>
     );
