d4559f77a054f071a62945e1349270d0feff72d1
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-09-20 17:45:21 +0200

    Add sidebars to Replies

--- src/replies/Replies.js
+++ src/replies/Replies.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Affix from '../components/Utils/Affix';
+import LeftSidebar from '../app/Sidebar/LeftSidebar';
+import RightSidebar from '../app/Sidebar/RightSidebar';
 
-const UserReplies = () => <h2>User replies</h2>;
+const Replies = () => (
+  <div className="shifted">
+    <div className="feed-layout container">
+      <Affix className="leftContainer" stickPosition={72}>
+        <div className="left">
+          <LeftSidebar />
+        </div>
+      </Affix>
+      <Affix className="rightContainer" stickPosition={72}>
+        <div className="right">
+          <RightSidebar />
+        </div>
+      </Affix>
+      <div className="center">
+        <h1>User replies</h1>
+      </div>
+    </div>
+  </div>);
 
-export default UserReplies;
+export default Replies;
