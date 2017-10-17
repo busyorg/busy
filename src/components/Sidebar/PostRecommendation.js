f4b522a73199087488d4f56bbf63333743c353e2
JM Myers <jm.myers@everbridge.com>
2017-10-17 15:00:08 -0400

    Use similar style for sidebar block content title ion Interesting People and Post Recommendation
    * update snapshot test

--- src/components/Sidebar/PostRecommendation.js
+++ src/components/Sidebar/PostRecommendation.js
@@ -103,7 +103,7 @@ class PostRecommendation extends Component {
     const username = location.pathname.split('/')[2].replace('@', '');
     return (
       <div className="PostRecommendation">
-        <h4 className="PostRecommendation__title">
+        <h4 className="PostRecommendation__title SidebarBlock__content-title">
           <i className="iconfont icon-headlines PostRecommendation__icon" />
           {' '}
           {intl.formatMessage({ id: 'more_from', defaultMessage: 'More from' })}
