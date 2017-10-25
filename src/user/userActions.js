ed39280f9940a97a93e70a646e678d5bf46b5438
ryanbaer <rrb5068@gmail.com>
2017-10-25 15:41:33 -0700

    PR Review: update action creator

--- src/user/userActions.js
+++ src/user/userActions.js
@@ -136,5 +136,4 @@ export const getFollowing = (userName = '') => (dispatch, getState) => {
 };
 
 export const UPDATE_RECOMMENDATIONS = '@user/UPDATE_RECOMMENDATIONS';
-const updateRecommendationsAction = createAction(UPDATE_RECOMMENDATIONS);
-export const updateRecommendations = () => dispatch => dispatch(updateRecommendationsAction());
+export const updateRecommendations = createAction(UPDATE_RECOMMENDATIONS, () => {});
