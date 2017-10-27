d9e3b5064d34af14ee05006247499305cd5de8b4
JM Myers <jm.myers@everbridge.com>
2017-10-26 20:04:28 -0400

    Force update - Merge branch with latest changes from new-design

--- src/app/Sidebar/RightSidebar.js
+++ src/app/Sidebar/RightSidebar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
-import people from '../../helpers/people';
 import {
   getIsAuthenticated,
   getAuthenticatedUser,
@@ -10,15 +9,13 @@ import {
   getRecommendations,
 } from '../../reducers';
 import { updateRecommendations } from '../../user/userActions';
-import {
-  getFollowingList,
-} from '../../reducers';
 import InterestingPeople from '../../components/Sidebar/InterestingPeople';
 import InterestingPeopleWithAPI from '../../components/Sidebar/InterestingPeopleWithAPI';
-import PostRecommendation from '../../components/Sidebar/PostRecommendation';
 import StartNow from '../../components/Sidebar/StartNow';
 import SignUp from '../../components/Sidebar/SignUp';
+import PostRecommendation from '../../components/Sidebar/PostRecommendation';
 
+@connect(
   state => ({
     authenticated: getIsAuthenticated(state),
     authenticatedUser: getAuthenticatedUser(state),
@@ -29,18 +26,11 @@ import SignUp from '../../components/Sidebar/SignUp';
     updateRecommendations,
   },
 )
-@connect(state => ({
-  authenticated: getIsAuthenticated(state),
-  authenticatedUser: getAuthenticatedUser(state),
-  authFetching: getIsAuthFetching(state),
-  followingList: getFollowingList(state),
 export default class RightSidebar extends React.Component {
   static propTypes = {
     authenticated: PropTypes.bool.isRequired,
     authenticatedUser: PropTypes.shape().isRequired,
     isAuthFetching: PropTypes.bool.isRequired,
-    authFetching: PropTypes.bool.isRequired,
-    followingList: PropTypes.arrayOf(PropTypes.string).isRequired,
     showPostRecommendation: PropTypes.bool,
     recommendations: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string })).isRequired,
     updateRecommendations: PropTypes.func,
@@ -49,70 +39,41 @@ export default class RightSidebar extends React.Component {
   static defaultProps = {
     showPostRecommendation: false,
     updateRecommendations: () => {},
+  };
 
   handleInterestingPeopleRefresh = () => this.props.updateRecommendations();
-=======
-  constructor(props) {
-    super(props);
-    this.state = {
-      randomPeople: this.getRandomPeople(),
-    };
-  }
-
-  getRandomPeople = () =>
-    people
-      .reduce((res, item) => {
-        if (!this.props.followingList.includes(item)) {
-          res.push({ name: item });
-        }
-        return res;
-      }, [])
-      .sort(() => 0.5 - Math.random())
-      .slice(0, 5);
-
-  handleRefreshInterestingPeople = () =>
-    this.setState({
-      randomPeople: this.getRandomPeople(),
-    });
 
   render() {
-    const { authenticated, authenticatedUser, authFetching, showPostRecommendation } = this.props;
+    const { authenticated, authenticatedUser, showPostRecommendation, isAuthFetching } = this.props;
+
     return (
       <div>
         {!authenticated && <SignUp />}
         <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <div>
-                {authenticatedUser.last_root_post === '1970-01-01T00:00:00' && <StartNow />}
-<<<<<<< 3aa813af336a1228402490e4ca53a76fb88b6ca9
-                <InterestingPeople
-                  users={this.props.recommendations}
-                  onRefresh={this.handleInterestingPeopleRefresh}
-                />
-=======
-                {authenticated &&
-                  <InterestingPeople
-                    users={this.state.randomPeople}
-                    onRefresh={this.handleRefreshInterestingPeople}
-                  />}
->>>>>>> Hide Interesting People for unauthenticated users
-              </div>
-            )}
-          />
           <Route
             path="/@:name"
             component={() =>
               authenticated &&
               <InterestingPeopleWithAPI
                 authenticatedUser={authenticatedUser}
-                authFetching={authFetching}
+                authFetching={isAuthFetching}
               />}
           />
+          <Route
+            path="/"
+            render={() => (
+              <div>
+                {authenticatedUser.last_root_post === '1970-01-01T00:00:00' && <StartNow />}
+                {authenticated &&
+                  <InterestingPeople
+                    users={this.props.recommendations}
+                    onRefresh={this.handleInterestingPeopleRefresh}
+                  />}
+              </div>
+            )}
+          />
         </Switch>
-        {showPostRecommendation && <PostRecommendation isAuthFetching={authFetching} />}
+        {showPostRecommendation && <PostRecommendation isAuthFetching={isAuthFetching} />}
       </div>
     );
   }
