3e4e0d59059b61d352d142cf21ddeacadd1773cd
JM Myers <jm.myers@everbridge.com>
2017-10-30 13:13:32 -0400

    Add SteemTrendingCharts to User Wallet Pages & display delegated SP (#949)
    * Add SteemTrendingCharts to User Wallet Pages & display delegated SP
    * Update Text format in UserWalletSummary
    
    * Update User Wallet
    
    * Add Transfer Button to own wallet page: https://github.com/busyorg/busy/projects/6#card-5364055
    * Add tooltip to time ago for wallet transactions: https://github.com/busyorg/busy/projects/6#card-5382480
    * display delegated SP

--- src/wallet/Wallet.js
+++ src/wallet/Wallet.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { injectIntl } from 'react-intl';
+import { openTransfer } from '../wallet/walletActions';
 import Affix from '../components/Utils/Affix';
 import LeftSidebar from '../app/Sidebar/LeftSidebar';
+import Action from '../components/Button/Action';
 import SteemTrendingCharts from '../components/Sidebar/SteemTrendingCharts';
 import UserWallet from '../user/UserWallet';
 
-const Wallet = () => (
+const Wallet = props => (
   <div className="shifted">
     <div className="feed-layout container">
       <Affix className="leftContainer" stickPosition={77}>
@@ -15,6 +20,14 @@ const Wallet = () => (
       <Affix className="rightContainer" stickPosition={77}>
         <div className="right">
           <SteemTrendingCharts />
+          <Action
+            style={{ margin: '10px 0' }}
+            text={props.intl.formatMessage({
+              id: 'transfer',
+              defaultMessage: 'Transfer',
+            })}
+            onClick={() => props.openTransfer('')}
+          />
         </div>
       </Affix>
       <div className="center">
@@ -23,4 +36,12 @@ const Wallet = () => (
     </div>
   </div>
 );
-export default Wallet;
+
+Wallet.propTypes = {
+  openTransfer: PropTypes.func.isRequired,
+  intl: PropTypes.shape().isRequired,
+};
+
+export default connect(null, {
+  openTransfer,
+})(injectIntl(Wallet));
