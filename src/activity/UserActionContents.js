bd06129b95bd8d560e673d63f1b5ec6e13bb7b67
JM Myers <jm.myers@everbridge.com>
2017-11-06 18:27:43 -0500

    Refactor UserActionContents to display all unparsed action details as table

--- src/activity/UserActionContents.js
+++ src/activity/UserActionContents.js
@@ -3,58 +3,36 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import * as accountHistory from '../constants/accountHistory';
 
-class UserActionContents extends React.Component {
-  static propTypes = {
-    actionType: PropTypes.string.isRequired,
-    actionDetails: PropTypes.shape().isRequired,
-  };
-
-  renderContents() {
-    const { actionType, actionDetails } = this.props;
-    switch (actionType) {
-      case accountHistory.ACCOUNT_UPDATE: {
-        const accountUpdateProperties = ['account', 'posting', 'memo_key', 'json_metadata'];
-        const accountUpdateContents = [];
-        _.each(accountUpdateProperties, (accountProperty) => {
-          if (_.has(actionDetails, accountProperty)) {
-            accountUpdateContents.push(
-              <tr className="UserActivityActions__contents__item" key={accountProperty}>
-                <td className="UserActivityActions__contents__label">
-                  {accountProperty}
-                </td>
-                <td className="UserActivityActions__contents__value">
-                  {_.isObject(actionDetails[accountProperty])
-                    ? <pre>
-                      <code>{JSON.stringify(actionDetails[accountProperty], null, 2)}</code>
-                    </pre>
-                    : actionDetails[accountProperty]}
-                </td>
-              </tr>,
-            );
-          }
-        });
-        return accountUpdateContents;
-      }
-      default:
-        return null;
-    }
-  }
-
-  render() {
-    const contents = this.renderContents();
-
-    if (contents) {
-      return (
-        <table className="UserActivityActions__contents">
-          <tbody>
-            {this.renderContents()}
-          </tbody>
-        </table>
-      );
-    }
-
+const UserActionContents = ({ actionType, actionDetails }) => {
+  if (_.includes(accountHistory.PARSED_PROPERTIES, actionType)) {
     return null;
   }
-}
+
+  return (
+    <table className="UserActivityActions__contents">
+      <tbody>
+        {_.map(actionDetails, (details, property) => (
+          <tr className="UserActivityActions__contents__item" key={property}>
+            <td className="UserActivityActions__contents__label">
+              {property}
+            </td>
+            <td className="UserActivityActions__contents__value">
+              {_.isObject(details)
+                ? <pre>
+                  <code>{JSON.stringify(details, null, 2)}</code>
+                </pre>
+                : details}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+UserActionContents.propTypes = {
+  actionType: PropTypes.string.isRequired,
+  actionDetails: PropTypes.shape().isRequired,
+};
 
 export default UserActionContents;
