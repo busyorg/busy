13dc3d8e2c80b680b691708629181fe38930d7a5
JM Myers <jm.myers@everbridge.com>
2017-11-19 15:28:36 -0500

    discover more people page style updates

--- src/discover/Discover.js
+++ src/discover/Discover.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
-import { getIsAuthenticated } from '../reducers';
-import SignUp from '../components/Sidebar/SignUp';
 import LeftSidebar from '../app/Sidebar/LeftSidebar';
 import DiscoverContent from './DiscoverContent';
 import Affix from '../components/Utils/Affix';
 import './Discover.less';
 
-const Discover = ({ authenticated }) => (
+const Discover = () => (
   <div>
     <div className="shifted">
       <div className="feed-layout container">
@@ -18,22 +14,17 @@ const Discover = ({ authenticated }) => (
             <LeftSidebar />
           </div>
         </Affix>
-        <Affix className="rightContainer" stickPosition={77}>
-          <div className="right">
-            {!authenticated && <SignUp />}
+        <div className="Discover">
+          <div className="Discover__title">
+            <h1>
+              <FormattedMessage id="discover_more_people" defaultMessage="Discover more people" />
+            </h1>
+            <FormattedMessage
+              id="discover_more_people_info"
+              defaultMessage="Discover the most reputable users of this platform"
+            />
           </div>
-        </Affix>
-        <div className="center">
           <div className="Discover__content">
-            <div className="Discover__title">
-              <h1>
-                <FormattedMessage id="discover_more_people" defaultMessage="Discover more people" />
-              </h1>
-              <FormattedMessage
-                id="discover_more_people_info"
-                defaultMessage="These people are the top contributors on Steem"
-              />
-            </div>
             <DiscoverContent />
           </div>
         </div>
@@ -42,10 +33,4 @@ const Discover = ({ authenticated }) => (
   </div>
 );
 
-Discover.propTypes = {
-  authenticated: PropTypes.bool.isRequired,
-};
-
-export default connect(state => ({
-  authenticated: getIsAuthenticated(state),
-}))(Discover);
+export default Discover;
