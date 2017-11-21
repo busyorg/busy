f8ab141a36de75835a6ad222dd6a680b731070c9
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-11-22 00:50:26 +0100

    Make page logging isomorphic

--- src/client/routes.js
+++ src/client/routes.js
@@ -4,18 +4,15 @@ import createBrowserHistory from 'history/createBrowserHistory';
 import createMemoryHistory from 'history/createMemoryHistory';
 import { Switch } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
-
 import routes from '../common/routes';
 
-// TODO: Handle logPageView
-// const logPageView = () => {
-//   if (window.analytics) {
-//     window.analytics.page({ url: window.location.pathname });
-//   }
-//   return null;
-// };
+const history = typeof window === 'undefined' ? createMemoryHistory() : createBrowserHistory();
 
+if (typeof window !== 'undefined' && window.analytics) {
+  history.listen((location) => {
+    window.analytics.page({ url: location.pathname });
+  });
+}
+
+export { history };
 export default <Switch>{renderRoutes(routes)}</Switch>;
-
-export const history =
-  typeof window === 'undefined' ? createMemoryHistory() : createBrowserHistory();
