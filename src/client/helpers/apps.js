9e61ac5e59034af6c91ca9eed538ad095066fcc7
Kenneth Bennett <Kennybll@outlook.com>
2017-11-28 15:56:24 -0500

    Adds More Steem Apps To Show
    Adds dtube, dmania, dsound, steepshot, zappl, and steemkr

--- src/client/helpers/apps.js
+++ src/client/helpers/apps.js
@@ -4,4 +4,10 @@ export default {
   esteem: 'eSteem',
   chainbb: 'chainBB',
   utopian: 'Utopian',
+  dtube: 'dTube',
+  dmania: 'dMania',
+  dsound: 'dSound',
+  steepshot: 'Steepshot',
+  zappl: 'Zappl',
+  steemkr: 'Steemkr',
 };
