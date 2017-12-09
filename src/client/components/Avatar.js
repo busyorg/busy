559390ec4b14ab05d09d3259559db8a465006bba
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-12-09 11:12:52 +0100

    Use div instead of image for Avatar

--- src/client/components/Avatar.js
+++ src/client/components/Avatar.js
@@ -1,60 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import getImage from '../helpers/getImage';
 import './Avatar.less';
 
-const defaultImage =
-  'https://res.cloudinary.com/hpiynhbhq/image/upload/v1506948447/p72avlprkfariyti7q2l.png';
+const Avatar = ({ username, size }) => (
+  <div
+    className="Avatar"
+    style={{
+      minWidth: `${size}px`,
+      width: `${size}px`,
+      height: `${size}px`,
+      backgroundImage: `url(${getImage(`@${username}?width=${size}&height=${size}`)})`,
+    }}
+  />
+);
 
-class Avatar extends Component {
-  static propTypes = {
-    username: PropTypes.string,
-    size: PropTypes.number,
-  };
+Avatar.propTypes = {
+  username: PropTypes.string.isRequired,
+  size: PropTypes.number,
+};
 
-  static defaultProps = {
-    username: undefined,
-    size: 34,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loadFailed: false,
-    };
-
-    this.onError = this.onError.bind(this);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.username !== nextProps.username) {
-      this.setState({
-        loadFailed: false,
-      });
-    }
-  }
-
-  onError() {
-    this.setState({
-      loadFailed: true,
-    });
-  }
-
-  render() {
-    const { username, size } = this.props;
-    const { loadFailed } = this.state;
-
-    return (
-      <img
-        className="Avatar"
-        style={{ minWidth: `${size}px`, width: `${size}px`, height: `${size}px` }}
-        onError={this.onError}
-        alt={username}
-        src={loadFailed ? defaultImage : getImage(`@${username}?s=${size}`)}
-      />
-    );
-  }
-}
+Avatar.defaultProps = {
+  size: 100,
+};
 
 export default Avatar;
