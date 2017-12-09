5900e4fcac85c930056524cca25e88f8f1484078
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-12-09 20:31:42 +0100

    Log initial page view (#1162) (#1176)
    Send pageview on initial load

--- src/client/routes.js
+++ src/client/routes.js
@@ -9,6 +9,7 @@ import routes from '../common/routes';
 const history = typeof window === 'undefined' ? createMemoryHistory() : createBrowserHistory();
 
 if (typeof window !== 'undefined' && window.analytics) {
+  window.analytics.page({ url: history.location.pathname });
   history.listen((location) => {
     window.analytics.page({ url: location.pathname });
   });
