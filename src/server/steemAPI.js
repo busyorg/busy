bba7afae2d1dce0f7487a2622796385188604d83
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-12-10 16:46:49 +0100

    Use lightrpc on server

---
+++ src/server/steemAPI.js
@@ -0,0 +1,12 @@
+import { createClient } from 'lightrpc';
+
+const client = createClient(process.env.STEEMJS_URL);
+client.sendAsync = (message, params) =>
+  new Promise((resolve, reject) => {
+    client.send(message, params, (err, result) => {
+      if (err !== null) return reject(err);
+      return resolve(result);
+    });
+  });
+
+module.exports = client;
