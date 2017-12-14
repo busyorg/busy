f9fbb2c4e8c4383c75ed5886c744558b5746089d
Kenneth Bennett <Kennybll@outlook.com>
2017-12-14 11:53:17 -0500

    Add Code Splitting for locales

--- src/client/Wrapper.js
+++ src/client/Wrapper.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { IntlProvider } from 'react-intl';
+import { IntlProvider, addLocaleData } from 'react-intl';
 import { withRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
 import { LocaleProvider, Layout } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
 
-import { getAuthenticatedUser, getLocale } from './reducers';
+import { getAuthenticatedUser, getAuthenticatedUserName, getLocale } from './reducers';
 
-import { login, logout, getCurrentUserFollowing } from './auth/authActions';
+import { login, logout } from './auth/authActions';
+import { getFollowing } from './user/userActions';
 import { getRate, getRewardFund, getTrendingTopics } from './app/appActions';
 import Topnav from './components/Navigation/Topnav';
 import Transfer from './wallet/Transfer';
 import * as reblogActions from './app/Reblog/reblogActions';
-import getTranslations, { getAvailableLocale } from './translations';
+import Loading from './components/Icon/Loading';
 
 @withRouter
 @connect(
   state => ({
     user: getAuthenticatedUser(state),
+    username: getAuthenticatedUserName(state),
     locale: getLocale(state),
   }),
   {
     login,
     logout,
-    getCurrentUserFollowing,
+    getFollowing,
     getRate,
     getRewardFund,
     getTrendingTopics,
@@ -38,9 +40,10 @@ export default class Wrapper extends React.PureComponent {
     user: PropTypes.shape().isRequired,
     locale: PropTypes.string.isRequired,
     history: PropTypes.shape().isRequired,
+    username: PropTypes.string,
     login: PropTypes.func,
     logout: PropTypes.func,
-    getCurrentUserFollowing: PropTypes.func,
+    getFollowing: PropTypes.func,
     getRewardFund: PropTypes.func,
     getRebloggedList: PropTypes.func,
     getRate: PropTypes.func,
@@ -48,21 +51,40 @@ export default class Wrapper extends React.PureComponent {
   };
 
   static defaultProps = {
+    username: '',
     login: () => {},
     logout: () => {},
-    getCurrentUserFollowing: () => {},
+    getFollowing: () => {},
     getRewardFund: () => {},
     getRebloggedList: () => {},
     getRate: () => {},
     getTrendingTopics: () => {},
   };
 
+  state = {
+    locale: '',
+    loading: true,
+    translations: {},
+  };
+
   static fetchData(store) {
     return store.dispatch(login());
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.locale !== this.props.locale) {
+      this.setState({ locale: this.getAvailableLocale(nextProps.locale), });
+      import('react-intl/locale-data/' + this.getAvailableLocale(nextProps.locale)).then((localeData) => {
+        addLocaleData(localeData);
+        import('./locales/'+this.getAvailableLocale(nextProps.locale)+'.json').then((data) => {
+          this.setState({ translations: data, loading: false, });
+        });
+      });
+    }
+  }
+
   componentDidMount() {
-    this.props.login().then(() => this.props.getCurrentUserFollowing());
+    this.props.login().then(() => this.props.getFollowing());
     this.props.getRewardFund();
     this.props.getRebloggedList();
     this.props.getRate();
@@ -89,19 +111,56 @@ export default class Wrapper extends React.PureComponent {
       case 'settings':
         this.props.history.push('/settings');
         break;
+      case 'feed':
+        this.props.history.push('/');
+        break;
+      case 'news':
+        this.props.history.push('/trending');
+        break;
+      case 'wallet':
+        this.props.history.push('/wallet');
+        break;
+      case 'my-profile':
+        this.props.history.push(`/@${this.props.username}`);
+        break;
       default:
         break;
     }
   };
 
+  getAvailableLocale = (appLocale) => {
+    let locale = appLocale || 'auto';
+  
+    if (locale === 'auto') {
+      locale = this.getBrowserLocale() || 'en';
+    }
+  
+    return locale;
+  };
+
+  getBrowserLocale = () => {
+    let detectedLocale;
+    if (typeof navigator !== 'undefined') {
+      detectedLocale =
+        navigator.userLanguage ||
+        navigator.language ||
+        (navigator.languages && navigator.languages[0] ? navigator.languages[0] : undefined);
+    }
+    if (detectedLocale) {
+      return detectedLocale.slice(0, 2);
+    }
+    return undefined;
+  };
+
   render() {
     const { locale: appLocale, user } = this.props;
 
-    const locale = getAvailableLocale(appLocale);
-    const translations = getTranslations(appLocale);
+    if(this.state.loading) {
+      return null;
+    }
 
     return (
-      <IntlProvider key={locale} locale={locale} messages={translations}>
+      <IntlProvider key={this.state.locale} locale={this.state.locale} messages={this.state.translations}>
         <LocaleProvider locale={enUS}>
           <Layout>
             <Layout.Header style={{ position: 'fixed', width: '100%', zIndex: 5 }}>
