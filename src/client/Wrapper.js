a9c602b58f7a69bccfd4ca3c8c92e8423569dd86
Kenneth Bennett <Kennybll@outlook.com>
2017-12-14 11:53:17 -0500

    Add Code Splitting for locales

--- src/client/Wrapper.js
+++ src/client/Wrapper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { IntlProvider } from 'react-intl';
+import { IntlProvider, addLocaleData } from 'react-intl';
 import { withRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
 import { LocaleProvider, Layout } from 'antd';
@@ -15,7 +15,7 @@ import { getRate, getRewardFund, getTrendingTopics } from './app/appActions';
 import Topnav from './components/Navigation/Topnav';
 import Transfer from './wallet/Transfer';
 import * as reblogActions from './app/Reblog/reblogActions';
-import getTranslations, { getAvailableLocale } from './translations';
+import Loading from './components/Icon/Loading';
 
 @withRouter
 @connect(
@@ -61,8 +61,26 @@ export default class Wrapper extends React.PureComponent {
     getTrendingTopics: () => {},
   };
 
+  state = {
+    locale: '',
+    loading: true,
+    translations: {},
+  };
+
   static fetchData(store) {
     return store.dispatch(login());
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.locale !== this.props.locale) {
+      this.setState({ locale: this.getAvailableLocale(nextProps.locale), });
+      import('react-intl/locale-data/' + this.getAvailableLocale(nextProps.locale)).then((localeData) => {
+        addLocaleData(localeData);
+        import('./locales/'+this.getAvailableLocale(nextProps.locale)+'.json').then((data) => {
+          this.setState({ translations: data, loading: false, });
+        });
+      });
+    }
   }
 
   componentDidMount() {
@@ -110,14 +128,39 @@ export default class Wrapper extends React.PureComponent {
     }
   };
 
+  getAvailableLocale = (appLocale) => {
+    let locale = appLocale || 'auto';
+  
+    if (locale === 'auto') {
+      locale = this.getBrowserLocale() || 'en';
+    }
+  
+    return locale;
+  };
+
+  getBrowserLocale = () => {
+    let detectedLocale;
+    if (typeof navigator !== 'undefined') {
+      detectedLocale =
+        navigator.userLanguage ||
+        navigator.language ||
+        (navigator.languages && navigator.languages[0] ? navigator.languages[0] : undefined);
+    }
+    if (detectedLocale) {
+      return detectedLocale.slice(0, 2);
+    }
+    return undefined;
+  };
+
   render() {
     const { locale: appLocale, user } = this.props;
 
-    const locale = getAvailableLocale(appLocale);
-    const translations = getTranslations(appLocale);
+    if(this.state.loading) {
+      return null;
+    }
 
     return (
-      <IntlProvider key={locale} locale={locale} messages={translations}>
+      <IntlProvider key={this.state.locale} locale={this.state.locale} messages={this.state.translations}>
         <LocaleProvider locale={enUS}>
           <Layout>
             <Layout.Header style={{ position: 'fixed', width: '100%', zIndex: 5 }}>
