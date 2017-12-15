002f5e9b2862e8c965e49eaaf063c9f3c01d825f
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-12-15 13:43:26 +0100

    Update error pages

--- src/client/statics/Error401.js
+++ src/client/statics/Error401.js
@@ -2,37 +2,45 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import SteemConnect from '../steemConnectAPI';
+import './ErrorPage.less';
 
 const Error401 = () => (
-  <div className="main-panel">
-    <div className="container my-5">
-      <div className="ptl text-center">
-        <h1>
-          <FormattedMessage id="page_forbidden" defaultMessage="Page Requires Authentication" />
-        </h1>
-        <h2>
-          <FormattedMessage
-            id="page_forbidden_message"
-            defaultMessage="Oops! Looks like you need to login to use this page."
-          />
-        </h2>
-        <p>
-          <FormattedMessage
-            id="need_login"
-            defaultMessage="You need to login to use this feature, please"
-          />{' '}
-          <a href={SteemConnect.getLoginURL()}>
-            <FormattedMessage id="login" defaultMessage="Login" />
-          </a>
-          <br />
-          <FormattedMessage id="here_link" defaultMessage="Here's a link to the" />
-          <Link to="/">
-            <FormattedMessage id="homepage" defaultMessage="home page" />
-          </Link>
-          .
-        </p>
-      </div>
-    </div>
+  <div className="ErrorPage container">
+    <h1>
+      <FormattedMessage id="page_forbidden" defaultMessage="Page Requires Authentication" />
+    </h1>
+    <h2>
+      <FormattedMessage
+        id="page_forbidden_message"
+        defaultMessage="Oops! Looks like you need to login to use this page."
+      />
+    </h2>
+    <p>
+      <FormattedMessage
+        id="need_login_link_text"
+        defaultMessage="You need to login to use this feature, please {link}."
+        values={{
+          link: (
+            <a href={SteemConnect.getLoginURL()}>
+              <FormattedMessage id="login" defaultMessage="Login" />
+            </a>
+          ),
+        }}
+      />
+    </p>
+    <p>
+      <FormattedMessage
+        id="homepage_link_text"
+        defaultMessage="Here's a link to {link}."
+        values={{
+          link: (
+            <Link to="/">
+              <FormattedMessage id="homepage" defaultMessage="the home page" />
+            </Link>
+          ),
+        }}
+      />
+    </p>
   </div>
 );
 
--- src/client/statics/Error404.js
+++ src/client/statics/Error404.js
@@ -1,37 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
+import './ErrorPage.less';
 
-const Error404 = () =>
-  (<div className="main-panel">
-    <div className="container my-5">
-      <div className="ptl text-center">
-        <h1>
-          <FormattedMessage id="page_not_found" defaultMessage="Page Not Found" />
-        </h1>
-        <h2>
-          <FormattedMessage
-            id="page_not_found_message"
-            defaultMessage="Oops! Looks like you followed a bad link."
-          />
-        </h2>
-        <p>
-          <FormattedMessage
-            id="@statics/is_a_problem_0"
-            defaultMessage="If you think this is a problem with Busy, please"
-          />{' '}
-          <a rel="noopener noreferrer" href="https://busy.org" target="_blank">
-            <FormattedMessage id="@statics/is_a_problem_1" defaultMessage="tell us" />
-          </a>
-          .<br />
-          <FormattedMessage id="@statics/here_link" defaultMessage="Here's a link to the" />{' '}
+const Error404 = () => (
+  <div className="ErrorPage container">
+    <h1>
+      <FormattedMessage id="page_not_found" defaultMessage="Page Not Found" />
+    </h1>
+    <h2>
+      <FormattedMessage
+        id="page_not_found_message"
+        defaultMessage="Oops! Looks like you followed a bad link."
+      />
+    </h2>
+    <FormattedMessage
+      id="homepage_link_text"
+      defaultMessage="Here's a link to {link}."
+      values={{
+        link: (
           <Link to="/">
-            <FormattedMessage id="@statics/homepage" defaultMessage="home page" />
+            <FormattedMessage id="homepage" defaultMessage="the home page" />
           </Link>
-          .
-        </p>
-      </div>
-    </div>
-  </div>);
+        ),
+      }}
+    />
+  </div>
+);
 
 export default Error404;
