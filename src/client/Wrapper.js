eb18ea10b1d64992154cf402525b1d3589c4eaa7
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-12-23 13:41:47 +0100

    Load translations asynchronously

--- src/client/Wrapper.js
+++ src/client/Wrapper.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { IntlProvider } from 'react-intl';
+import { IntlProvider, addLocaleData } from 'react-intl';
 import { withRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
 import { LocaleProvider, Layout } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
 
-import { getAuthenticatedUser, getAuthenticatedUserName, getLocale } from './reducers';
+import { getIsLoaded, getAuthenticatedUser, getAuthenticatedUserName, getLocale } from './reducers';
 
+import { getAvailableLocale } from './translations';
 import { login, logout } from './auth/authActions';
 import { getFollowing } from './user/userActions';
 import { getRate, getRewardFund, getTrendingTopics } from './app/appActions';
+import * as reblogActions from './app/Reblog/reblogActions';
 import Topnav from './components/Navigation/Topnav';
 import Transfer from './wallet/Transfer';
-import * as reblogActions from './app/Reblog/reblogActions';
-import getTranslations, { getAvailableLocale } from './translations';
+import Loading from './components/Icon/Loading';
 
 @withRouter
 @connect(
   state => ({
+    loaded: getIsLoaded(state),
     user: getAuthenticatedUser(state),
     username: getAuthenticatedUserName(state),
     locale: getLocale(state),
@@ -37,6 +39,7 @@ import getTranslations, { getAvailableLocale } from './translations';
 export default class Wrapper extends React.PureComponent {
   static propTypes = {
     route: PropTypes.shape().isRequired,
+    loaded: PropTypes.bool.isRequired,
     user: PropTypes.shape().isRequired,
     locale: PropTypes.string.isRequired,
     history: PropTypes.shape().isRequired,
@@ -65,15 +68,54 @@ export default class Wrapper extends React.PureComponent {
     return store.dispatch(login());
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      i18nLoaded: false,
+      translations: {},
+    };
+
+    this.loadLocale = this.loadLocale.bind(this);
+    this.handleMenuItemClick = this.handleMenuItemClick.bind(this);
+  }
+
   componentDidMount() {
+    const { loaded, locale } = this.props;
+
     this.props.login().then(() => this.props.getFollowing());
     this.props.getRewardFund();
     this.props.getRebloggedList();
     this.props.getRate();
     this.props.getTrendingTopics();
+
+    if (loaded) {
+      this.loadLocale(getAvailableLocale(locale));
+    }
   }
 
-  handleMenuItemClick = (key) => {
+  componentWillReceiveProps(nextProps) {
+    const { loaded, locale } = this.props;
+
+    if (loaded !== nextProps.loaded || locale !== nextProps.locale) {
+      this.loadLocale(getAvailableLocale(nextProps.locale));
+    }
+  }
+
+  loadLocale(locale) {
+    const localeDataPromise = import(`react-intl/locale-data/${locale}`);
+    const translationsPromise = import(`./locales/${locale}.json`);
+
+    Promise.all([localeDataPromise, translationsPromise]).then(([localeData, translations]) => {
+      addLocaleData(localeData);
+      this.setState({
+        i18nLoaded: true,
+        translations,
+      });
+    });
+  }
+
+  handleMenuItemClick(key) {
     switch (key) {
       case 'logout':
         this.props.logout();
@@ -108,16 +150,18 @@ export default class Wrapper extends React.PureComponent {
       default:
         break;
     }
-  };
+  }
 
   render() {
-    const { locale: appLocale, user } = this.props;
+    const { locale, user } = this.props;
+    const { i18nLoaded, translations } = this.state;
 
-    const locale = getAvailableLocale(appLocale);
-    const translations = getTranslations(appLocale);
+    if (!i18nLoaded) return <Loading />;
+
+    const appLocale = getAvailableLocale(locale);
 
     return (
-      <IntlProvider key={locale} locale={locale} messages={translations}>
+      <IntlProvider key={appLocale} locale={appLocale} messages={translations}>
         <LocaleProvider locale={enUS}>
           <Layout>
             <Layout.Header style={{ position: 'fixed', width: '100%', zIndex: 5 }}>
