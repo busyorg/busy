d400c87655e023fb3877b021d4cab9515ad3dd3a
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-12-24 22:44:07 +0100

    Add intersect util

---
+++ scripts/utils/intersect.js
@@ -0,0 +1,15 @@
+const minimatch = require('minimatch');
+
+function intersect(files, patterns) {
+  let intersection = [];
+  patterns.forEach((pattern) => {
+    intersection = [
+      ...intersection,
+      ...minimatch.match(files, pattern, { matchBase: true }),
+    ];
+  });
+
+  return intersection;
+}
+
+module.exports = intersect;
