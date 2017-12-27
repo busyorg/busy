ab2eeb11805c438330c9e81d72220cb681e1e772
JM Myers <jm.myers@everbridge.com>
2017-12-27 16:11:35 +0900

    update default account limit to 20

--- src/client/helpers/apiHelpers.js
+++ src/client/helpers/apiHelpers.js
@@ -110,7 +110,7 @@ export const isWalletTransaction = actionType =>
   actionType === accountHistoryConstants.DELEGATE_VESTING_SHARES ||
   actionType === accountHistoryConstants.CLAIM_REWARD_BALANCE;
 
-export const getAccountReputation = (name, limit = 5) =>
+export const getAccountReputation = (name, limit = 20) =>
   SteemAPI.sendAsync('call', ['follow_api', 'get_account_reputations', [name, limit]]);
 
 
