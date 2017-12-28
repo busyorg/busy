451b5ee967645051cc5f71db40d3169a9bc4c7f0
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-12-29 00:10:07 +0100

    Update ProfilSettings links

--- src/client/app/Sidebar/LeftSidebar.js
+++ src/client/app/Sidebar/LeftSidebar.js
@@ -3,19 +3,19 @@ import { Route, Switch } from 'react-router-dom';
 
 import UserInfo from './UserInfo';
 import Navigation from './Navigation';
-import NavigationUser from './NavigationUser';
+import SidenavUser from '../../components/Navigation/SidenavUser';
 
 const LeftSidebar = () => (
   <Switch>
     <Route path="/@:name/wallet" component={Navigation} />
     <Route path="/@:name" component={UserInfo} />
-    <Route path="/activity" component={NavigationUser} />
+    <Route path="/activity" component={SidenavUser} />
     <Route path="/replies" component={Navigation} />
-    <Route path="/bookmarks" component={NavigationUser} />
-    <Route path="/drafts" component={NavigationUser} />
-    <Route path="/edit-profile" component={NavigationUser} />
-    <Route path="/settings" component={NavigationUser} />
-    <Route path="/invite" component={NavigationUser} />
+    <Route path="/bookmarks" component={SidenavUser} />
+    <Route path="/drafts" component={SidenavUser} />
+    <Route path="/edit-profile" component={SidenavUser} />
+    <Route path="/settings" component={SidenavUser} />
+    <Route path="/invite" component={SidenavUser} />
     <Route path="/" component={Navigation} />
   </Switch>
 );
