fbd1658efe7dc2b6e1dc68acf41d10d03fc1b3bb
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-12-31 11:32:18 +0100

    Profile settings (#1280)
     Add UI.
     Add translations.
     Use antd form.
     Generate SteemConnect link.
     Link to ProfileSettings.
     Load values into form.
     Use only updated values.
     Update UserInfo.

---
+++ src/client/helpers/socialProfiles.js
@@ -0,0 +1,19 @@
+const socialTransformers = {
+  facebook: id => `https://facebook.com/${id}`,
+  twitter: id => `https://twitter.com/${id}`,
+  instagram: id => `https://instagram.com/${id}`,
+  github: id => `https://github.com/${id}`,
+  bitcoin: id => `https://blockchain.info/address/${id}`,
+  ethereum: id => `https://etherscan.io/address/${id}`,
+};
+
+export const transform = (socialId, id) => socialTransformers[socialId](id);
+
+export default [
+  { id: 'facebook', icon: 'facebook', color: '#3b5998', name: 'Facebook' },
+  { id: 'twitter', icon: 'twitter', color: '#00aced', name: 'Twitter' },
+  { id: 'instagram', icon: 'instagram', color: '#8a3ab9', name: 'Instagram' },
+  { id: 'github', icon: 'github', color: 'black', name: 'GitHub' },
+  { id: 'bitcoin', icon: 'bitcoin', color: '#ff9900', name: 'Bitcoin' },
+  { id: 'ethereum', icon: 'ethereum', color: '#3c3c3d', name: 'Ethereum' },
+];
