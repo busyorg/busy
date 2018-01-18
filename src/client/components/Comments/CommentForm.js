0f7fd44593adcc7aaedc00953940207b0c0ae4b8
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-01-18 05:38:52 +0100

    Refactor Editor and CommentForms (#1317)
    * Add EditorInput and withEditor HOC
    
    * Use withEditor and EditorInput for CommentForm
    
    * Use withEditor and EditorInput for EmbeddedCommentForm
    
    * Add inputRef and restore focus
    
    * Add autosize prop

--- src/client/components/Comments/CommentForm.js
+++ src/client/components/Comments/CommentForm.js
@@ -1,266 +1,112 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
-import { injectIntl, FormattedMessage } from 'react-intl';
-import { Input, Icon } from 'antd';
-import Dropzone from 'react-dropzone';
+import { FormattedMessage } from 'react-intl';
+import { Icon } from 'antd';
 import Scroll from 'react-scroll';
-import { isValidImage, MAXIMUM_UPLOAD_SIZE } from '../../helpers/image';
+import withEditor from '../Editor/withEditor';
+import EditorInput from '../Editor/EditorInput';
 import Body, { remarkable } from '../Story/Body';
 import Avatar from '../Avatar';
 import './CommentForm.less';
 
 const Element = Scroll.Element;
 
-@injectIntl
+@withEditor
 class CommentForm extends React.Component {
   static propTypes = {
-    intl: PropTypes.shape().isRequired,
     parentPost: PropTypes.shape().isRequired,
     username: PropTypes.string.isRequired,
+    top: PropTypes.bool,
     isSmall: PropTypes.bool,
     isLoading: PropTypes.bool,
     submitted: PropTypes.bool,
     inputValue: PropTypes.string.isRequired,
-    onImageInserted: PropTypes.func,
+    onImageUpload: PropTypes.func,
     onImageInvalid: PropTypes.func,
     onSubmit: PropTypes.func,
   };
 
   static defaultProps = {
-    username: undefined,
+    top: false,
     isSmall: false,
     isLoading: false,
     submitted: false,
     inputValue: '',
-    onImageInserted: () => {},
+    onImageUpload: () => {},
     onImageInvalid: () => {},
     onSubmit: () => {},
   };
 
-  state = {
-    inputValue: this.props.inputValue,
-    isDisabledSubmit: false,
-    imageUploading: false,
-    dropzoneActive: false,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      body: '',
+      bodyHTML: '',
+      isDisabledSubmit: false,
+    };
+
+    this.setInput = this.setInput.bind(this);
+    this.setBodyAndRender = this.setBodyAndRender.bind(this);
+    this.handleBodyUpdate = this.handleBodyUpdate.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   componentDidMount() {
-    if (this.input) {
-      this.input.addEventListener('paste', this.handlePastedImage);
-
-      if (this.props.parentPost.depth !== 0) {
-        this.input.focus();
-      }
+    if (this.input && !this.props.top) {
+      this.input.focus();
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if ((!nextProps.isLoading && nextProps.inputValue !== '') || nextProps.submitted) {
-      this.setState({ inputValue: nextProps.inputValue });
+      this.setBodyAndRender(nextProps.inputValue);
     }
   }
 
-  setInput = input => {
-    if (input && input.refs && input.refs.input) {
-      // eslint-disable-next-line react/no-find-dom-node
-      this.input = ReactDOM.findDOMNode(input.refs.input);
-    }
-  };
+  setInput(input) {
+    this.input = input;
+  }
 
-  setInputCursorPosition = pos => {
-    if (this.input && this.input.setSelectionRange) {
-      this.input.setSelectionRange(pos, pos);
-    }
-  };
-
-  disableAndInsertImage = (image, imageName = 'image') => {
+  setBodyAndRender(body) {
     this.setState({
-      imageUploading: false,
+      body,
+      bodyHTML: remarkable.render(body),
     });
-    this.insertImage(image, imageName);
-  };
+  }
 
-  insertImage = (image, imageName = 'image') => {
-    if (!this.input) return;
+  handleBodyUpdate(body) {
+    this.setBodyAndRender(body);
+  }
 
-    const startPos = this.input.selectionStart;
-    const endPos = this.input.selectionEnd;
-    const imageText = `![${imageName}](${image})\n`;
-    const newValue = `${this.input.value.substring(
-      0,
-      startPos,
-    )}${imageText}${this.input.value.substring(endPos, this.input.value.length)}`;
-    this.setState({ inputValue: newValue });
-    this.setInputCursorPosition(startPos + imageText.length);
-  };
-
-  handleCommentTextChange = e => {
-    this.setState({ inputValue: e.target.value });
-  };
-
-  handlePastedImage = e => {
-    if (e.clipboardData && e.clipboardData.items) {
-      const items = e.clipboardData.items;
-      Array.from(items).forEach(item => {
-        if (item.kind === 'file') {
-          e.preventDefault();
-
-          const blob = item.getAsFile();
-
-          if (!isValidImage(blob)) {
-            this.props.onImageInvalid();
-            return;
-          }
-
-          this.setState({
-            imageUploading: true,
-          });
-
-          this.props.onImageInserted(blob, this.disableAndInsertImage, () =>
-            this.setState({
-              imageUploading: false,
-            }),
-          );
-        }
-      });
-    }
-  };
-
-  handleImageChange = e => {
-    if (e.target.files && e.target.files[0]) {
-      if (!isValidImage(e.target.files[0])) {
-        this.props.onImageInvalid();
-        return;
-      }
-
-      this.setState({
-        imageUploading: true,
-      });
-      this.props.onImageInserted(e.target.files[0], this.disableAndInsertImage, () =>
-        this.setState({
-          imageUploading: false,
-        }),
-      );
-      // Input reacts on value change, so if user selects the same file nothing will happen.
-      // We have to reset its value, so if same image is selected it will emit onChange event.
-      e.target.value = '';
-    }
-  };
-
-  handleDrop = files => {
-    if (files.length === 0) {
-      this.setState({
-        dropzoneActive: false,
-      });
-      return;
-    }
-
-    this.setState({
-      dropzoneActive: false,
-      imageUploading: true,
-    });
-    let callbacksCount = 0;
-    Array.from(files).forEach(item => {
-      this.props.onImageInserted(
-        item,
-        (image, imageName) => {
-          callbacksCount += 1;
-          this.insertImage(image, imageName);
-          if (callbacksCount === files.length) {
-            this.setState({
-              imageUploading: false,
-            });
-          }
-        },
-        () => {
-          this.setState({
-            imageUploading: false,
-          });
-        },
-      );
-    });
-  };
-
-  handleDragEnter = () => this.setState({ dropzoneActive: true });
-
-  handleDragLeave = () => this.setState({ dropzoneActive: false });
-
-  handleSubmit = e => {
+  handleSubmit(e) {
     e.stopPropagation();
     this.setState({ isDisabledSubmit: true });
-    if (this.state.inputValue) {
-      this.props.onSubmit(this.props.parentPost, this.state.inputValue);
+    if (this.state.body) {
+      this.props.onSubmit(this.props.parentPost, this.state.body);
+      this.setBodyAndRender('');
     }
-  };
+  }
 
   render() {
-    const { intl, username, isSmall, isLoading } = this.props;
+    const { username, isSmall, isLoading } = this.props;
+    const { bodyHTML } = this.state;
+
     const buttonClass = isLoading ? 'CommentForm__button_disabled' : 'CommentForm__button_primary';
 
     return (
       <div className="CommentForm">
         <Avatar username={username} size={!isSmall ? 40 : 32} />
         <div className="CommentForm__text">
-          <div className="CommentForm__dropzone-base">
-            <Dropzone
-              disableClick
-              style={{}}
-              accept="image/*"
-              maxSize={MAXIMUM_UPLOAD_SIZE}
-              onDropRejected={this.props.onImageInvalid}
-              onDrop={this.handleDrop}
-              onDragEnter={this.handleDragEnter}
-              onDragLeave={this.handleDragLeave}
-            >
-              {this.state.dropzoneActive && (
-                <div className="CommentForm__dropzone">
-                  <div>
-                    <i className="iconfont icon-picture" />
-                    <FormattedMessage id="drop_image" defaultMessage="Drop your images here" />
-                  </div>
-                </div>
-              )}
-              <Element name="commentFormInputScrollerElement">
-                <Input
-                  id="commentFormInput"
-                  ref={ref => this.setInput(ref)}
-                  value={this.state.inputValue}
-                  autosize={{ minRows: 2, maxRows: 6 }}
-                  onChange={this.handleCommentTextChange}
-                  placeholder={intl.formatMessage({
-                    id: 'comment_placeholder',
-                    defaultMessage: 'Write a comment',
-                  })}
-                  type="textarea"
-                  disabled={isLoading}
-                />
-              </Element>
-            </Dropzone>
-          </div>
-          <p className="CommentForm__imagebox">
-            <input
-              type="file"
-              accept="image/*"
-              id={`inputfile${this.props.parentPost.id}`}
-              onChange={this.handleImageChange}
+          <Element name="commentFormInputScrollerElement">
+            <EditorInput
+              inputRef={this.setInput}
+              autosize={{ minRows: 3, maxRows: 6 }}
+              onChange={this.handleBodyUpdate}
+              onImageUpload={this.props.onImageUpload}
+              onImageInvalid={this.props.onImageInvalid}
             />
-            <label htmlFor={`inputfile${this.props.parentPost.id}`}>
-              {this.state.imageUploading ? (
-                <Icon type="loading" />
-              ) : (
-                <i className="iconfont icon-picture" />
-              )}
-              {this.state.imageUploading ? (
-                <FormattedMessage id="image_uploading" defaultMessage="Uploading your image..." />
-              ) : (
-                <FormattedMessage
-                  id="select_or_past_image"
-                  defaultMessage="Select image or paste it from the clipboard."
-                />
-              )}
-            </label>
-          </p>
+          </Element>
           <button
             onClick={this.handleSubmit}
             disabled={isLoading}
@@ -273,12 +119,12 @@ class CommentForm extends React.Component {
               <FormattedMessage id="comment_send" defaultMessage="Comment" />
             )}
           </button>
-          {this.state.inputValue && (
+          {bodyHTML && (
             <div className="CommentForm__preview">
               <span className="Editor__label">
                 <FormattedMessage id="preview" defaultMessage="Preview" />
               </span>
-              <Body body={remarkable.render(this.state.inputValue)} />
+              <Body body={bodyHTML} />
             </div>
           )}
         </div>
