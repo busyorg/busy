0e5cc3a7de170e3f088b1ae689e12d74e0397c70
JM Myers <jm.myers@everbridge.com>
2018-01-21 17:49:25 -0500

    Update Design on Post Recommendations (#1339)
    * Update Design on Post Recommendations
    
    * Fix aspect ratio on preview images, increase font size on post title, remove link from bullet
    
    * styling updates
    
    * update post recommendation to get small image for preview

---
+++ src/client/components/Sidebar/PostRecommendationLink.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import { Link } from 'react-router-dom';
+import { FormattedMessage, FormattedNumber, FormattedRelative } from 'react-intl';
+import { getFromMetadata } from '../../helpers/parser';
+import { getProxyImageURL } from '../../helpers/image';
+import { image } from '../../vendor/steemitLinks';
+
+const PostRecommendationLink = ({ post, navigateToPost, navigateToPostComments }) => {
+  const images = getFromMetadata(post.json_metadata, 'image');
+  const firstImage = _.head(images);
+  let imagePath = '';
+
+  if (images && firstImage) {
+    imagePath = getProxyImageURL(firstImage, 'small');
+  } else {
+    const bodyImg = post.body.match(image());
+    if (bodyImg && bodyImg.length) {
+      imagePath = getProxyImageURL(bodyImg[0], 'small');
+    }
+  }
+
+  return (
+    <div className="PostRecommendation__link" key={post.id}>
+      <div>
+        <div className="PostRecommendation__link__header">
+          <Link role="presentation" to={`/@${post.author}`}>
+            <span className="PostRecommendation__link__author username">{post.author}</span>
+          </Link>
+          <span className="PostRecommendation__link__bullet" />
+          <FormattedRelative value={`${post.created}Z`} />
+        </div>
+        <Link
+          to={`/${post.category}/@${post.author}/${post.permlink}`}
+          onClick={() => navigateToPost(post.author)}
+          className="PostRecommendation__link__post-title"
+        >
+          {post.title}
+        </Link>
+        {post.children > 0 && (
+          <Link
+            to={`/${post.category}/@${post.author}/${post.permlink}#comments`}
+            onClick={navigateToPostComments}
+            className="PostRecommendation__comments"
+          >
+            <i className="PostRecommendation__comments__icon iconfont icon-message_fill" />
+            {post.children === 1 ? (
+              <FormattedMessage
+                id="comment_count"
+                values={{ count: <FormattedNumber value={post.children} /> }}
+                defaultMessage="{count} comment"
+              />
+            ) : (
+              <FormattedMessage
+                id="comments_count"
+                values={{ count: <FormattedNumber value={post.children} /> }}
+                defaultMessage="{count} comments"
+              />
+            )}
+          </Link>
+        )}
+      </div>
+      <div className="PostRecommendation__link__image-container">
+        <Link
+          to={`/${post.category}/@${post.author}/${post.permlink}`}
+          onClick={() => navigateToPost(post.author)}
+          className="PostRecommendation__link__post-title"
+        >
+          <img alt="" src={imagePath} className="PostRecommendation__link__image" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+PostRecommendationLink.propTypes = {
+  post: PropTypes.shape(),
+  navigateToPost: PropTypes.func,
+  navigateToPostComments: PropTypes.func,
+};
+
+PostRecommendationLink.defaultProps = {
+  post: {},
+  navigateToPost: () => {},
+  navigateToPostComments: () => {},
+};
+
+export default PostRecommendationLink;
