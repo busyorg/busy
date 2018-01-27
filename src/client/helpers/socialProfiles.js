9767cee4fc6621dff230c0c2e45fe4b855031d3c
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-01-27 23:24:09 +0100

    Add YouTube social profile

--- src/client/helpers/socialProfiles.js
+++ src/client/helpers/socialProfiles.js
@@ -1,6 +1,7 @@
 const socialTransformers = {
   facebook: id => `https://facebook.com/${id}`,
   twitter: id => `https://twitter.com/${id}`,
+  youtube: id => `https://www.youtube.com/user/${id}`,
   instagram: id => `https://instagram.com/${id}`,
   github: id => `https://github.com/${id}`,
   bitcoin: id => `https://blockchain.info/address/${id}`,
@@ -12,6 +13,7 @@ export const transform = (socialId, id) => socialTransformers[socialId](id);
 export default [
   { id: 'facebook', icon: 'facebook', color: '#3b5998', name: 'Facebook' },
   { id: 'twitter', icon: 'twitter', color: '#00aced', name: 'Twitter' },
+  { id: 'youtube', icon: 'youtube', color: '#ff0202', name: 'YouTube' },
   { id: 'instagram', icon: 'instagram', color: '#8a3ab9', name: 'Instagram' },
   { id: 'github', icon: 'github', color: 'black', name: 'GitHub' },
   { id: 'bitcoin', icon: 'bitcoin', color: '#ff9900', name: 'Bitcoin' },
