d858a82815443eee2e11ca44e68f416bb184c503
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-01-30 12:11:45 +0100

    Add errors object

--- src/client/errorMiddleware.js
+++ src/client/errorMiddleware.js
@@ -1,20 +1,12 @@
+import _ from 'lodash';
+import errors from '../common/constants/errors';
 import { notify } from './app/Notification/notificationActions';
 
 function parseBlockChainError(error) {
-  if (error.indexOf('You may only post once every 5 minutes') !== -1) {
-    return 'You may only post once every 5 minutes.';
-  } else if (error.indexOf('You may only comment once every 20 seconds') !== -1) {
-    return 'You may only comment once every 20 seconds.';
-  } else if (error.indexOf('You have already voted in a similar way') !== -1) {
-    return 'You have already voted in a similar way.';
-  } else if (error.indexOf('Account has already reblogged this post') !== -1) {
-    return 'You have already reblogged this post.';
-  } else if (
-    error.indexOf('<= (get_dynamic_global_properties().maximum_block_size - 256)') !== -1
-  ) {
-    return 'Your post is too big.';
-  } else if (error.indexOf('bandwidth limit exceeded') !== -1) {
-    return 'Your bandwith has been exceeded. Please wait to transact or power up STEEM.';
+  const errorType = _.find(errors, e => _.includes(error, e.fingerprint));
+
+  if (_.has(errorType, 'message')) {
+    return errorType.message;
   }
 
   // Log error to console for further investigation.
