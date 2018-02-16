7d540c6bea78deb339125a26de76c1f9481ef4e9
Philip Kirkbride <phk@2klic.com>
2018-02-16 15:07:14 -0500

    Add auto-upvote setting to settings page

--- src/client/settings/Settings.js
+++ src/client/settings/Settings.js
@@ -13,6 +13,7 @@ import {
   getShowNSFWPosts,
   getRewriteLinks,
   getUseBeta,
+  getUpvoteSetting,
 } from '../reducers';
 import { saveSettings } from './settingsActions';
 import { reload } from '../auth/authActions';
@@ -37,6 +38,7 @@ import './Settings.less';
     rewriteLinks: getRewriteLinks(state),
     useBeta: getUseBeta(state),
     loading: getIsSettingsLoading(state),
+    upvoteSetting: getUpvoteSetting(state),
   }),
   { reload, saveSettings, notify },
 )
@@ -54,6 +56,7 @@ export default class Settings extends React.Component {
     reload: PropTypes.func,
     saveSettings: PropTypes.func,
     notify: PropTypes.func,
+    upvoteSetting: PropTypes.bool,
   };
 
   static defaultProps = {
@@ -65,6 +68,7 @@ export default class Settings extends React.Component {
     showNSFWPosts: false,
     rewriteLinks: false,
     useBeta: false,
+    upvoteSetting: true,
     reload: () => {},
     saveSettings: () => {},
     notify: () => {},
@@ -86,6 +90,7 @@ export default class Settings extends React.Component {
       showNSFWPosts: this.props.showNSFWPosts,
       rewriteLinks: this.props.rewriteLinks,
       useBeta: this.props.useBeta,
+      upvoteSetting: this.props.upvoteSetting,
     });
   }
 
@@ -116,6 +121,10 @@ export default class Settings extends React.Component {
 
     if (nextProps.useBeta !== this.props.useBeta) {
       this.setState({ useBeta: nextProps.useBeta });
+    }
+
+    if (nextProps.upvoteSetting !== this.props.upvoteSetting) {
+      this.setState({ upvoteSetting: nextProps.upvoteSetting });
     }
   }
 
@@ -172,6 +181,7 @@ export default class Settings extends React.Component {
         showNSFWPosts: this.state.showNSFWPosts,
         rewriteLinks: this.state.rewriteLinks,
         useBeta: this.state.useBeta,
+        upvoteSetting: this.state.upvoteSetting,
       })
       .then(() =>
         this.props.notify(
@@ -187,6 +197,7 @@ export default class Settings extends React.Component {
   handleShowNSFWPosts = event => this.setState({ showNSFWPosts: event.target.checked });
   handleRewriteLinksChange = event => this.setState({ rewriteLinks: event.target.checked });
   handleUseBetaChange = event => this.setState({ useBeta: event.target.checked });
+  handleUpvoteSettingChange = event => this.setState({ upvoteSetting: event.target.checked });
 
   render() {
     const {
@@ -197,7 +208,7 @@ export default class Settings extends React.Component {
       showNSFWPosts: initialShowNSFWPosts,
       loading,
     } = this.props;
-    const { votingPower, locale, showNSFWPosts, rewriteLinks, useBeta } = this.state;
+    const { votingPower, locale, showNSFWPosts, rewriteLinks, useBeta, upvoteSetting } = this.state;
 
     const languageOptions = [];
 
@@ -352,6 +363,26 @@ export default class Settings extends React.Component {
                   <div className="Settings__section__checkbox">
                     <Checkbox name="use_beta" checked={useBeta} onChange={this.handleUseBetaChange}>
                       <FormattedMessage id="use_beta" defaultMessage="Use Busy beta" />
+                    </Checkbox>
+                  </div>
+                </div>
+                <div className="Settings__section">
+                  <h3>
+                    <FormattedMessage id="upvote_setting" defaultMessage="Automatically upvote" />
+                  </h3>
+                  <p>
+                    <FormattedMessage
+                      id="upvote_setting_details"
+                      defaultMessage="Enable this option to automatically upvote your own posts."
+                    />
+                  </p>
+                  <div className="Settings__section__checkbox">
+                    <Checkbox
+                      name="upvote_setting"
+                      checked={upvoteSetting}
+                      onChange={this.handleUpvoteSettingChange}
+                    >
+                      <FormattedMessage id="upvote_setting" defaultMessage="Automatically upvote" />
                     </Checkbox>
                   </div>
                 </div>
