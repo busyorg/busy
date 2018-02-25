7d95a6e2977945f9ad5ae66554de2488756e8501
JM Myers <jm.myers@everbridge.com>
2018-02-25 15:40:35 -0500

    add check for parser notifications

--- src/common/constants/notifications.js
+++ src/common/constants/notifications.js
@@ -5,3 +5,5 @@ export const VOTE = 'vote';
 export const REBLOG = 'reblog';
 export const WITNESS_VOTE = 'witness_vote';
 export const TRANSFER = 'transfer';
+
+export const PARSED_NOTIFICATIONS = [REPLY, MENTION, FOLLOW, VOTE, REBLOG, WITNESS_VOTE, TRANSFER];
