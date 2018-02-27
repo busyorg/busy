9fd1583cf331f886f7e1e6ef7e73f574e5d4d3d4
JM Myers <jm.myers@everbridge.com>
2018-02-27 10:08:15 -0500

    update hero banner and add busy bolt

--- src/client/components/Navigation/Topnav.js
+++ src/client/components/Navigation/Topnav.js
@@ -338,6 +338,7 @@ class Topnav extends React.Component {
         <div className="topnav-layout">
           <div className={classNames('left', { 'Topnav__mobile-hidden': searchBarActive })}>
             <Link className="Topnav__brand" to="/">
+              <i className="iconfont icon-busy Topnav__brand-icon" />
               busy
             </Link>
             <span className="Topnav__version">beta</span>
