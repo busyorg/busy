8cbeca666eba21939780233fc41e05bd843d9e64
Philip Kirkbride <kirkins@gmail.com>
2018-03-08 15:02:59 -0500

    Add auto-upvote setting to settings page (#1541)
    * Add auto-upvote setting to settings page
    
    * add text to default.json, update text to requested
    
    * bind function using constructor
    
    * update default message to match default.json

--- src/client/settings/Settings.js
+++ src/client/settings/Settings.js
@@ -13,6 +13,7 @@ import {
   getShowNSFWPosts,
   getRewriteLinks,
   getUseBeta,
+  getUpvoteSetting,
 } from '../reducers';
 import { saveSettings } from './settingsActions';
 import { reload } from '../auth/authActions';
@@ -37,6 +38,7 @@ import './Settings.less';
     rewriteLinks: getRewriteLinks(state),
     useBeta: getUseBeta(state),
     loading: getIsSettingsLoading(state),
+    upvoteSetting: getUpvoteSetting(state),
   }),
   { reload, saveSettings, notify },
 )
@@ -54,6 +56,7 @@ export default class Settings extends React.Component {
     reload: PropTypes.func,
     saveSettings: PropTypes.func,
     notify: PropTypes.func,
+    upvoteSetting: PropTypes.bool,
   };
 
   static defaultProps = {
@@ -65,10 +68,16 @@ export default class Settings extends React.Component {
     showNSFWPosts: false,
     rewriteLinks: false,
     useBeta: false,
+    upvoteSetting: true,
     reload: () => {},
     saveSettings: () => {},
     notify: () => {},
   };
+
+  constructor(props) {
+    super(props);
+    this.handleUpvoteSettingChange = this.handleUpvoteSettingChange.bind(this);
+  }
 
   state = {
     locale: 'auto',
@@ -86,6 +95,7 @@ export default class Settings extends React.Component {
       showNSFWPosts: this.props.showNSFWPosts,
       rewriteLinks: this.props.rewriteLinks,
       useBeta: this.props.useBeta,
+      upvoteSetting: this.props.upvoteSetting,
     });
   }
 
@@ -116,6 +126,10 @@ export default class Settings extends React.Component {
 
     if (nextProps.useBeta !== this.props.useBeta) {
       this.setState({ useBeta: nextProps.useBeta });
+    }
+
+    if (nextProps.upvoteSetting !== this.props.upvoteSetting) {
+      this.setState({ upvoteSetting: nextProps.upvoteSetting });
     }
   }
 
@@ -172,6 +186,7 @@ export default class Settings extends React.Component {
         showNSFWPosts: this.state.showNSFWPosts,
         rewriteLinks: this.state.rewriteLinks,
         useBeta: this.state.useBeta,
+        upvoteSetting: this.state.upvoteSetting,
       })
       .then(() =>
         this.props.notify(
@@ -188,6 +203,10 @@ export default class Settings extends React.Component {
   handleRewriteLinksChange = event => this.setState({ rewriteLinks: event.target.checked });
   handleUseBetaChange = event => this.setState({ useBeta: event.target.checked });
 
+  handleUpvoteSettingChange(event) {
+    this.setState({ upvoteSetting: event.target.checked });
+  }
+
   render() {
     const {
       intl,
@@ -197,7 +216,7 @@ export default class Settings extends React.Component {
       showNSFWPosts: initialShowNSFWPosts,
       loading,
     } = this.props;
-    const { votingPower, locale, showNSFWPosts, rewriteLinks, useBeta } = this.state;
+    const { votingPower, locale, showNSFWPosts, rewriteLinks, useBeta, upvoteSetting } = this.state;
 
     const languageOptions = [];
 
@@ -352,6 +371,26 @@ export default class Settings extends React.Component {
                   <div className="Settings__section__checkbox">
                     <Checkbox name="use_beta" checked={useBeta} onChange={this.handleUseBetaChange}>
                       <FormattedMessage id="use_beta" defaultMessage="Use Busy beta" />
+                    </Checkbox>
+                  </div>
+                </div>
+                <div className="Settings__section">
+                  <h3>
+                    <FormattedMessage id="upvote_setting" defaultMessage="Like my posts" />
+                  </h3>
+                  <p>
+                    <FormattedMessage
+                      id="upvote_setting_details"
+                      defaultMessage="Enable this option to automatically like your own posts."
+                    />
+                  </p>
+                  <div className="Settings__section__checkbox">
+                    <Checkbox
+                      name="upvote_setting"
+                      checked={upvoteSetting}
+                      onChange={this.handleUpvoteSettingChange}
+                    >
+                      <FormattedMessage id="upvote_setting" defaultMessage="Like my posts" />
                     </Checkbox>
                   </div>
                 </div>
