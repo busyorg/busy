4e2053ef0b0ed6046c328ae554a804cf8f75c6b0
Philip Kirkbride <kirkins@gmail.com>
2018-03-08 15:09:57 -0500

    Scroll back to top when user clicks profile link (#1538)
    * Scroll back to top when user clicks profile link
    
    * scroll to top when Router URL changes

--- src/client/components/Navigation/Topnav.js
+++ src/client/components/Navigation/Topnav.js
@@ -205,7 +205,7 @@ class Topnav extends React.Component {
             </Tooltip>
           </Menu.Item>
           <Menu.Item key="user" className="Topnav__item-user">
-            <Link className="Topnav__user" to={`/@${username}`}>
+            <Link className="Topnav__user" to={`/@${username}`} onClick={window.scrollTo(0, 0)}>
               <Avatar username={username} size={36} />
             </Link>
           </Menu.Item>
