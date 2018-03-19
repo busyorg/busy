86ee0283643cb50c3a878ed6a68a1889fea5e67c
JM Myers <jm.myers@everbridge.com>
2018-03-19 16:23:47 -0400

    initial commit for editor 2 column

---
+++ src/client/components/Editor/EditorFullscreen.js
@@ -0,0 +1,268 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { injectIntl, FormattedMessage } from 'react-intl';
+import readingTime from 'reading-time';
+import { Checkbox, Form, Input, Select, Button, Modal } from 'antd';
+import { rewardsValues } from '../../../common/constants/rewards';
+import Action from '../Button/Action';
+import EditorInput from './EditorInput';
+import Body from '../Story/Body';
+import { validateTopics } from '../../helpers/postHelpers';
+import withEditor from './withEditor';
+import './EditorFullscreen.less';
+
+@injectIntl
+@withEditor
+class EditorFullScreen extends React.Component {
+  static propTypes = {
+    displayFullscreenEditor: PropTypes.bool.isRequired,
+    handleHideFullscreenEditor: PropTypes.func.isRequired,
+    setBodyAndRender: PropTypes.func.isRequired,
+    bodyHTML: PropTypes.string,
+  };
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  handleValidateTopics = intl => (rule, value, callback) =>
+    validateTopics(rule, value, callback, intl);
+
+  render() {
+    const {
+      displayFullscreenEditor,
+      handleHideFullscreenEditor,
+      bodyHTML,
+      intl,
+      form,
+      loading,
+      isUpdating,
+      saving,
+      draftId,
+    } = this.props;
+    const { getFieldDecorator } = form;
+    const { words, minutes } = readingTime(bodyHTML);
+
+    return (
+      <Modal
+        title={null}
+        footer={null}
+        visible={displayFullscreenEditor}
+        onCancel={handleHideFullscreenEditor}
+        wrapClassName="EditorFullscreen"
+        destroyOnClose
+        width="100%"
+        style={{ top: 0 }}
+        bodyStyle={{ height: '100vh', padding: 0 }}
+      >
+        <div className="EditorFullscreen__container">
+          <div className="EditorFullscreen__column EditorFullscreen__form">
+            <Form className="Editor" layout="vertical" onSubmit={this.handleSubmit}>
+              <Form.Item
+                label={
+                  <span className="Editor__label">
+                    <FormattedMessage id="title" defaultMessage="Title" />
+                  </span>
+                }
+              >
+                {getFieldDecorator('title', {
+                  initialValue: '',
+                  rules: [
+                    {
+                      required: true,
+                      message: intl.formatMessage({
+                        id: 'title_error_empty',
+                        defaultMessage: 'title_error_empty',
+                      }),
+                    },
+                    {
+                      max: 255,
+                      message: intl.formatMessage({
+                        id: 'title_error_too_long',
+                        defaultMessage: "Title can't be longer than 255 characters.",
+                      }),
+                    },
+                  ],
+                })(
+                  <Input
+                    onChange={this.props.onUpdate}
+                    className="Editor__title"
+                    placeholder={intl.formatMessage({
+                      id: 'title_placeholder',
+                      defaultMessage: 'Add title',
+                    })}
+                  />,
+                )}
+              </Form.Item>
+              <Form.Item
+                label={
+                  <span className="Editor__label">
+                    <FormattedMessage id="topics" defaultMessage="Topics" />
+                  </span>
+                }
+                extra={intl.formatMessage({
+                  id: 'topics_extra',
+                  defaultMessage:
+                    'Separate topics with commas. Only lowercase letters, numbers and hyphen character is permitted.',
+                })}
+              >
+                {getFieldDecorator('topics', {
+                  initialValue: [],
+                  rules: [
+                    {
+                      required: true,
+                      message: intl.formatMessage({
+                        id: 'topics_error_empty',
+                        defaultMessage: 'Please enter topics',
+                      }),
+                      type: 'array',
+                    },
+                    { validator: this.handleValidateTopics(this.props.intl) },
+                  ],
+                })(
+                  <Select
+                    onChange={this.props.onUpdate}
+                    className="Editor__topics"
+                    mode="tags"
+                    placeholder={intl.formatMessage({
+                      id: 'topics_placeholder',
+                      defaultMessage: 'Add story topics here',
+                    })}
+                    dropdownStyle={{ display: 'none' }}
+                    tokenSeparators={[' ', ',']}
+                  />,
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('body', {
+                  rules: [
+                    {
+                      required: true,
+                      message: intl.formatMessage({
+                        id: 'story_error_empty',
+                        defaultMessage: "Story content can't be empty.",
+                      }),
+                    },
+                  ],
+                })(
+                  <EditorInput
+                    autosize={{ minRows: 6, maxRows: 12 }}
+                    addon={
+                      <FormattedMessage
+                        id="reading_time"
+                        defaultMessage={'{words} words / {min} min read'}
+                        values={{
+                          words,
+                          min: Math.ceil(minutes),
+                        }}
+                      />
+                    }
+                    onChange={this.props.onUpdate}
+                    onImageUpload={this.props.onImageUpload}
+                    onImageInvalid={this.props.onImageInvalid}
+                    inputId={'fullscreen-editor-inputfile'}
+                  />,
+                )}
+              </Form.Item>
+              <Form.Item
+                className={classNames({ Editor__hidden: isUpdating })}
+                label={
+                  <span className="Editor__label">
+                    <FormattedMessage id="reward" defaultMessage="Reward" />
+                  </span>
+                }
+              >
+                {getFieldDecorator('reward')(
+                  <Select onChange={this.props.onUpdate} disabled={isUpdating}>
+                    <Select.Option value={rewardsValues.all}>
+                      <FormattedMessage id="reward_option_100" defaultMessage="100% Steem Power" />
+                    </Select.Option>
+                    <Select.Option value={rewardsValues.half}>
+                      <FormattedMessage id="reward_option_50" defaultMessage="50% SBD and 50% SP" />
+                    </Select.Option>
+                    <Select.Option value={rewardsValues.none}>
+                      <FormattedMessage id="reward_option_0" defaultMessage="Declined" />
+                    </Select.Option>
+                  </Select>,
+                )}
+              </Form.Item>
+              <Form.Item className={classNames({ Editor__hidden: isUpdating })}>
+                {getFieldDecorator('upvote', { valuePropName: 'checked', initialValue: true })(
+                  <Checkbox onChange={this.props.onUpdate} disabled={isUpdating}>
+                    <FormattedMessage id="like_post" defaultMessage="Like this post" />
+                  </Checkbox>,
+                )}
+              </Form.Item>
+              <div className="Editor__bottom">
+                <span className="Editor__bottom__info">
+                  <i className="iconfont icon-markdown" />{' '}
+                  <FormattedMessage
+                    id="markdown_supported"
+                    defaultMessage="Styling with markdown supported"
+                  />
+                </span>
+                <div className="Editor__bottom__right">
+                  {saving && (
+                    <span className="Editor__bottom__right__saving">
+                      <FormattedMessage id="saving" defaultMessage="Saving..." />
+                    </span>
+                  )}
+                  <Form.Item className="Editor__bottom__cancel">
+                    {draftId && (
+                      <Button type="danger" disabled={loading} onClick={this.handleDelete}>
+                        <FormattedMessage id="draft_delete" defaultMessage="Delete this draft" />
+                      </Button>
+                    )}
+                  </Form.Item>
+                  <Form.Item className="Editor__bottom__submit">
+                    {isUpdating ? (
+                      <Action
+                        primary
+                        loading={loading}
+                        disabled={loading}
+                        text={intl.formatMessage({
+                          id: loading ? 'post_send_progress' : 'post_update_send',
+                          defaultMessage: loading ? 'Submitting' : 'Update post',
+                        })}
+                      />
+                    ) : (
+                      <Action
+                        primary
+                        loading={loading}
+                        disabled={loading}
+                        text={intl.formatMessage({
+                          id: loading ? 'post_send_progress' : 'post_send',
+                          defaultMessage: loading ? 'Submitting' : 'Post',
+                        })}
+                      />
+                    )}
+                  </Form.Item>
+                </div>
+              </div>
+            </Form>
+          </div>
+          <div className="EditorFullscreen__column EditorFullscreen__preview">
+            {bodyHTML && (
+              <Form.Item
+                label={
+                  <span className="Editor__label">
+                    <FormattedMessage id="preview" defaultMessage="Preview" />
+                  </span>
+                }
+              >
+                <Body full body={bodyHTML} />
+              </Form.Item>
+            )}
+          </div>
+        </div>
+      </Modal>
+    );
+  }
+}
+
+export default EditorFullScreen;
