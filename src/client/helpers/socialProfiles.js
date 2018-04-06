0d54a2215cdc8c1f3ccb44c04abe6ffe448a3583
Diego Hernandes <diego@hernandev.com>
2018-04-06 04:21:20 -0300

    fix brand name typo

--- src/client/helpers/socialProfiles.js
+++ src/client/helpers/socialProfiles.js
@@ -18,7 +18,7 @@ export const getTwitterShareURL = (text, url) =>
 export default [
   { id: 'facebook', icon: 'facebook', color: '#3b5998', name: 'Facebook' },
   { id: 'twitter', icon: 'twitter', color: '#00aced', name: 'Twitter' },
-  { id: 'linkedin', icon: 'linkedin', color: '#007bb6', name: 'Linkedin' },
+  { id: 'linkedin', icon: 'linkedin', color: '#007bb6', name: 'LinkedIn' },
   { id: 'youtube', icon: 'youtube', color: '#ff0202', name: 'YouTube' },
   { id: 'instagram', icon: 'instagram', color: '#8a3ab9', name: 'Instagram' },
   { id: 'github', icon: 'github', color: 'black', name: 'GitHub' },
