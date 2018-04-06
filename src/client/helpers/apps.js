ee2e299532bf31c941b0ffe6200d516dd1378ec6
Diego Hernandes <diego@hernandev.com>
2018-04-06 02:29:41 -0300

    style fix

--- src/client/helpers/apps.js
+++ src/client/helpers/apps.js
@@ -1,13 +1,13 @@
 // get full application list from scripts json.
-const appsList = require('steemscript/apps.json')
+const appsList = require('steemscript/apps.json');
 
 // start an empty object for the applications names.
-const apps = {}
+const apps = {};
 
 // "map" each one extracting the name key
 Object.keys(appsList).forEach((key) => {
-  apps[key] = appsList[key].name
+  apps[key] = appsList[key].name;
 });
 
 // export the result.
-export default apps
+export default apps;
