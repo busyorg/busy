cc7e5fb61d21990ca6d598687906800f5188f729
Diego Hernandes <diego@hernandev.com>
2018-04-07 17:23:52 -0300

    remove comments

--- src/client/helpers/apps.js
+++ src/client/helpers/apps.js
@@ -1,13 +1,9 @@
-// get full application list from scripts json.
 const appsList = require('steemscript/apps.json');
 
-// start an empty object for the applications names.
 const apps = {};
 
-// "map" each one extracting the name key
 Object.keys(appsList).forEach(key => {
   apps[key] = appsList[key].name;
 });
 
-// export the result.
 export default apps;
