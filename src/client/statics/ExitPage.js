6d6c7fdf0c820718f30351a8f0e0f98f1860cf3b
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-04-13 22:23:37 +0200

    feat: add back button to ExitPage

--- src/client/statics/ExitPage.js
+++ src/client/statics/ExitPage.js
@@ -1,35 +1,44 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import 'url-search-params-polyfill';
 import ActionLink from '../components/Button/ActionLink';
+import ActionButton from '../components/Button/Action';
 import './ExitPage.less';
 
-const ExitPage = ({ location }) => {
-  const url = new URLSearchParams(location.search).get('url');
+export default class ExitPage extends React.Component {
+  closeWindow = () => {
+    if (typeof window !== 'undefined') {
+      window.close();
+    }
+  };
 
-  if (!url) {
-    return null;
+  render() {
+    const url = new URLSearchParams(location.search).get('url');
+
+    if (!url) {
+      return null;
+    }
+
+    return (
+      <div className="ExitPage container">
+        <h2>
+          <FormattedMessage id="page_exit" defaultMessage="You are leaving Busy.org" />
+        </h2>
+        <FormattedMessage
+          id="page_exit_message"
+          defaultMessage="You are about to go to external website. Please be careful when sharing your credentials."
+        />
+        <pre>{url}</pre>
+        <ActionLink style={{ width: 'auto' }} primary href={url}>
+          <FormattedMessage id="page_exit_go" defaultMessage="Visit this website" />
+        </ActionLink>
+        <ActionButton
+          style={{ width: 'auto', marginLeft: '16px' }}
+          small
+          text="Go back"
+          onClick={this.closeWindow}
+        />
+      </div>
+    );
   }
-
-  return (
-    <div className="ExitPage container">
-      <h2>
-        <FormattedMessage id="page_exit" defaultMessage="You are leaving Busy.org" />
-      </h2>
-      <FormattedMessage
-        id="page_exit_message"
-        defaultMessage="You are about to go to external website. Please be careful when sharing your credentials."
-      />
-      <pre>{url}</pre>
-      <ActionLink style={{ width: 'auto' }} primary href={url}>
-        <FormattedMessage id="page_exit_go" defaultMessage="Visit this website" />
-      </ActionLink>
-    </div>
-  );
-};
-ExitPage.propTypes = {
-  location: PropTypes.shape().isRequired,
-};
-
-export default ExitPage;
+}
