edd258c53c694e6bd2b3da77b6f65fc9331465b6
JM Myers <jm.myers@everbridge.com>
2018-04-17 19:51:31 -0400

    show slider for mobile

--- src/client/components/HeroBanner.js
+++ src/client/components/HeroBanner.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import _ from 'lodash';
+import { Carousel } from 'antd';
 import './HeroBanner.less';
+import './HeroBannerSlider.less';
+
+const SLIDER_CONTENTS = [
+  {
+    image: '/images/hero-1.svg',
+    titleID: 'hero_banner_title_1',
+    defaultTitle: 'Write and publish a post on Steem',
+    className: 'HeroBannerSlider__image HeroBannerSlider__image-1',
+  },
+  {
+    image: '/images/hero-2.svg',
+    titleID: 'hero_banner_title_2',
+    defaultTitle: 'The community upvotes your post',
+    className: 'HeroBannerSlider__image',
+  },
+  {
+    image: '/images/hero-3.svg',
+    titleID: 'hero_banner_title_3',
+    defaultTitle: 'Earn rewards in Steem',
+    className: 'HeroBannerSlider__image',
+  },
+];
 
 const HeroBanner = ({ visible, onCloseClick }) => {
   if (!visible) return null;
@@ -58,6 +82,24 @@ const HeroBanner = ({ visible, onCloseClick }) => {
               </div>
             </div>
           </div>
+        </div>
+        <div className="HeroBannerSlider__container">
+          <Carousel effect="fade" autoplay autoplaySpeed={8000}>
+            {_.map(SLIDER_CONTENTS, slide => (
+              <div key={slide.titleID}>
+                <div className="HeroBannerSlider">
+                  <div className={slide.className}>
+                    <img src={slide.image} alt={slide.titleID} />
+                  </div>
+                  <div className="HeroBannerSlider__content">
+                    <div className="HeroBannerSlider__title">
+                      <FormattedMessage id={slide.titleID} defaultMessage={slide.defaultTitle} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Carousel>
         </div>
       </div>
     </div>
