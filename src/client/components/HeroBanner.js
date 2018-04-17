3f33401c67d13be17c259948cae9ad8b84fdae64
JM Myers <jm.myers@everbridge.com>
2018-04-17 16:59:45 -0400

    wip - new hero banner

--- src/client/components/HeroBanner.js
+++ src/client/components/HeroBanner.js
@@ -1,40 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 import { FormattedMessage } from 'react-intl';
-import { Carousel } from 'antd';
 import './HeroBanner.less';
 import './HeroBannerSlider.less';
-
-const SLIDER_CONTENTS = [
-  {
-    image: '/images/hero-1.svg',
-    titleID: 'hero_slider_title_1',
-    defaultTitle: 'Write and publish post',
-    descriptionID: 'hero_slider_description_1',
-    defaultDescription:
-      'Spread your ideas with the world through the easy to use and interactive Busy editor with markdown built-in',
-    className: 'HeroBannerSlider__image HeroBannerSlider__image-1',
-  },
-  {
-    image: '/images/hero-2.svg',
-    titleID: 'hero_slider_title_2',
-    defaultTitle: 'Get upvoted by the community',
-    descriptionID: 'hero_slider_description_1',
-    defaultDescription:
-      'By providing value to the community in any form, the people can upvote you to show their appreciation and reward your efforts',
-    className: 'HeroBannerSlider__image',
-  },
-  {
-    image: '/images/hero-3.svg',
-    titleID: 'hero_slider_title_3',
-    defaultTitle: 'Get rewarded in Steem',
-    descriptionID: 'hero_slider_description_1',
-    defaultDescription:
-      'Contrary to other social media networks, here you can earn cryptocurrency that has a real dollar value for your work',
-    className: 'HeroBannerSlider__image',
-  },
-];
 
 const HeroBanner = ({ visible, onCloseClick }) => {
   if (!visible) return null;
@@ -45,29 +13,40 @@ const HeroBanner = ({ visible, onCloseClick }) => {
         <i className="iconfont icon-close" />
       </a>
       <div className="HeroBanner__container">
-        <div className="HeroBanner__container__content HeroBannerSlider__container">
-          <Carousel effect="fade" autoplay autoplaySpeed={8000}>
-            {_.map(SLIDER_CONTENTS, slide => (
-              <div key={slide.titleID}>
-                <div className="HeroBannerSlider">
-                  <div className={slide.className}>
-                    <img src={slide.image} alt={slide.titleID} />
-                  </div>
-                  <div className="HeroBannerSlider__content">
-                    <h1 className="HeroBannerSlider__title">
-                      <FormattedMessage id={slide.titleID} defaultMessage={slide.defaultTitle} />
-                    </h1>
-                    <span className="HeroBannerSlider__description">
-                      <FormattedMessage
-                        id={slide.descriptionID}
-                        defaultMessage={slide.defaultDescription}
-                      />
-                    </span>
-                  </div>
-                </div>
+        <div className="HeroBanner__content-container">
+          <div className="HeroBanner__content">
+            <img src="/images/hero-1.svg" className="HeroBanner__content__image" />
+            <div className="HeroBanner__content__description">
+              <div className="HeroBanner__content__number">1</div>
+              <div className="HeroBanner__content__title">
+                <FormattedMessage
+                  id="hero_banner_title_1"
+                  defaultMessage="Write and publish a post on Steem"
+                />
               </div>
-            ))}
-          </Carousel>
+            </div>
+          </div>
+          <div className="HeroBanner__content">
+            <img src="/images/hero-2.svg" className="HeroBanner__content__image" />
+            <div className="HeroBanner__content__description">
+              <div className="HeroBanner__content__number">2</div>
+              <div className="HeroBanner__content__title">
+                <FormattedMessage
+                  id="hero_banner_title_2"
+                  defaultMessage="The community upvotes your post"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="HeroBanner__content">
+            <img src="/images/hero-3.svg" className="HeroBanner__content__image" />
+            <div className="HeroBanner__content__description">
+              <div className="HeroBanner__content__number">3</div>
+              <div className="HeroBanner__content__title">
+                <FormattedMessage id="hero_banner_title_3" defaultMessage="Earn rewards in Steem" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
