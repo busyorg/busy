5fd90ce187a1b0fa779a869e6efc1f5b8bc36a3d
JM Myers <jm.myers@everbridge.com>
2018-04-26 13:56:06 -0400

    fix for react slick and antd carousel slider

--- src/client/components/HeroBanner.js
+++ src/client/components/HeroBanner.js
@@ -6,118 +6,138 @@ import { Carousel } from 'antd';
 import './HeroBanner.less';
 import './HeroBannerSlider.less';
 
-const SLIDER_CONTENTS = [
-  {
-    image: '/images/hero-1.svg',
-    titleID: 'hero_banner_title_1',
-    defaultTitle: 'Write and publish a post on Steem',
-    className: 'HeroBannerSlider__image HeroBannerSlider__image-1',
-    number: 1,
-  },
-  {
-    image: '/images/hero-2.svg',
-    titleID: 'hero_banner_title_2',
-    defaultTitle: 'The community upvotes your post',
-    className: 'HeroBannerSlider__image',
-    number: 2,
-  },
-  {
-    image: '/images/hero-3.svg',
-    titleID: 'hero_banner_title_3',
-    defaultTitle: 'Earn rewards in Steem',
-    className: 'HeroBannerSlider__image',
-    number: 3,
-  },
-];
+class HeroBanner extends React.Component {
+  static SLIDER_CONTENTS = [
+    {
+      image: '/images/hero-1.svg',
+      titleID: 'hero_banner_title_1',
+      defaultTitle: 'Write and publish a post on Steem',
+      className: 'HeroBannerSlider__image',
+      number: 1,
+    },
+    {
+      image: '/images/hero-2.svg',
+      titleID: 'hero_banner_title_2',
+      defaultTitle: 'The community upvotes your post',
+      className: 'HeroBannerSlider__image',
+      number: 2,
+    },
+    {
+      image: '/images/hero-3.svg',
+      titleID: 'hero_banner_title_3',
+      defaultTitle: 'Earn rewards in Steem',
+      className: 'HeroBannerSlider__image',
+      number: 3,
+    },
+  ];
 
-const HeroBanner = ({ visible, onCloseClick }) => {
-  if (!visible) return null;
+  static propTypes = {
+    visible: PropTypes.bool,
+    onCloseClick: PropTypes.func,
+  };
 
-  return (
-    <div className="HeroBanner">
-      <a onClick={onCloseClick} role="button" tabIndex="0" className="HeroBanner__close">
-        <i className="iconfont icon-close" />
-      </a>
-      <div className="HeroBanner__container">
-        <div className="HeroBanner__content-container">
-          <div className="HeroBanner__content">
-            <img
-              src="/images/hero-1.svg"
-              className="HeroBanner__content__image"
-              alt="Write and publish a post on Steem"
-            />
-            <div className="HeroBanner__content__description">
-              <div className="HeroBanner__content__number">1</div>
-              <div className="HeroBanner__content__title">
-                <FormattedMessage
-                  id="hero_banner_title_1"
-                  defaultMessage="Write and publish a post on Steem"
-                />
+  static defaultProps = {
+    visible: true,
+    onCloseClick: () => {},
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentWillMount() {
+    setTimeout(() => this.setState({ loading: false }, () => this.forceUpdate()), 1500);
+  }
+
+  render() {
+    const { onCloseClick, visible } = this.props;
+    const { loading } = this.state;
+    const sliderStyles = loading ? { visibility: 'hidden' } : {};
+    if (!visible) return null;
+
+    return (
+      <div className="HeroBanner">
+        <a onClick={onCloseClick} role="button" tabIndex="0" className="HeroBanner__close">
+          <i className="iconfont icon-close" />
+        </a>
+        <div className="HeroBanner__container">
+          <div className="HeroBanner__content-container">
+            <div className="HeroBanner__content">
+              <img
+                src="/images/hero-1.svg"
+                className="HeroBanner__content__image"
+                alt="Write and publish a post on Steem"
+              />
+              <div className="HeroBanner__content__description">
+                <div className="HeroBanner__content__number">1</div>
+                <div className="HeroBanner__content__title">
+                  <FormattedMessage
+                    id="hero_banner_title_1"
+                    defaultMessage="Write and publish a post on Steem"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="HeroBanner__content">
+              <img
+                src="/images/hero-2.svg"
+                className="HeroBanner__content__image"
+                alt="The community upvotes your post"
+              />
+              <div className="HeroBanner__content__description">
+                <div className="HeroBanner__content__number">2</div>
+                <div className="HeroBanner__content__title">
+                  <FormattedMessage
+                    id="hero_banner_title_2"
+                    defaultMessage="The community upvotes your post"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="HeroBanner__content">
+              <img
+                src="/images/hero-3.svg"
+                className="HeroBanner__content__image"
+                alt="Earn rewards in Steem"
+              />
+              <div className="HeroBanner__content__description">
+                <div className="HeroBanner__content__number">3</div>
+                <div className="HeroBanner__content__title" style={{ width: '130px' }}>
+                  <FormattedMessage
+                    id="hero_banner_title_3"
+                    defaultMessage="Earn rewards in Steem"
+                  />
+                </div>
               </div>
             </div>
           </div>
-          <div className="HeroBanner__content">
-            <img
-              src="/images/hero-2.svg"
-              className="HeroBanner__content__image"
-              alt="The community upvotes your post"
-            />
-            <div className="HeroBanner__content__description">
-              <div className="HeroBanner__content__number">2</div>
-              <div className="HeroBanner__content__title">
-                <FormattedMessage
-                  id="hero_banner_title_2"
-                  defaultMessage="The community upvotes your post"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="HeroBanner__content">
-            <img
-              src="/images/hero-3.svg"
-              className="HeroBanner__content__image"
-              alt="Earn rewards in Steem"
-            />
-            <div className="HeroBanner__content__description">
-              <div className="HeroBanner__content__number">3</div>
-              <div className="HeroBanner__content__title" style={{ width: '130px' }}>
-                <FormattedMessage id="hero_banner_title_3" defaultMessage="Earn rewards in Steem" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="HeroBannerSlider__container">
-          <Carousel effect="fade" autoplay autoplaySpeed={8000}>
-            {_.map(SLIDER_CONTENTS, slide => (
-              <div key={slide.titleID}>
-                <div className="HeroBannerSlider">
-                  <div className={slide.className}>
-                    <img src={slide.image} alt={slide.titleID} />
-                  </div>
-                  <div className="HeroBannerSlider__content">
-                    <div className="HeroBannerSlider__content__number">{slide.number}</div>
-                    <div className="HeroBannerSlider__content__title">
-                      <FormattedMessage id={slide.titleID} defaultMessage={slide.defaultTitle} />
+          <div className="HeroBannerSlider__container" style={sliderStyles}>
+            <Carousel effect="fade" autoplay autoplaySpeed={8000}>
+              {_.map(HeroBanner.SLIDER_CONTENTS, slide => (
+                <div key={slide.titleID}>
+                  <div className="HeroBannerSlider">
+                    <div className={slide.className}>
+                      <img src={slide.image} alt={slide.titleID} />
+                    </div>
+                    <div className="HeroBannerSlider__content">
+                      <div className="HeroBannerSlider__content__number">{slide.number}</div>
+                      <div className="HeroBannerSlider__content__title">
+                        <FormattedMessage id={slide.titleID} defaultMessage={slide.defaultTitle} />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </Carousel>
+              ))}
+            </Carousel>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-HeroBanner.propTypes = {
-  visible: PropTypes.bool,
-  onCloseClick: PropTypes.func,
-};
-
-HeroBanner.defaultProps = {
-  visible: true,
-  onCloseClick: () => {},
-};
+    );
+  }
+}
 
 export default HeroBanner;
