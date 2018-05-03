e108898f25fbebfd00ad2ad07b34c3d1a6bf17b7
th7nder <vesphy@gmail.com>
2018-05-03 21:38:56 +0200

    feat: button shows after inactivity

--- src/client/components/BBackTop.js
+++ src/client/components/BBackTop.js
@@ -4,23 +4,69 @@ import { BackTop } from 'antd';
 import classNames from 'classnames';
 import './BBackTop.less';
 
-const BBackTop = ({ isModal, ...restProps }) => (
-  <div className="BBackTop">
-    <div
-      className={classNames('BBackTop__container', {
-        'BBackTop__container--shifted': isModal,
-      })}
-    >
-      <BackTop {...restProps} className="BBackTop_button" />
-    </div>
-  </div>
-);
+class BBackTop extends React.Component {
+  static propTypes = {
+    isModal: PropTypes.bool,
+    target: PropTypes.node,
+  };
 
-BBackTop.propTypes = {
-  isModal: PropTypes.bool,
-};
+  static defaultProps = {
+    isModal: false,
+    target: undefined,
+  };
 
-BBackTop.defaultProps = {
-  isModal: false,
-};
+  static getDefaultTarget() {
+    return window;
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
+
+  componentDidMount() {
+    this.scrollEvent = this.getTarget().addEventListener('scroll', this.handleScroll);
+    this.timer = setInterval(() => {
+      if (Date.now() - this.lastTick > 3000) {
+        this.setState({ visible: true });
+      }
+    }, 500);
+    this.lastTick = 0;
+  }
+
+  componentWillUnmount() {
+    if (this.scrollEvent) this.scrollEvent.remove();
+    if (this.timer) clearInterval(this.timer);
+  }
+
+  getTarget() {
+    return this.props.target || BBackTop.getDefaultTarget();
+  }
+
+  handleScroll = () => {
+    this.lastTick = Date.now();
+    if (this.state.visible === true) {
+      this.setState({ visible: false });
+    }
+  };
+
+  render() {
+    return (
+      this.state.visible && (
+        <div className="BBackTop">
+          <div
+            className={classNames('BBackTop__container', {
+              'BBackTop__container--shifted': this.props.isModal,
+            })}
+          >
+            <BackTop {...this.props} className="BBackTop_button" />
+          </div>
+        </div>
+      )
+    );
+  }
+}
+
 export default BBackTop;
