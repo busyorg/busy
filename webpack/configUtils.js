4ddfe2d626247834bfc6e5cf96e72b3d0294571e
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-05-03 18:43:47 +0200

    fix: update define plugin

--- webpack/configUtils.js
+++ webpack/configUtils.js
@@ -20,16 +20,22 @@ const POSTCSS_LOADER = {
 };
 
 const DEFINE_PLUGIN = new webpack.DefinePlugin({
-  'process.env': {
-    NODE_ENV: isDevelopment ? JSON.stringify('development') : JSON.stringify('production'),
-    STEEMCONNECT_CLIENT_ID: JSON.stringify(process.env.STEEMCONNECT_CLIENT_ID || 'busy.app'),
-    STEEMCONNECT_REDIRECT_URL: JSON.stringify(
-      process.env.STEEMCONNECT_REDIRECT_URL || 'http://localhost:3000/callback',
-    ),
-    STEEMCONNECT_HOST: JSON.stringify(process.env.STEEMCONNECT_HOST || 'https://steemconnect.com'),
-    STEEMJS_URL: JSON.stringify(process.env.STEEMJS_URL || 'https://api.steemit.com'),
-    SIGNUP_URL: JSON.stringify(process.env.SIGNUP_URL || 'https://signup.steemit.com/?ref=busy'),
-  },
+  'process.env.NODE_ENV': isDevelopment
+    ? JSON.stringify('development')
+    : JSON.stringify('production'),
+  'process.env.STEEMCONNECT_CLIENT_ID': JSON.stringify(
+    process.env.STEEMCONNECT_CLIENT_ID || 'busy.app',
+  ),
+  'process.env.STEEMCONNECT_REDIRECT_URL': JSON.stringify(
+    process.env.STEEMCONNECT_REDIRECT_URL || 'http://localhost:3000/callback',
+  ),
+  'process.env.STEEMCONNECT_HOST': JSON.stringify(
+    process.env.STEEMCONNECT_HOST || 'https://steemconnect.com',
+  ),
+  'process.env.STEEMJS_URL': JSON.stringify(process.env.STEEMJS_URL || 'https://api.steemit.com'),
+  'process.env.SIGNUP_URL': JSON.stringify(
+    process.env.SIGNUP_URL || 'https://signup.steemit.com/?ref=busy',
+  ),
 });
 
 module.exports = {
