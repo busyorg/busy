8702cb03ced3f0d9c252a9d52cd204af40eaa55b
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-05-11 00:51:00 +0200

    chore: cleanup

--- public/js/busyapp-294.cfb750c8d8579d925043.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([294],{1659:function(n,c){}});
-//# sourceMappingURL=busyapp-294.cfb750c8d8579d925043.js.map
--- public/js/busyapp-296.e57a3316ffd689c92ed0.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([296],{1657:function(n,c){}});
-//# sourceMappingURL=busyapp-296.e57a3316ffd689c92ed0.js.map
