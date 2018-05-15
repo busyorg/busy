e9cd31ad2ec540ad8e2876dd503e5ab55765774b
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-05-15 19:41:45 +0200

    fix: catch analytics errors (#1857)

--- src/client/history.js
+++ src/client/history.js
@@ -4,10 +4,14 @@ import createMemoryHistory from 'history/createMemoryHistory';
 const history = typeof window === 'undefined' ? createMemoryHistory() : createBrowserHistory();
 
 if (typeof window !== 'undefined' && window.analytics) {
-  window.analytics.page({ url: history.location.pathname });
-  history.listen(location => {
-    window.analytics.page({ url: location.pathname });
-  });
+  try {
+    window.analytics.page({ url: history.location.pathname });
+    history.listen(location => {
+      window.analytics.page({ url: location.pathname });
+    });
+  } catch (err) {
+    console.log('Logging error', err);
+  }
 }
 
 export default history;
