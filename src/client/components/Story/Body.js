7d5ce6ed61f523acb78130f63aca24deaf86311d
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-05-24 11:53:04 +0200

    feat: migrate to showdownjs

--- src/client/components/Story/Body.js
+++ src/client/components/Story/Body.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import classNames from 'classnames';
 import sanitizeHtml from 'sanitize-html';
-import Remarkable from 'remarkable';
+import showdown from 'showdown';
 import embedjs from 'embedjs';
 import { jsonParse } from '../../helpers/formatter';
 import sanitizeConfig from '../../vendor/SanitizeConfig';
@@ -14,12 +14,9 @@ import improve from '../../helpers/improve';
 import PostFeedEmbed from './PostFeedEmbed';
 import './Body.less';
 
-export const remarkable = new Remarkable({
-  html: true, // remarkable renders first then sanitize runs...
-  breaks: true,
-  linkify: false, // linkify is done locally
-  typographer: false, // https://github.com/jonschlinkert/remarkable/issues/142#issuecomment-221546793
-  quotes: '“”‘’',
+export const converter = new showdown.Converter({
+  tables: true,
+  strikethrough: true,
 });
 
 const getEmbed = link => {
@@ -54,7 +51,7 @@ export function getHtml(body, jsonMetadata = {}, returnType = 'Object', options 
 
   const htmlReadyOptions = { mutate: true, resolveIframe: returnType === 'text' };
   parsedBody = improve(parsedBody);
-  parsedBody = remarkable.render(parsedBody);
+  parsedBody = converter.makeHtml(parsedBody);
 
   if (options.rewriteLinks) {
     parsedBody = parsedBody.replace(
