d09dbba4e5aa55237c3da5bcba11746ba6fb5888
Emre Yılmaz <hello@emre.sh>
2018-05-24 19:55:45 +0300

    Show powered-up username on powerup transaction (#1835)

--- src/client/wallet/PowerUpTransaction.js
+++ src/client/wallet/PowerUpTransaction.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { FormattedMessage, FormattedRelative, FormattedDate, FormattedTime } from 'react-intl';
 import BTooltip from '../components/BTooltip';
 
-const PowerUpTransaction = ({ timestamp, amount }) => (
+const PowerUpTransaction = ({ timestamp, amount, to, from }) => (
   <div className="UserWalletTransactions__transaction">
     <div className="UserWalletTransactions__icon-container">
       <i className="iconfont icon-flashlight_fill UserWalletTransactions__icon" />
     </div>
     <div className="UserWalletTransactions__content">
       <div className="UserWalletTransactions__content-recipient">
-        <FormattedMessage id="powered_up" defaultMessage="Powered up " />
+        {to === from ? (
+          <FormattedMessage id="powered_up" defaultMessage="Powered up " />
+        ) : (
+          <FormattedMessage
+            id="powered_up_to"
+            defaultMessage="Powered up {to} "
+            values={{
+              to: (
+                <Link to={`/@${to}`}>
+                  <span className="username">{to}</span>
+                </Link>
+              ),
+            }}
+          />
+        )}
+
         <span className="UserWalletTransactions__payout">{amount}</span>
       </div>
       <span className="UserWalletTransactions__timestamp">
@@ -33,6 +49,8 @@ const PowerUpTransaction = ({ timestamp, amount }) => (
 PowerUpTransaction.propTypes = {
   timestamp: PropTypes.string.isRequired,
   amount: PropTypes.element.isRequired,
+  to: PropTypes.string.isRequired,
+  from: PropTypes.string.isRequired,
 };
 
 export default PowerUpTransaction;
