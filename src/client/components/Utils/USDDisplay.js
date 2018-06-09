f6fee634e688ebac848f37b4ff33061c2937db39
Pratyush Singh <singh.pratyush96@gmail.com>
2018-06-10 01:56:53 +0530

    Fix #1947: Show negative votes' value in reaction modal

--- src/client/components/Utils/USDDisplay.js
+++ src/client/components/Utils/USDDisplay.js
@@ -2,12 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedNumber } from 'react-intl';
 
-const USDDisplay = ({ value }) => (
-  <span>
-    {'$'}
-    <FormattedNumber value={value} minimumFractionDigits={2} maximumFractionDigits={2} />
-  </span>
-);
+const USDDisplay = ({ value }) => {
+  const negative = value.toFixed(2) < 0;
+  const absValue = Math.abs(value);
+  return (
+    <span>
+      {negative && '-'}
+      {'$'}
+      <FormattedNumber value={absValue} minimumFractionDigits={2} maximumFractionDigits={2} />
+    </span>
+  );
+};
 
 USDDisplay.propTypes = {
   value: PropTypes.number,
