a16a6a8fc841999c1ae2d03e350aac74bd3e9245
mkt <kontakt@markus-kottlaender.de>
2018-06-15 12:03:21 +0200

    added isJsonStr helper

--- src/client/helpers/formatter.js
+++ src/client/helpers/formatter.js
@@ -6,6 +6,15 @@ export const jsonParse = str => {
   }
 };
 
+export const isJsonStr = str => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
+
 export const epochToUTC = epochTimestamp => new Date(0).setUTCSeconds(epochTimestamp);
 
 export default jsonParse;
