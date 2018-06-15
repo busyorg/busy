be6f204831e9ba03e227f57ee8cb9978b892ce0d
Grégory <gregory.latinier@gmail.com>
2018-06-15 09:56:00 +0200

    Remove the useless react component

--- src/client/containers/Story/BodyContainer.js
+++ src/client/containers/Story/BodyContainer.js
@@ -1,10 +1,7 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import Body from '../../components/Story/Body';
 import { getExitPageSetting } from '../../reducers';
 
-const BodyContainer = props => <Body {...props} />;
-
 export default connect(state => ({
   exitPageSetting: getExitPageSetting(state),
-}))(BodyContainer);
+}))(Body);
