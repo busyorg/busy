fdef6de03c97c1d00e8c1b5e01d7cff349907639
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-07-03 16:06:49 +0200

    fix: loading more feed content (#2028)

--- src/client/feed/feedReducer.js
+++ src/client/feed/feedReducer.js
@@ -28,8 +28,8 @@ const feedIdsList = (state = [], action) => {
     case feedTypes.GET_REPLIES.SUCCESS:
     case feedTypes.GET_BOOKMARKS.SUCCESS:
       return action.payload.map(post => post.id);
-    case feedTypes.GET_MORE_FEED_CONTENT:
-    case feedTypes.GET_MORE_USER_COMMENTS:
+    case feedTypes.GET_MORE_FEED_CONTENT.SUCCESS:
+    case feedTypes.GET_MORE_USER_COMMENTS.SUCCESS:
     case feedTypes.GET_MORE_REPLIES.SUCCESS:
       return _.uniq([...state, ...action.payload.map(post => post.id)]);
     default:
