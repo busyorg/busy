2c97a4ade0f55e849025fce93109a4b7d0d4ed16
lopu <ireply@myleisure.com.au>
2018-09-11 05:12:46 +1000

    changed sidenav system

--- src/client/components/Navigation/Sidenav.js
+++ src/client/components/Navigation/Sidenav.js
@@ -9,159 +9,166 @@ const isWallet = (match, location) => location.pathname.match(/wallet/);
 const isReplies = (match, location) => location.pathname.match(/replies/);
 
 const Sidenav = ({ username }) =>
-  username ? (
-    <ul className="Sidenav">
-      <li>
-        <NavLink to={`/@${username}`}>
-          <i className="iconfont icon-mine" />
-          <FormattedMessage id="my_profile" defaultMessage="My profile" />
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/" activeClassName="Sidenav__item--active" exact>
-          <i className="iconfont icon-clock" />
-          <FormattedMessage id="feed" defaultMessage="Feed" />
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/trending" activeClassName="Sidenav__item--active" isActive={isNews}>
-          <i className="iconfont icon-headlines" />
-          <FormattedMessage id="news" defaultMessage="News" />
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/replies" activeClassName="Sidenav__item--active" isActive={isReplies}>
-          <i className="iconfont icon-eit" />
-          <FormattedMessage id="mentions" defaultMessage="Mentions" />
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/wallet" activeClassName="Sidenav__item--active" isActive={isWallet}>
-          <i className="iconfont icon-wallet" />
-          <FormattedMessage id="wallet" defaultMessage="Wallet" />
-        </NavLink>
-      </li>
-      <li>
-				<div className="floating-label-container">
-					<div className="floating-label-positioner">
-						<div className="floating-label">
-							<span>soon</span>
-						</div>
-					</div>
-				</div>
-
-        <NavLink to="/exchange" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
-          <i className="iconfont icon-chart" />
-          <FormattedMessage id="exchange" defaultMessage="Exchange" />
-        </NavLink>
-      </li>
-      <li>
-				<div className="floating-label-container">
-					<div className="floating-label-positioner">
-						<div className="floating-label">
-							<span>soon</span>
-						</div>
-					</div>
-				</div>
-        <NavLink to="/groups" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
-          <i className="iconfont icon-group" />
-          <FormattedMessage id="Groups" defaultMessage="Groups" />
-        </NavLink>
-      </li>
-      <li>
-				<div className="floating-label-container">
-					<div className="floating-label-positioner">
-						<div className="floating-label">
-							<span>soon</span>
-						</div>
-					</div>
-				</div>
-        <NavLink to="/apps" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
-					{/* <i className="iconfont icon-manage" /> */}
-					<i className="iconfont icon-mobilephone_fill" />
-          <FormattedMessage id="apps" defaultMessage="Apps" />
-        </NavLink>
-      </li>
-      <li>
-				<div className="floating-label-container">
-					<div className="floating-label-positioner">
-						<div className="floating-label">
-							<span>soon</span>
-						</div>
-					</div>
-				</div>
-        <NavLink to="/marketplace" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
-          <i className="iconfont icon-shop_fill" />
-          <FormattedMessage id="marketplace" defaultMessage="Marketplace" />
-        </NavLink>
-      </li>
-      <li>
-				<div className="floating-label-container">
-					<div className="floating-label-positioner">
-						<div className="floating-label">
-							<span>soon</span>
-						</div>
-					</div>
-				</div>
-        <NavLink to="/events" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
-          <i className="iconfont icon-remind_fill" />
-          <FormattedMessage id="events" defaultMessage="Events" />
-        </NavLink>
-      </li>
-      <li>
-				<div className="floating-label-container">
-					<div className="floating-label-positioner">
-						<div className="floating-label">
-							<span>soon</span>
-						</div>
-					</div>
-				</div>
-        <NavLink to="/boards" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
-          <i className="iconfont icon-barrage" />
-          <FormattedMessage id="boards" defaultMessage="Boards" />
-        </NavLink>
-      </li>
-      <li>
-				<div className="floating-label-container">
-					<div className="floating-label-positioner">
-						<div className="floating-label">
-							<span>soon</span>
-						</div>
-					</div>
-				</div>
-        <NavLink to="/feeds" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
-          <i className="iconfont icon-keyboard" />
-          <FormattedMessage id="feeds" defaultMessage="Feeds" />
-        </NavLink>
-      </li>
+	<ul className="Sidenav">
+		{ username ? (
+		<li>
+			<NavLink to={`/@${username}`}>
+				<i className="iconfont icon-mine" />
+				<FormattedMessage id="my_profile" defaultMessage="My profile" />
+			</NavLink>
+		</li>
+		) : null}
+		<li>
+			<NavLink to="/" activeClassName="Sidenav__item--active" exact>
+				<i className="iconfont icon-clock" />
+				<FormattedMessage id="feed" defaultMessage="Feed" />
+			</NavLink>
+		</li>
+		<li>
+			<NavLink to="/trending" activeClassName="Sidenav__item--active" isActive={isNews}>
+				<i className="iconfont icon-headlines" />
+				<FormattedMessage id="news" defaultMessage="News" />
+			</NavLink>
+		</li>
+		{ username ? 
+		(
 			<li>
-				<div className="floating-label-container">
-					<div className="floating-label-positioner">
-						<div className="floating-label">
-							<span>soon</span>
-						</div>
-					</div>
-				</div>
-				<NavLink to="/messenger" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
-					<i className="iconfont icon-interactive_fill" />
-					<FormattedMessage id="messenger" defaultMessage="Messenger" />
+				<NavLink to="/replies" activeClassName="Sidenav__item--active" isActive={isReplies}>
+					<i className="iconfont icon-eit" />
+					<FormattedMessage id="mentions" defaultMessage="Mentions" />
 				</NavLink>
 			</li>
+		) : null
+		}
+		{ username ? 
+		(
 			<li>
-				<div className="floating-label-container">
-					<div className="floating-label-positioner">
-						<div className="floating-label">
-							<span>soon</span>
-						</div>
-					</div>
-				</div>
-				<NavLink to="/network" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
-					<i className="iconfont icon-share" />
-					<FormattedMessage id="network" defaultMessage="Network" />
+				<NavLink to="/wallet" activeClassName="Sidenav__item--active" isActive={isWallet}>
+					<i className="iconfont icon-wallet" />
+					<FormattedMessage id="wallet" defaultMessage="Wallet" />
 				</NavLink>
 			</li>
-		</ul>
-) : null;
+		) : null }
+		<li>
+			<div className="floating-label-container">
+				<div className="floating-label-positioner">
+					<div className="floating-label">
+						<span>soon</span>
+					</div>
+				</div>
+			</div>
+			<NavLink to="/exchange" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
+				<i className="iconfont icon-chart" />
+				<FormattedMessage id="exchange" defaultMessage="Exchange" />
+			</NavLink>
+		</li>
+		<li>
+			<div className="floating-label-container">
+				<div className="floating-label-positioner">
+					<div className="floating-label">
+						<span>soon</span>
+					</div>
+				</div>
+			</div>
+			<NavLink to="/groups" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
+				<i className="iconfont icon-group" />
+				<FormattedMessage id="Groups" defaultMessage="Groups" />
+			</NavLink>
+		</li>
+		<li>
+			<div className="floating-label-container">
+				<div className="floating-label-positioner">
+					<div className="floating-label">
+						<span>soon</span>
+					</div>
+				</div>
+			</div>
+			<NavLink to="/apps" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
+				{/* <i className="iconfont icon-manage" /> */}
+				<i className="iconfont icon-mobilephone_fill" />
+				<FormattedMessage id="apps" defaultMessage="Apps" />
+			</NavLink>
+		</li>
+		<li>
+			<div className="floating-label-container">
+				<div className="floating-label-positioner">
+					<div className="floating-label">
+						<span>soon</span>
+					</div>
+				</div>
+			</div>
+			<NavLink to="/marketplace" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
+				<i className="iconfont icon-shop_fill" />
+				<FormattedMessage id="marketplace" defaultMessage="Marketplace" />
+			</NavLink>
+		</li>
+		<li>
+			<div className="floating-label-container">
+				<div className="floating-label-positioner">
+					<div className="floating-label">
+						<span>soon</span>
+					</div>
+				</div>
+			</div>
+			<NavLink to="/events" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
+				<i className="iconfont icon-remind_fill" />
+				<FormattedMessage id="events" defaultMessage="Events" />
+			</NavLink>
+		</li>
+		<li>
+			<div className="floating-label-container">
+				<div className="floating-label-positioner">
+					<div className="floating-label">
+						<span>soon</span>
+					</div>
+				</div>
+			</div>
+			<NavLink to="/boards" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
+				<i className="iconfont icon-barrage" />
+				<FormattedMessage id="boards" defaultMessage="Boards" />
+			</NavLink>
+		</li>
+		<li>
+			<div className="floating-label-container">
+				<div className="floating-label-positioner">
+					<div className="floating-label">
+						<span>soon</span>
+					</div>
+				</div>
+			</div>
+			<NavLink to="/feeds" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
+				<i className="iconfont icon-keyboard" />
+				<FormattedMessage id="feeds" defaultMessage="Feeds" />
+			</NavLink>
+		</li>
+		<li>
+			<div className="floating-label-container">
+				<div className="floating-label-positioner">
+					<div className="floating-label">
+						<span>soon</span>
+					</div>
+				</div>
+			</div>
+			<NavLink to="/messenger" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
+				<i className="iconfont icon-interactive_fill" />
+				<FormattedMessage id="messenger" defaultMessage="Messenger" />
+			</NavLink>
+		</li>
+		<li>
+			<div className="floating-label-container">
+				<div className="floating-label-positioner">
+					<div className="floating-label">
+						<span>soon</span>
+					</div>
+				</div>
+			</div>
+			<NavLink to="/network" activeClassName="Sidenav__item--active" isActive={function(){return false}}>
+				<i className="iconfont icon-share" />
+				<FormattedMessage id="network" defaultMessage="Network" />
+			</NavLink>
+		</li>
+	</ul>
+		;
 
 Sidenav.propTypes = {
   username: PropTypes.string,
