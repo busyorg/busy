a199cdd8f8ad2a1b7edda8280cedb0b79df5b342
JM Myers <jm.myers@everbridge.com>
2018-09-17 14:10:21 -0400

    update text on popup

--- src/client/notifications/EmergencyNotificationPopup.js
+++ src/client/notifications/EmergencyNotificationPopup.js
@@ -22,26 +22,18 @@ class EmergencyNotificationPopup extends React.Component {
   render() {
     return (
       <Modal
-        title=""
+        title="Important Announcement"
         visible={this.state.visible}
         onCancel={this.handleModalClose}
         onOk={this.handleModalClose}
       >
         <div className="EmergencyNotificationPopup">
-          <div
-            style={{
-              fontSize: 24,
-              fontWeight: 'bold',
-            }}
-          >
-            <FormattedMessage id="important_announcement" defaultMessage="Important Announcement" />
-          </div>
           <span>
             <FormattedMessage
               id="steem_emergency_maintenance"
               defaultMessage="Steem is experiencing a new fork (HF20), and it went bad somehow, so the blockchain is facing some issues.
-we apologize for the interruptions in service that users experienced today. These interruptions were due to an issue that arose out of the process of
-transitioning to Hardfork 20 for Steem. This issue is currently being worked on and will be resolved."
+we apologize for the interruptions in service that users experienced today. These interruptions were due to an issue that arose during the process of
+transitioning to Hardfork 20 for Steem. This issue is currently being worked on and it will be resolved."
             />
           </span>
         </div>
