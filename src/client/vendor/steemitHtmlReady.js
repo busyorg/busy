84db306f24c12d360a9267d22bbcfb74bfbc9943
Seungwon Eugene Jeong <economicstudio@hotmail.com>
2018-12-30 03:08:11 +0000

    fix relative url

--- src/client/vendor/steemitHtmlReady.js
+++ src/client/vendor/steemitHtmlReady.js
@@ -122,9 +122,8 @@ function link(state, child) {
     state.links.add(url);
     if (state.mutate) {
       // If this link is not relative, http, or https -- add https.
-      if (!/^[\w.-]+:(\/\/)?/.test(url)) {
-        child.setAttribute('href', `https://${url}`);
-      }
+      if (/^\//.test(url)) child.setAttribute('href', `https://busy.org${url}`);
+      else if (!/^[\w.-]+:(\/\/)?/.test(url)) child.setAttribute('href', `https://${url}`);
     }
   }
 }
