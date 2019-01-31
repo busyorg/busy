8c6e4381e4e2b8aab45735c754baa2c050cf8b8e
economicstudio <38183982+economicstudio@users.noreply.github.com>
2019-01-31 12:58:07 +0000

    Fix 2175: 3-digit precision (#2176)
    * Fix 2175: 3-digit precision
    
    * Fix 2175: fix negative and add more test
    
    * 2175: fix negative values
    
    -$0.00 should still be with - sign for consistency.
    
    negative values always have 2-digit precision, since they are shown only in the downvoted list.
    
    * 3-digit-precision for all small value except for 0
    Close #2175
    
    more test codes are also added

--- src/client/components/Utils/USDDisplay.js
+++ src/client/components/Utils/USDDisplay.js
@@ -3,13 +3,19 @@ import PropTypes from 'prop-types';
 import { FormattedNumber } from 'react-intl';
 
 const USDDisplay = ({ value }) => {
-  const negative = value.toFixed(2) < 0;
+  const negative = value < 0;
   const absValue = Math.abs(value);
+  // 0.02 is dust payout threshold (STEEM_MIN_PAYOUT_SBD)
+  const precision = absValue < 0.02 && absValue > 0 ? 3 : 2;
   return (
     <span>
       {negative && '-'}
       {'$'}
-      <FormattedNumber value={absValue} minimumFractionDigits={2} maximumFractionDigits={2} />
+      <FormattedNumber
+        value={absValue}
+        minimumFractionDigits={precision}
+        maximumFractionDigits={precision}
+      />
     </span>
   );
 };
