c2bf69e9cb42ee4c8db807669106705813453111
EM @YEHEY <em@king.net>
2021-01-17 01:02:17 -0500

    update api.

--- src/server/steemAPI.js
+++ src/server/steemAPI.js
@@ -1,6 +1,6 @@
 const createClient = require('lightrpc').createClient;
 
-const client = createClient(process.env.STEEMJS_URL || 'https://api.steemit.com');
+const client = createClient(process.env.STEEMJS_URL || 'https://rpc.blurt.world');
 client.sendAsync = (message, params) =>
   new Promise((resolve, reject) => {
     client.send(message, params, (err, result) => {
